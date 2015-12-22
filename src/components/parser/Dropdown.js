c9f2f786e17deddf7a347c395b8add8701051e30
Alex Plugaru <alex@plugaru.org>
2015-12-22 16:42:26 +0100

    Added some advanced schema for rules

--- src/components/parser/Dropdown.js
+++ src/components/parser/Dropdown.js
@@ -1,9 +1,47 @@
 import React from 'react'
 import Immutable from 'immutable'
 
+
+const BASIC_COMPARISON = [
+    {value: 'eq', label: 'Is'},
+    {value: 'neq', label: 'Is not'}
+]
+
+const ADVANCED_COMPARISON = [
+    {
+        value: 'eq',
+        label: 'is equal'
+    },
+    {
+        value: 'neq',
+        label: 'is NOT equal'
+    },
+    {
+        value: 'gt',
+        label: 'Greater than'
+    },
+    {
+        value: 'lt',
+        label: 'Less than'
+    },
+    {
+        value: 'gte',
+        label: 'Greater than or equal'
+    },
+    {
+        value: 'lte',
+        label: 'Less than or equal'
+    }
+]
+
 export const DEFAULT_OPTION_CHAINS = {
     _: {
+        widget: 'select',
         choices: [
+            {
+                value: 'event',
+                label: 'event'
+            },
             {
                 value: 'ticket',
                 label: 'ticket'
@@ -13,8 +51,24 @@ export const DEFAULT_OPTION_CHAINS = {
                 label: 'user'
             }
         ],
-
+        event: {
+            widget: 'select',
+            choices: BASIC_COMPARISON,
+            operator: {
+                choices: [
+                    {
+                        value: 'ticket-created',
+                        label: 'Ticket created'
+                    },
+                    {
+                        value: 'ticket-updated',
+                        label: 'Ticket updated'
+                    }
+                ]
+            }
+        },
         user: {
+            widget: 'select',
             choices: [
                 {
                     value: 'age',
@@ -26,52 +80,11 @@ export const DEFAULT_OPTION_CHAINS = {
                 }
             ],
             age: {
-                choices: [
-                    {
-                        value: 'equal',
-                        label: 'Is'
-                    },
-                    {
-                        value: 'notEqual',
-                        label: 'Is not'
-                    },
-                    {
-                        value: 'greaterThan',
-                        label: 'Greater than'
-                    },
-                    {
-                        value: 'lessThan',
-                        label: 'Less than'
-                    }
-                ],
-                operator: {
-                    choices: [
-                        {
-                            value: 20,
-                            label: '20'
-                        },
-                        {
-                            value: 30,
-                            label: '30'
-                        },
-                        {
-                            value: 40,
-                            label: '40'
-                        }
-                    ]
-                }
+                widget: 'input',
+                choices: ADVANCED_COMPARISON
             },
             country: {
-                choices: [
-                    {
-                        value: 'equal',
-                        label: 'Is'
-                    },
-                    {
-                        value: 'notEqual',
-                        label: 'Is not'
-                    }
-                ],
+                choices: BASIC_COMPARISON,
                 operator: {
                     choices: [
                         {
@@ -100,28 +113,15 @@ export const DEFAULT_OPTION_CHAINS = {
                 {
                     value: 'channel',
                     label: 'channel'
+                },
+                {
+                    value: 'message',
+                    label: 'message'
                 }
             ],
 
             status: {
-                choices: [
-                    {
-                        value: 'equal',
-                        label: 'Is'
-                    },
-                    {
-                        value: 'notEqual',
-                        label: 'Is not'
-                    },
-                    {
-                        value: 'greaterThan',
-                        label: 'Greater than'
-                    },
-                    {
-                        value: 'lessThan',
-                        label: 'Less than'
-                    }
-                ],
+                choices: ADVANCED_COMPARISON,
                 operator: {
                     choices: [
                         {
@@ -141,16 +141,7 @@ export const DEFAULT_OPTION_CHAINS = {
             },
 
             channel: {
-                choices: [
-                    {
-                        value: 'equal',
-                        label: 'Is'
-                    },
-                    {
-                        value: 'notEqual',
-                        label: 'Is not'
-                    }
-                ],
+                choices: BASIC_COMPARISON,
                 operator: {
                     choices: [
                         {
@@ -167,25 +158,60 @@ export const DEFAULT_OPTION_CHAINS = {
                         }
                     ]
                 }
+            },
+            message: {
+                choices: [
+                    {
+                        value: 'from_agent',
+                        label: 'From Agent'
+                    },
+                    {
+                        value: 'created_datetime',
+                        label: 'Created'
+                    }
+                ],
+                from_agent: {
+                    choices: BASIC_COMPARISON,
+                    operator: {
+                        choices: [
+                            {value: true, label: 'True'},
+                            {value: false, label: 'False'}
+                        ]
+                    }
+                }
             }
         }
     },
     _action: {
         choices: [
             {
-                value: 'action_send_email_notification',
-                label: 'Send email notification to user'
+                value: 'notify',
+                label: 'Send notification'
             },
             {
-                value: 'action_add_tag_to_ticket',
-                label: 'Add tag to ticket'
+                value: 'add_tag',
+                label: 'Add tag'
             }
         ],
-        action_send_email_notification: {
+        notify: {
+            channel: {
+                widget: 'select',
+                choices: [
+                    {value: 'email', label: 'email'},
+                    {value: 'twitter', label: 'twitter'},
+                    {value: 'facebook', label: 'facebook'}
+                ]
+            },
+            sender: {
+                widget: 'input'
+            },
             subject: {
                 widget: 'input'
             },
-            body: {
+            body_text: {
+                widget: 'textarea'
+            },
+            body_html: {
                 widget: 'textarea'
             }
         },
@@ -193,7 +219,7 @@ export const DEFAULT_OPTION_CHAINS = {
             tag: {
                 widget: 'input'
             }
-        },
+        }
     }
 }
 
@@ -207,7 +233,7 @@ function getOptionsBySiblings(leftsiblings) {
 
     let optionItems
     if (options !== undefined) {
-        optionItems = options.toJS().map(function(option, idx) {
+        optionItems = options.toJS().map(function (option, idx) {
             return <option value={ option.value } key={ idx }>{ option.label }</option>
         })
         optionItems.unshift((
@@ -243,13 +269,14 @@ class DropdownButton extends React.Component {
         }
 
         let neutralBtn = this.props.text === "equal" ||
-                         this.props.text === "notEqual" ||
-                         this.props.text === "greaterThan" ||
-                         this.props.text === "lessThan"
-                         ? "neutral" : ""
+        this.props.text === "notEqual" ||
+        this.props.text === "greaterThan" ||
+        this.props.text === "lessThan"
+            ? "neutral" : ""
 
         return (
-            <select className={"ui dropdown " + neutralBtn} value={ this.props.text } onChange={ this.handleChange.bind(this) }>
+            <select className={"ui dropdown " + neutralBtn} value={ this.props.text }
+                    onChange={ this.handleChange.bind(this) }>
                 { optionItems }
             </select>
         )
