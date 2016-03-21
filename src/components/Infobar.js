b9b93618ceb65a5a747388a4af9e4fcd7c5f8a09
Ionut Colceriu <contact@ghinda.net>
2016-03-21 16:12:13 +0200

    Begin work on the resizable infobar.

--- src/components/Infobar.js
+++ src/components/Infobar.js
@@ -2,6 +2,66 @@ import React, { PropTypes } from 'react'
 import Search from './Search'
 
 export default class Infobar extends React.Component {
+    constructor() {
+        super()
+
+        this.dragging = false
+        this.classHandle = 'infobar-drag-handle'
+        this.classActive = 'infobar-drag-active'
+
+        console.log('constructor')
+    }
+
+    getInitialState() {
+        // TODO get bar size from api
+        return {
+            width: 200
+        }
+    }
+
+    dragStart(e) {
+        if (!e.target.classList.contains(this.classHandle)) {
+            return
+        }
+
+        this.dragging = true
+        document.body.classList.add(this.classActive)
+    }
+
+    dragStop(e) {
+        this.dragging = false
+        document.body.classList.remove(this.classActive)
+    }
+
+    drag() {
+        if (this.dragging !== true) {
+            return
+        }
+
+        // TODO set css size and setState
+
+        console.log(this.dragging, 'drag')
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        // TODO, if width changed, check if the same as css (math.round)
+        // and don't re-render if the same
+        return true;
+    }
+
+    componentDidMount() {
+        // TODO stop drag when outside tab
+        window.addEventListener('mousedown', this.dragStart.bind(this))
+        window.addEventListener('mouseup', this.dragStop.bind(this))
+        window.addEventListener('mousemove', this.drag.bind(this))
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('mousedown', this.dragStart.bind(this))
+        window.removeEventListener('mouseup', this.dragStop.bind(this))
+        window.removeEventListener('mousemove', this.drag.bind(this))
+    }
+
     render() {
         const { widgets } = this.props
         if (!widgets.get('items').length) {
@@ -10,7 +70,8 @@ export default class Infobar extends React.Component {
 
         return (
             <div className="infobar">
-                <div className="infobar-top infobar-box infobar-search">
+                <div className="infobar-drag-handle"></div>
+                <div className="infobar-box infobar-search">
                     <Search id="ticket"/>
                 </div>
                 <div className="infobar-top infobar-box">
