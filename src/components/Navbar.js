d45839fd73c2665995dea02b1a785bc041feb732
Martin Latrille <martin@gorgias.io>
2016-03-24 17:08:36 +0100

    added a signout button in the navbar bottom menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -86,6 +86,7 @@ export default class Navbar extends React.Component {
                     </div>
 
                     <div className="menu">
+                        <a className="item" href="/logout"><i className="sign out icon"/> Sign Out</a>
                         <a className="item"><i className="edit icon"/> Edit Profile</a>
                         <a className="item"><i className="globe icon"/> Choose Language</a>
                         <a className="item"><i className="settings icon"/> Account Settings</a>
