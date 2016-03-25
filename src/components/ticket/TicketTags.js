7733c4f469ac68c1a0ed55bbccacb0571b4b4f94
Ionut Colceriu <contact@ghinda.net>
2016-03-25 18:03:15 +0200

    Finish up styles for the Ticket Tags.

--- src/components/ticket/TicketTags.js
+++ src/components/ticket/TicketTags.js
@@ -4,19 +4,23 @@ import _ from 'lodash'
 export default class TicketTags extends React.Component {
     render = () => {
         return (
-            <div className="ui tiny labels">
+            <div className="ui labels">
                 {
                     this.props.tags.map((tag) => {
                         // TODO: Make [x] actually remove tags
                         return (
-                            <div key={tag.get('id')} className="ticket-tag ui teal horizontal label">
+                            <div key={tag.get('id')} className="ticket-tag ui label">
                                 {tag.get('name')}
                                 <i className="icon close" />
                             </div>
                         )
                     })
                 }
-                <a href="#">+ Add tag</a>
+
+                <button className="ticket-tag-add-btn ui button">
+                    <i className="icon plus" />
+                    ADD TAG
+                </button>
             </div>
         )
     }
