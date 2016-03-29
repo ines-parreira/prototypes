56f02d3021bea73f87f8eb1dd3ad110469b64cb2
Alex Plugaru <alex@plugaru.org>
2016-03-29 16:54:57 +0200

    :train: Fixed serving of webpack bundles. Upgraded Flask-Security.

--- webpack.config.js
+++ webpack.config.js
@@ -7,34 +7,36 @@ var staticDirectory = './g/static/private/'
 
 var jsMainFile = staticDirectory + 'js/main.js'
 var jsBuildPath = staticDirectory + '_build/js'
-var jsBundleFile = __PRODUCTION__ ? 'build.min.js' : 'build.js'
+var jsBundleFile = __PRODUCTION__ ? '[hash].build.min.js' : 'build.js'
 
 
 var prodEnvPlugin = new webpack.DefinePlugin({
-  __PRODUCTION__: JSON.stringify(__PRODUCTION__),
+    'process.env': {
+        NODE_ENV: JSON.stringify('production')
+    }
 })
 
 
 module.exports = {
-  devtool: 'eval',
-  entry: jsMainFile,
-  output: { path: jsBuildPath, filename: jsBundleFile },
-  plugins: [prodEnvPlugin],
-  module: {
-    loaders: [
-      {
-        test: /.jsx?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /.json$/,
-        loader: 'json-loader',
-      },
-      {
-        test: /\.less$/,
-        loader: "style!css!less"
-      },
-    ]
-  },
+    devtool: 'eval',
+    entry: jsMainFile,
+    output: {path: jsBuildPath, filename: jsBundleFile},
+    plugins: [prodEnvPlugin],
+    module: {
+        loaders: [
+            {
+                test: /.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /.json$/,
+                loader: 'json-loader'
+            },
+            {
+                test: /\.less$/,
+                loader: 'style!css!less'
+            }
+        ]
+    }
 }
