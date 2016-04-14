080b643bd0fcf6b372147f0270617bb69f4b9528
Alex Plugaru <alex@plugaru.org>
2016-04-14 16:48:01 +0200

    Initial version of the editor

--- src/components/ticket/ticketview/replyarea/TicketReply.js
+++ src/components/ticket/ticketview/replyarea/TicketReply.js
@@ -1,10 +1,56 @@
-import React, { PropTypes } from 'react'
+import React, {PropTypes} from 'react'
+import {Editor, EditorState, ContentState, RichUtils} from 'draft-js'
 
 export default class TicketReply extends React.Component {
+    constructor(props) {
+        super(props)
+        const contentState = ContentState.createFromText(props.value)
+        this.state = {editorState: EditorState.createWithContent(contentState)}
+        this.focus = () => this.refs.editor.focus()
+        this.handleKeyCommand = this.handleKeyCommand.bind(this)
+    }
+
     onChange = (ev) => {
+        this.setState({editorState: ev})
+        const cleanHTML = this.cleanupHTML(this.refs.editor.refs.editor).innerHTML
+        const text = ev.getCurrentContent().getPlainText()
+
         return this.props.actions.ticket.setResponseText(
-            this.props.currentUser, ev.target.value
+            this.props.currentUser,
+            text,
+            cleanHTML
         )
+    }
+
+    handleKeyCommand(command) {
+        const newState = RichUtils.handleKeyCommand(this.state.editorState, command)
+        if (newState) {
+            this.onChange(newState)
+            return true
+        }
+        return false
+    }
+
+    // Before sending it to our API we're cleaning up all data-* attributes from elements
+    cleanupHTML = (domNode) => {
+        const domClone = domNode.cloneNode(true)
+        const domNodes = domClone.querySelectorAll('*')
+
+        for (let k = 0; k <= domNodes.length; k++) {
+            if (!domNodes.hasOwnProperty(k)) {
+                continue
+            }
+            const attrs = domNodes[k].attributes
+            for (let i = 0; i < attrs.length; i++) {
+                if (attrs[i].name.slice(0, 5) !== 'data-') {
+                    continue
+                }
+                domNodes[k].removeAttribute(attrs[i].name)
+                i--
+            }
+        }
+
+        return domClone
     }
 
     render() {
@@ -12,9 +58,11 @@ export default class TicketReply extends React.Component {
             <div className="TicketReply search ui raised segment">
                 <form className="ui reply form">
                     <div className="field">
-                        <textarea
-                            value={this.props.value}
+                        <Editor
+                            editorState={this.state.editorState}
                             onChange={this.onChange}
+                            handleKeyCommand={this.handleKeyCommand}
+                            ref="editor"
                         />
                     </div>
                 </form>
