4e76cb5aa814e515a660bde679ba1b7dbc07dc8a
Martin Latrille <martin@gorgias.io>
2016-04-18 11:48:14 +0200

    cleanup

--- src/containers/Ticket.js
+++ src/containers/Ticket.js
@@ -80,8 +80,9 @@ class TicketContainer extends React.Component {
 
     computeNextUrl(ascending) {
         const translation = ascending ? 1 : -1
-        console.log(translation)
-        const nextTicket = this.props.tickets.get('items').toJS()[this.props.tickets.get('currentTicketIndex') + translation]
+        const nextTicket = this.props.tickets.get('items').toJS()[
+            this.props.tickets.get('currentTicketIndex') + translation
+        ]
         let nextTicketUrl = null
 
         if (nextTicket) {
