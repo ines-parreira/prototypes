071e00f9fdba78033f53f64ec415f38d08e0ba22
Alex Plugaru <alex@plugaru.org>
2016-05-02 11:32:04 +0200

    Reset the editor after the message was submitted

--- src/components/ticket/ticketview/replyarea/TicketReply.js
+++ src/components/ticket/ticketview/replyarea/TicketReply.js
@@ -14,6 +14,11 @@ export default class TicketReply extends React.Component {
     constructor(props) {
         super(props)
 
+        this.focus = () => this.refs.editor.focus()
+        this.state = this.initialState(this.props)
+    }
+
+    initialState(props) {
         const contentState = ContentState.createFromText(props.value)
 
         const mentions = fromJS(props.users.get('agents', []).map(user => {
@@ -32,15 +37,26 @@ export default class TicketReply extends React.Component {
             createEmojiPlugin()
         ]
 
-        this.state = {
+        return {
             editorState: EditorState.createWithContent(contentState),
             plugins
         }
+    }
 
-        this.focus = () => this.refs.editor.focus()
-        this.handleKeyCommand = this.handleKeyCommand.bind(this)
-        this.updateAfterGorgias = this.updateAfterGorgias.bind(this)
-        this.receiveMessage = this.receiveMessage.bind(this)
+    componentDidMount() {
+        // we're listening from stuff that comes from the extension
+        window.addEventListener('message', this.onMessage, false)
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.value === '') {
+            this.state = this.initialState(nextProps)
+        }
+    }
+
+    componentWillUnmount() {
+        // We should remove the listener so we don't explode the browser
+        window.removeEventListener('message', this.onMessage, false)
     }
 
     onChange = (editorState) => {
@@ -56,36 +72,29 @@ export default class TicketReply extends React.Component {
         )
     }
 
-    updateAfterGorgias(payload) {
-        const actualHTML = this.refs.editor.refs.editor.refs.editor.innerHTML
-        const contentBlock = convertFromHTML(actualHTML)
-        const contentState = ContentState.createFromBlockArray(contentBlock)
-        const editorState = EditorState.moveFocusToEnd(EditorState.createWithContent(contentState))
+    onMessage = (event) => {
+        // We're waiting for an event from Gorgias extension - that the template has been inserted
+        if (event.data.source && event.data.source === 'gorgias-extension') {
+            if (event.data.payload.event === 'template-inserted') {
+                // update the editor state after the extension was used
+                const actualHTML = this.refs.editor.refs.editor.refs.editor.innerHTML
+                const contentBlock = convertFromHTML(actualHTML)
+                const contentState = ContentState.createFromBlockArray(contentBlock)
+                const editorState = EditorState.moveFocusToEnd(EditorState.createWithContent(contentState))
 
-        this.setState({editorState})
+                this.setState({editorState})
+            }
+        }
     }
 
     // This is for handling things like Bold, Italic, etc..
-    handleKeyCommand(command) {
+    handleKeyCommand = (command) => {
         const newState = RichUtils.handleKeyCommand(this.state.editorState, command)
         if (newState) {
             this.onChange(newState)
             return true
         }
         return false
-    }
-
-    receiveMessage(event) {
-        // We're waiting for an event from Gorgias extension - that the template has been inserted
-        if (event.data.source && event.data.source === 'gorgias-extension') {
-            if (event.data.payload.event === 'template-inserted') {
-                this.updateAfterGorgias(event.data.payload)
-            }
-        }
-    }
-
-    componentDidMount() {
-        window.addEventListener('message', this.receiveMessage, false)
     }
 
     render() {
