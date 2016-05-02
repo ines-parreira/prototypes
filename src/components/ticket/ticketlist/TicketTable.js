d65fb1683358236360f8b51e39ca489502357d46
Kalkut <berenger.makita@gmail.com>
2016-05-02 15:38:32 +0200

    Loader as a component

--- src/components/ticket/ticketlist/TicketTable.js
+++ src/components/ticket/ticketlist/TicketTable.js
@@ -3,6 +3,7 @@ import TicketTableRow from './TicketTableRow'
 import PlainColumnHeader from './PlainColumnHeader'
 import ColumnHeader from './ColumnHeader'
 import SemanticPaginator from '../../SemanticPaginator'
+import Loader from '../../Loader'
 
 
 const columnToFilterName = {
@@ -14,19 +15,6 @@ const columnToFilterName = {
 
 export default class TicketTable extends React.Component {
     onPageChange = (page) => this.props.fetchPage(page)
-
-    renderLoading = () => {
-        const nbPages = this.props.tickets.getIn(['resp_meta', 'nb_pages'])
-        const message = nbPages === 0 ? 'No tickets found.' : 'Loading...'
-
-        return (
-            <div className="loading-container">
-                <div className="loading">
-                    <p>{message}</p>
-                </div>
-            </div>
-        )
-    }
 
     renderColumnHeader = (column) => {
         const filterSpec = this.props.getFilterSpecForColumn(column.name)
@@ -51,51 +39,55 @@ export default class TicketTable extends React.Component {
         const style = { maxWidth: this.props.width }
         const ticketTableStyle = { marginTop: this.props.isDirty ? '14em' : '9em' }
 
-        if (this.props.tickets.get('items').size === 0) {
-            return this.renderLoading()
-        }
+        const nbPages = this.props.tickets.getIn(['resp_meta', 'nb_pages'])
+        const message = nbPages === 0 ? 'No tickets found.' : 'Loading...'
 
         return (
-            <div className="TicketTable" style={ticketTableStyle}>
-                <div>
-                    <div className="ui grid" style={style}>
-                        <div className="row head-row" >
-                            <div className="one-fixed wide column">
-                                <span className="ui checkbox">
-                                    <input type="checkbox"/>
-                                    <label></label>
-                                </span>
+            <Loader 
+                loaded={this.props.tickets.get('items').size !== 0}
+                message={ message }
+            >   
+                <div className="TicketTable" style={ticketTableStyle}>
+                    <div>
+                        <div className="ui grid" style={style}>
+                            <div className="row head-row" >
+                                <div className="one-fixed wide column">
+                                    <span className="ui checkbox">
+                                        <input type="checkbox"/>
+                                        <label></label>
+                                    </span>
+                                </div>
+                                {this.props.columns.map(this.renderColumnHeader)}
                             </div>
-                            {this.props.columns.map(this.renderColumnHeader)}
+                        </div>
+                        <div>
+                            {
+                                this.props.tickets.get('items').toJS().map((ticket, curIndex) => (
+                                    <TicketTableRow
+                                        key={ticket.id}
+                                        ticket={ticket}
+                                        view={this.props.view}
+                                        page={this.props.tickets.getIn(['resp_meta', 'page'])}
+                                        width={this.props.width}
+                                        columns={this.props.columns}
+                                        currentUser={this.props.currentUser}
+                                        pushState={this.props.pushState}
+                                        curIndex={curIndex}
+                                        saveIndex={this.props.actions.ticket.saveIndex}
+                                    />
+                                ))
+                            }
                         </div>
                     </div>
-                    <div>
-                        {
-                            this.props.tickets.get('items').toJS().map((ticket, curIndex) => (
-                                <TicketTableRow
-                                    key={ticket.id}
-                                    ticket={ticket}
-                                    view={this.props.view}
-                                    page={this.props.tickets.getIn(['resp_meta', 'page'])}
-                                    width={this.props.width}
-                                    columns={this.props.columns}
-                                    currentUser={this.props.currentUser}
-                                    pushState={this.props.pushState}
-                                    curIndex={curIndex}
-                                    saveIndex={this.props.actions.ticket.saveIndex}
-                                />
-                            ))
-                        }
-                    </div>
+                    <SemanticPaginator
+                        page={this.props.tickets.getIn(['resp_meta', 'page'])}
+                        totalPages={this.props.tickets.getIn(['resp_meta', 'nb_pages'])}
+                        onChange={this.onPageChange}
+                        radius={0}
+                        anchor={3}
+                    />
                 </div>
-                <SemanticPaginator
-                    page={this.props.tickets.getIn(['resp_meta', 'page'])}
-                    totalPages={this.props.tickets.getIn(['resp_meta', 'nb_pages'])}
-                    onChange={this.onPageChange}
-                    radius={0}
-                    anchor={3}
-                />
-            </div>
+            </Loader>
         )
     }
 }
