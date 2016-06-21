3eca466e1c7f9866c9647b7980ce61a6ede95281
Alex Plugaru <alex@plugaru.org>
2016-06-21 14:43:46 +0200

    Only show quote button when the body and stripped are different

--- src/components/ticket/ticketview/TicketMessageBody.js
+++ src/components/ticket/ticketview/TicketMessageBody.js
@@ -13,15 +13,21 @@ export default class TicketMessageBody extends React.Component {
 
         let body = message.body_html || message.body_text
         const stripped = message.stripped_html || message.stripped_text
+
         let quoteButton = null
         if (stripped) {
-            quoteButton = (
-                <div className="mail-quote-toggle"
-                     onClick={() => this.setState({ showFullBody: !this.state.showFullBody})}
-                >
-                    <i className="ellipsis horizontal icon"/>
-                </div>
-            )
+            // only show quoteButton if the contents of body and stripped are different
+            // we're ignoring whitespaces because mailgun sends stripped_html without spaces
+            if (stripped.replace(/\s+/g, '') !== body.replace(/\s+/g, '')) {
+                quoteButton = (
+                    <div className="mail-quote-toggle"
+                         onClick={() => this.setState({ showFullBody: !this.state.showFullBody})}
+                    >
+                        <i className="ellipsis horizontal icon"/>
+                    </div>
+                )
+            }
+
             // by default we show only the stripped body
             if (!this.state.showFullBody) {
                 body = stripped
