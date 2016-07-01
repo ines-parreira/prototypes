13baadf16cb5dd03424431d41c0415b4130128b0
Martin Latrille <martin@gorgias.io>
2016-07-01 11:46:25 +0200

    used regular regex for email

--- src/utils.js
+++ src/utils.js
@@ -36,7 +36,7 @@ export function getCode(ast) {
 }
 
 export function validateEmail(email) {
-    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const re = /^[^@]+@[^@]+\.[^@]+$/;
     return re.test(email);
 }
 
