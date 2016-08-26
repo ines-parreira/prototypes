f8f4e3275389bece2cc38b24c9411dab04fddbd8
Alex Plugaru <alex@plugaru.org>
2016-08-26 18:13:48 +0200

    Fixed eslint error

--- src/pages/tickets/detail/components/replyarea/TicketReply.js
+++ src/pages/tickets/detail/components/replyarea/TicketReply.js
@@ -194,7 +194,7 @@ export default class TicketReply extends React.Component {
                         <Editor
                             ref="editor"
                             tabIndex="4"
-                            spellCheck={true}
+                            spellCheck
                             editorState={this.state.editorState}
                             plugins={plugins}
                             onChange={this.onChange}
