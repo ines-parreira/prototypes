caf34da106f258a1b00191de06633e6e84e6896b
Jean-Elie Barjonet <jebarjonet@users.noreply.github.com>
2016-09-28 10:14:18 +0200

    Add try/catch to template interpolation (#804)

--- src/pages/common/utils/template.js
+++ src/pages/common/utils/template.js
@@ -3,11 +3,15 @@ import {formatDatetime} from '../../../utils'
 
 // render a template like: `Order {self.id}` to `Order 37337`
 export function renderTemplate(body, context = {}) {
-    // So we can format the dates inside templates
-    const newContext = context
-    newContext.formatDatetime = formatDatetime
+    try {
+        // So we can format the dates inside templates
+        const newContext = context
+        newContext.formatDatetime = formatDatetime
 
-    return template(body, {
-        interpolate: /{([\s\S]+?)}/g
-    })(newContext)
+        return template(body, {
+            interpolate: /{([\s\S]+?)}/g
+        })(newContext)
+    } catch (e) {
+        return body
+    }
 }
