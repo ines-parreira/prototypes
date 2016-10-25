d0baf18d73732f9a11629d13128fd96a6c74dcdd
Louis Barranqueiro <LouisBarranqueiro@users.noreply.github.com>
2016-10-25 11:52:19 +0200

    apply devTools middleware in redux store (#919)
    * apply devTools middleware in redux store

--- src/store/configureStore.dev.js
+++ src/store/configureStore.dev.js
@@ -1,4 +1,4 @@
-import {createStore, applyMiddleware} from 'redux'
+import {createStore, compose, applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
 import amplitudeTracker from './middlewares/amplitudeTracker'
 import serverErrorHandler from './middlewares/serverErrorHandler'
@@ -6,14 +6,22 @@ import createLogger from 'redux-logger'
 import rootReducer from '../state/reducers'
 
 export default function configureStore(initialState) {
+    let middlewares = applyMiddleware(
+        thunk,
+        serverErrorHandler,
+        createLogger({collapsed: true}),
+        amplitudeTracker
+    )
+
+    // check if Redux devTools Chrome extension is installed
+    // to apply it as a middleware
+    if (window.devToolsExtension) {
+        middlewares = compose(middlewares, window.devToolsExtension())
+    }
+
     return createStore(
         rootReducer,
         initialState,
-        applyMiddleware(
-            thunk,
-            serverErrorHandler,
-            createLogger({collapsed: true}),
-            amplitudeTracker
-        )
+        middlewares
     )
 }
