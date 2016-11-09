09be2e6a00f75e5743faabd896847c97f22878d7
Jean-Elie <je.barjonet@gmail.com>
2016-11-09 13:57:49 -0800

    fix widget edition mode on tickets

--- src/pages/routes.js
+++ src/pages/routes.js
@@ -90,7 +90,7 @@ export default (
                 navbar: TicketNavbarContainer,
                 infobar: TicketInfobarContainer
             }}
-            isEditingWidget
+            isEditingWidgets
         />
         <Route
             path="tickets"
