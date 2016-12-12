930e2f7bf7f1443b83b08c67bef1919c4b40463a
Jean-Elie <je.barjonet@gmail.com>
2016-12-12 14:15:05 -0800

    rewrite active link in navbar menu

--- src/pages/common/components/Navbar.js
+++ src/pages/common/components/Navbar.js
@@ -1,12 +1,34 @@
 import React, {PropTypes} from 'react'
 import {connect} from 'react-redux'
 import {Link} from 'react-router'
+import classnames from 'classnames'
 import _capitalize from 'lodash/capitalize'
 
 // A <Link /> with some default styles
-const NavLink = (props) => (
-    <Link {...props} activeClassName="active" className="item"/>
-)
+const NavLink = (props) => {
+    let url = props.to || ''
+
+    // if the url ends with an "s", then we also count the urls without an "s"
+    // ex: we highlight "Tickets" when we are on /app/tickets/something or /app/ticket/something
+    if (url.endsWith('s')) {
+        url = url.slice(0, -1)
+    }
+
+    const className = classnames('item', {
+        active: window.location.pathname.includes(url),
+    })
+
+    return (
+        <Link
+            className={className}
+            {...props}
+        />
+    )
+}
+
+NavLink.propTypes = {
+    to: PropTypes.string.isRequired,
+}
 
 class Navbar extends React.Component {
     componentWillMount() {
@@ -29,7 +51,7 @@ class Navbar extends React.Component {
             <div className="navbar" ref="navbar">
                 <div id="main-menu" className="navbar-btn navbar-btn-category ui dropdown">
                     {this.state.title}
-                    <i className="icon angle down"/>
+                    <i className="icon angle down" />
                     <div className="menu">
                         <NavLink
                             to="/app/tickets"
@@ -62,7 +84,7 @@ class Navbar extends React.Component {
                             Rules
                         </NavLink>
                         <NavLink to="/app/settings"
-                            onClick={() => this.setState({title: 'Settings'})}
+                                 onClick={() => this.setState({title: 'Settings'})}
                         >
                             Settings
                         </NavLink>
@@ -74,24 +96,26 @@ class Navbar extends React.Component {
                 </div>
 
                 <div id="user-menu" className="navbar-btn ui dropdown">
-                    <i className="ellipsis horizontal icon"/>
+                    <i className="ellipsis horizontal icon" />
                     <div>
                         <h4 className="current-user">
-                            <i className="status green circle icon" title="User online"/>
+                            <i className="status green circle icon" title="User online" />
                             <span className="name" title={currentUser.get('name')}>{currentUser.get('name')}</span>
                         </h4>
                     </div>
 
                     <div className="menu">
                         <Link className="item" to="/app/settings/your-profile">
-                            <i className="user icon"/>
+                            <i className="user icon" />
                             Your profile
                         </Link>
-                        <a className="item" href="/logout"><i className="sign out icon"/> Sign Out</a>
-                        {/*
-                         <a className="disabled item"><i className="globe icon"/> Choose Language</a>
-                         <a className="disabled item"><i className="settings icon"/> Account Settings</a
-                         */}
+                        <a
+                            className="item"
+                            href="/logout"
+                        >
+                            <i className="sign out icon" />
+                            Sign Out
+                        </a>
                     </div>
                 </div>
             </div>
