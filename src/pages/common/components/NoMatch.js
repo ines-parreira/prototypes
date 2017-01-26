094c2e051d7880d9ae03d193ed92c99f10177168
Alex Plugaru <alex@gorgias.io>
2017-01-26 11:21:59 -0800

    Fixed lint

--- src/pages/common/components/NoMatch.js
+++ src/pages/common/components/NoMatch.js
@@ -1,19 +1,18 @@
 import React from 'react'
-import { Link } from 'react-router'
 
 export default class NoMatch extends React.Component {
 
     render() {
         return (
-           <div>
+            <div>
                 <h1 className="ui center aligned icon header">
-                    <i className="find icon"></i>
+                    <i className="find icon"/>
                     <div className="content">
                         Error! That page was not found :(
                         <div className="sub header">Use the dropdown on the left to navigate to the page you want!</div>
                     </div>
                 </h1>
-           </div>
+            </div>
         )
     }
 }
