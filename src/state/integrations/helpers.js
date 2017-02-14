dd95af139d4b395c5e0637a716cfa8b377a3ddc1
Louis Barranqueiro <LouisBarranqueiro@users.noreply.github.com>
2017-02-14 10:51:54 -0800

    fix integration count (#1372)
    * fix integration counts
    
    * lint code

--- src/state/integrations/helpers.js
+++ src/state/integrations/helpers.js
@@ -9,11 +9,15 @@ const getIntegrationsCountPerType = (integrations = []) => {
     return integrations
         .reduce((accumulator, item) => {
             const newAccumulator = accumulator
-            if (item.get('type') in accumulator && !item.get('deactivated_datetime')) {
-                newAccumulator[item.get('type')] += 1
-            } else {
-                newAccumulator[item.get('type')] = 1
+
+            if (!item.get('deactivated_datetime')) {
+                if (item.get('type') in accumulator) {
+                    newAccumulator[item.get('type')] += 1
+                } else {
+                    newAccumulator[item.get('type')] = 1
+                }
             }
+
             return newAccumulator
         }, {})
 }
