627ad0739bd29da601daf8053dcffe246068f711
Jean-Elie <je.barjonet@gmail.com>
2017-04-25 17:40:25 +0200

    Revert "Allow overlapping styles in draftjs (#1759)"
    This reverts commit 3357a9d6b6a080cdab715000ef3fc64c39a2f520.

--- src/pages/common/draftjs/plugins/toolbar/utils.js
+++ src/pages/common/draftjs/plugins/toolbar/utils.js
@@ -1,4 +1,4 @@
-import {RichUtils} from 'draft-js'
+import {RichUtils, Entity} from 'draft-js'
 import _forEach from 'lodash/forEach'
 import actions from './actions'
 
@@ -54,10 +54,94 @@ export const getToolbarActions = () => {
             return updatedAction.active(block, editorState)
         }
 
+        // disable style on links and links on style
+        // TODO @jebarjonet remove this after https://github.com/HubSpot/draft-convert/pull/17 is resolved
         if (!updatedAction.isDisabled) {
-            // useless for now but could be useful at some point
-            updatedAction.isDisabled = (getEditorState) => { //eslint-disable-line
-                return false
+            updatedAction.isDisabled = (getEditorState) => {
+                const editorState = getEditorState()
+
+                const isLink = updatedAction.label === 'Link'
+
+                let preventAction = false
+                const selection = editorState.getSelection()
+
+                // get all blocks
+                const blocks = editorState.getCurrentContent().getBlocksAsArray()
+
+                // check if a origin block is after a target block
+                const isBlockAfter = (originBlockKey, targetBlockKey, strict = false) => {
+                    const originIndex = blocks.findIndex(b => b.getKey() === originBlockKey)
+                    const targetIndex = blocks.findIndex(b => b.getKey() === targetBlockKey)
+                    return strict ? originIndex < targetIndex : originIndex <= targetIndex
+                }
+
+                // check if two blocks are the same
+                const isSameBlock = (originBlockKey, targetBlockKey) => {
+                    return originBlockKey === targetBlockKey
+                }
+
+                // check if a block is between two other blocks
+                const isBlockBetween = (targetBlockKey, startBlockKey, endBlockKey, strict) => {
+                    return isBlockAfter(startBlockKey, targetBlockKey, strict)
+                        && isBlockAfter(targetBlockKey, endBlockKey, strict)
+                }
+
+                // check if a target block is being selected
+                const isSelecting = (targetBlockKey, start, end) => {
+                    // block in between start and end of selection, it is therefore selected
+                    if (isBlockBetween(targetBlockKey, selection.getStartKey(), selection.getEndKey(), true)) {
+                        return true
+                    }
+
+                    // block on the start edge of selection
+                    if (isSameBlock(targetBlockKey, selection.getStartKey())) {
+                        // block on the end edge of selection too: we are selecting only one block from start to end
+                        if (isSameBlock(targetBlockKey, selection.getEndKey())) {
+                            return selection.getStartOffset() <= end && selection.getEndOffset() >= start
+                        }
+
+                        return selection.getStartOffset() <= end
+                    }
+
+                    // block on the end edge of selection
+                    if (isSameBlock(targetBlockKey, selection.getEndKey())) {
+                        return selection.getEndOffset() >= start
+                    }
+
+                    return false
+                }
+
+                const selectedBlocks = blocks
+                    .filter(block => isBlockBetween(block.getKey(), selection.getStartKey(), selection.getEndKey()))
+
+                if (isLink) {
+                    // if we are using a link button, prevent using it on a style
+                    selectedBlocks.forEach((block) => {
+                        block.findStyleRanges(
+                            (character) => {
+                                return character.get('style').size > 0
+                            },
+                            (start, end) => {
+                                preventAction = isSelecting(block.getKey(), start, end)
+                            }
+                        )
+                    })
+                } else {
+                    // if we are using a style button, prevent using it on a link
+                    selectedBlocks.forEach((block) => {
+                        block.findEntityRanges(
+                            (character) => {
+                                const entityKey = character.getEntity()
+                                return entityKey !== null && Entity.get(entityKey).getType() === 'link'
+                            },
+                            (start, end) => {
+                                preventAction = isSelecting(block.getKey(), start, end)
+                            }
+                        )
+                    })
+                }
+
+                return preventAction
             }
         }
 
