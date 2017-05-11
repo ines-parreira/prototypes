21c5e5cb9f76c6284ec263039a707c9dccd7ea13
Alex Plugaru <alex@gorgias.io>
2017-05-10 18:40:04 -0700

    Added Loader tests

---
+++ src/pages/common/components/test/Loader.spec.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import {Loader} from '../Loader'
+
+describe('Loader component', () => {
+    it('simple message', () => {
+        const component = renderer.create(
+            <Loader message={'hello'}/>
+        ).toJSON()
+        expect(component).toMatchSnapshot()
+    })
+
+    it('inline', () => {
+        const component = renderer.create(
+            <Loader inline/>
+        ).toJSON()
+        expect(component).toMatchSnapshot()
+    })
+
+    it('loading', () => {
+        const component = renderer.create(
+            <Loader loading/>
+        ).toJSON()
+        expect(component).toMatchSnapshot()
+    })
+
+    it('not loading', () => {
+        const component = renderer.create(
+            <Loader loading={false}/>
+        ).toJSON()
+        expect(component).toMatchSnapshot()
+    })
+
+    it('size', () => {
+        const component = renderer.create(
+            <Loader size="large"/>
+        ).toJSON()
+        expect(component).toMatchSnapshot()
+    })
+})
