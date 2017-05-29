cd077a8146defe2fba55808d5f8b5d32c6d25a75
martinlatrille <martin@gorgias.io>
2017-05-29 16:14:14 -0700

    Revert "remove customer from merge users modal"
    This reverts commit 6b9e23a648dc6c9afe2e7a06a09a325471ee8f23.

--- src/pages/common/components/mergeUsers/MergeUsersModal.js
+++ src/pages/common/components/mergeUsers/MergeUsersModal.js
@@ -7,10 +7,12 @@ import {Button, UncontrolledTooltip} from 'reactstrap'
 
 import Modal from '../Modal'
 
+import {JSONTree} from './../JSONTree'
 import {USER_CHANNEL_CLASS} from './../../../../config'
 import BinaryChoiceField from '../../forms/BinaryChoiceField'
 import MultiSelectBinaryChoiceField from '../../forms/MultiSelectBinaryChoiceField'
 import {logEvent} from '../../../../store/middlewares/amplitudeTracker'
+import {isCustomerDataPresent} from '../infobar/utils'
 
 class MergeUsersModal extends React.Component {
     componentDidMount = () => {
@@ -85,6 +87,11 @@ class MergeUsersModal extends React.Component {
 
     render() {
         const {destinationUser, sourceUser, handleSubmit, isLoading, primaryEmail, requiredAddresses} = this.props
+
+        let baseCustomer = destinationUser.get('customer')
+        baseCustomer = isCustomerDataPresent(baseCustomer) ? baseCustomer.toJS() : {}
+        let mergeCustomer = sourceUser.get('customer')
+        mergeCustomer = isCustomerDataPresent(mergeCustomer) ? mergeCustomer.toJS() : {}
 
         const allChannels = destinationUser.get('channels').toJS().concat(sourceUser.get('channels').toJS())
 
@@ -203,6 +210,22 @@ class MergeUsersModal extends React.Component {
                             ]}
                             propertiesToCompare={['address', 'type']}
                         />
+                        <Field
+                            type="json"
+                            label="Customer data"
+                            name="user.customer"
+                            component={BinaryChoiceField}
+                            options={[
+                                {
+                                    label: <JSONTree data={fromJS(baseCustomer)} />,
+                                    value: baseCustomer
+                                },
+                                {
+                                    label: <JSONTree data={fromJS(mergeCustomer)} />,
+                                    value: mergeCustomer
+                                }
+                            ]}
+                        />
                     </div>
 
                     <div className="pull-right buttons-bar">
@@ -215,7 +238,7 @@ class MergeUsersModal extends React.Component {
                             Cancel
                         </Button>
                         <Button
-                            color="success"
+                            color="warning"
                             type="submit"
                             className={classnames({
                                 'btn-loading': isLoading,
