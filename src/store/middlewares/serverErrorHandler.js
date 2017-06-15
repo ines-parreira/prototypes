52a65220be254c1a6f9b9362df1ed362dbba4551
Louis Barranqueiro <LouisBarranqueiro@users.noreply.github.com>
2017-06-14 17:50:19 -0700

    fix redirection (#2041)
    * fix redirection
    
    * add error message

--- src/store/middlewares/serverErrorHandler.js
+++ src/store/middlewares/serverErrorHandler.js
@@ -16,13 +16,16 @@ const IGNORED_PREFIXS = [
 
 const serverErrorHandler = store => next => action => {
     const status = _get(action, 'error.response.status', '')
+    const error = _get(action, 'error.response.data.error', '')
 
     // notify user and redirect him to the login page when his session has expired
-    if (status === 419 || status === 401) {
-        store.dispatch(notify({
-            type: 'error',
-            title: 'Your session has expired. You will be redirected on the login page in a few seconds.',
-        }))
+    if ([401, 419].includes(status)) {
+        if (error.msg) {
+            store.dispatch(notify({
+                type: 'error',
+                title: error.msg
+            }))
+        }
 
         setTimeout(() => {
             window.location.href = `${window.location.origin}/login`
@@ -36,8 +39,6 @@ const serverErrorHandler = store => next => action => {
         && !_some(IGNORED_PREFIXS, (prefix) => action.type.startsWith(prefix))
 
     if (shouldDisplayError) {
-        const error = _get(action, 'error.response.data.error', '')
-
         let message =
             error.msg
             || action.reason
