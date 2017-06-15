99277f351d4469922a4b0ebedc5914869ee1aeea
Ionuț Colceriu <contact@ghinda.net>
2017-06-15 20:08:51 +0300

    Optimize webpack build time (#2032)
    * Replace babel-preset-es2015 with babel-preset-env. Saves ~5 of build time.
    Setting `modules:false` to use webpack to bundle, instead of babel, doesn't seem to improve build time.
    babel-preset-es2015 will also soon be deprecated.
    
    * Use the cacheDirectory option on babel-loader. Can save around ~20s after the initial cache is built.
    
    * First version of a dll webpack config, to compile vendors separately.
    Performance just slightly improved.
    Still has some issues with the production manifest.json file missing the vendors build.
    
    * Add the missing vendors dll file to the final manifest.
    
    * Clean up both webpack configs.
    
    * Combine the dll webpack config with the default one, so we only have a single config to maintain.
    
    * Bump webpack-manifest-plugin version.
    
    * Remove unused node deps.
    Saves about ~2s of build time.
    
    * pathinfo should be disabled in production. also saves about 5s of build time.
    tweak the disableHostCheck devServer config.

--- webpack.config.js
+++ webpack.config.js
@@ -2,101 +2,124 @@ const webpack = require('webpack')
 const path = require('path')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const ManifestPlugin = require('webpack-manifest-plugin')
+
 const pkg = require('./package.json')
 
 const __PRODUCTION__ = process.env.NODE_ENV === 'production'
 const HASH = process.env.GIT_COMMIT ? process.env.GIT_COMMIT : '[hash]'
 
 const srcDir = path.join(__dirname, 'g/static/private')
+const buildDir = path.join(srcDir, '_build')
 const jsBundleFile = __PRODUCTION__ ? `${HASH}.build.min.js` : 'build.js'
 const styleBundleFile = __PRODUCTION__ ? `${HASH}.build.min.css` : 'build.css'
+const vendorsBundleFile = __PRODUCTION__ ? `${HASH}.vendors.min.js` : 'vendors.js'
 
-let plugins = [
-    new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /fr/),
-    new ExtractTextPlugin({
-        filename: styleBundleFile,
-        allChunks: true
-    }),
-    new webpack.optimize.CommonsChunkPlugin({
-        name: 'vendors',
-        filename: __PRODUCTION__ ? '[hash].[name].min.js' : '[name].js',
-        minChunks: 2
-    }),
-    new ManifestPlugin(),
-    new webpack.ProvidePlugin({
-        Tether: 'tether',
-    }),
-    // escodegen throws a warning
-    new webpack.ContextReplacementPlugin(/escodegen/, /^$/)
-]
-
+const pathinfo = !__PRODUCTION__
+const devtool = __PRODUCTION__ ? 'source-map' : 'eval'
 const devServer = {
+    contentBase: buildDir,
     host: '0.0.0.0',
-}
-
-if (!__PRODUCTION__) {
     // disable host check for dev env (if using proxy)
-    devServer.disableHostCheck = true
+    disableHostCheck: !__PRODUCTION__
 }
 
 const cssOnlyPackages = ['semantic-ui', 'bootstrap', 'font-awesome']
 const vendors = Object.keys(pkg.dependencies).filter(m => !cssOnlyPackages.includes(m))
 
-module.exports = {
-    devtool: __PRODUCTION__ ? 'source-map' : 'eval',
-    entry: {
-        build: `${srcDir}/js/main.js`,
-        vendors,
-    },
-    output: {
-        path: `${srcDir}/_build`,
-        pathinfo: true,
-        filename: jsBundleFile
-    },
-    devServer,
-    plugins,
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/
+module.exports = (env = {}) => {
+    if (env.dll) {
+        return {
+            devtool,
+            entry: {
+                vendors,
             },
-            {
-                test: /.json$/,
-                loader: 'json-loader'
+            output: {
+                path: buildDir,
+                pathinfo,
+                filename: vendorsBundleFile,
+                library: 'vendors',
             },
-            {
-                test: /\.(jpe?g|png|gif)$/i,
-                loader: 'url-loader'
-            },
-            {
-                test: /\.css$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: 'css-loader'
-                })
-            },
-            {
-                test: /\.less$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: [
-                        'css-loader',
-                        'less-loader'
-                    ]
-                })
-            },
-            // custom fonts
-            {
-                test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
-                loader: 'url-loader'
-            },
-            // audio
-            {
-                test: /\.mp3$/,
-                loader: 'url-loader'
-            }
+            plugins: [
+                new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /fr/),
+                new webpack.ContextReplacementPlugin(/escodegen/, /^$/),
+                new webpack.DllPlugin({
+                    name: '[name]',
+                    path: path.join(buildDir, '[name].manifest.json'),
+                }),
+                new ManifestPlugin(),
+            ]
+        }
+    }
+
+    return {
+        devtool,
+        devServer,
+        entry: {
+            build: `${srcDir}/js/main.js`,
+        },
+        output: {
+            path: buildDir,
+            pathinfo,
+            filename: jsBundleFile
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    loader: 'babel-loader?cacheDirectory'
+                },
+                {
+                    test: /.json$/,
+                    loader: 'json-loader'
+                },
+                {
+                    test: /\.(jpe?g|png|gif)$/i,
+                    loader: 'url-loader'
+                },
+                {
+                    test: /\.css$/,
+                    use: ExtractTextPlugin.extract({
+                        fallback: 'style-loader',
+                        use: 'css-loader'
+                    })
+                },
+                {
+                    test: /\.less$/,
+                    use: ExtractTextPlugin.extract({
+                        fallback: 'style-loader',
+                        use: [
+                            'css-loader',
+                            'less-loader'
+                        ]
+                    })
+                },
+                // custom fonts
+                {
+                    test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+                    loader: 'url-loader'
+                },
+                // audio
+                {
+                    test: /\.mp3$/,
+                    loader: 'url-loader'
+                }
+            ]
+        },
+        plugins: [
+            new ExtractTextPlugin({
+                filename: styleBundleFile,
+                allChunks: true
+            }),
+            new webpack.DllReferencePlugin({
+                manifest: require(`${buildDir}/vendors.manifest.json`)
+            }),
+            new ManifestPlugin({
+                cache: require(`${buildDir}/manifest.json`)
+            }),
+            new webpack.ProvidePlugin({
+                Tether: 'tether',
+            }),
         ]
     }
 }
