dfbedf1458d4b1c68dbde3096897f6ca4abd5b64
Jean-Elie <je.barjonet@gmail.com>
2017-07-24 18:56:37 -0700

    template var regex supports double braces

--- src/pages/common/utils/template.js
+++ src/pages/common/utils/template.js
@@ -1,6 +1,6 @@
 import _get from 'lodash/get'
 
-export const templateRegex = /{([a-zA-Z0-9.\[\]"'_]+)}/g
+export const templateRegex = /{\{?([a-zA-Z0-9.\[\]"'_]+)}}?/g
 
 // render a template like: `Order {self.id}` to `Order 37337`
 export const renderTemplate = (body, context = {}) => {
