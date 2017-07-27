394817a011cbdb0d3489181a7365952fa26e2fce
MartinLatrille <martinlatrille@live.fr>
2017-07-26 17:42:44 -0700

    Messenger - Various fixes (#2273)
    * fix case where we can't fetch the message; add facebook-messenger in channel filter option in views
    
    * handle fallback attachments
    
    * dont add filter channel on facebook view
    
    * better logging in notify
    
    * fix schema for fallback and add tests
    
    * added test for no match mid

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,5 +1,6 @@
 export const AVAILABLE_HTTP_METHODS = ['GET', 'POST', 'PUT', 'DELETE'] // keep uppercase strings
 export const TICKET_STATUSES = ['open', 'new', 'closed']
-export const TICKET_CHANNELS = ['email', 'phone', 'sms', 'chat', 'twitter', 'facebook', 'api']
+export const TICKET_CHANNELS = ['email', 'phone', 'sms', 'chat', 'twitter', 'facebook', 'facebook-messenger', 'api']
 export const TICKET_VIA = TICKET_CHANNELS.concat(['form', 'helpdesk', 'app', 'rule'])
-export const TICKET_ANSWERABLE_SOURCE_TYPES = ['email', 'chat', 'facebook-post', 'facebook-comment', 'facebook-message']
+export const TICKET_ANSWERABLE_SOURCE_TYPES = ['email', 'chat', 'facebook-post', 'facebook-comment', 'facebook-message',
+    'facebook-messenger']
