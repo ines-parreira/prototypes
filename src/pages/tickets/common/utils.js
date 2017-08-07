3f3fdc6ebdd78de4fe205daf3c883369b99d511f
Jean-Elie <je.barjonet@gmail.com>
2017-08-07 15:38:34 -0700

    shrink email addresses on display if too long

--- src/pages/tickets/common/utils.js
+++ src/pages/tickets/common/utils.js
@@ -4,12 +4,17 @@ import {getActionTemplate} from './../../../utils'
 
 export function displayUserNameFromSource(user, sourceType) {
     const valueProp = getValuePropFromSourceType(sourceType)
-    const value = user[valueProp]
+    let value = user[valueProp] || ''
 
     let label = user.name || value
 
     // if is email channel and has a name, show the address next to the name
     if (sourceType === 'email') {
+        // shrink email address if too long
+        if (value.length > 45) {
+            value = `${value.slice(0, 20)}[...]${value.slice(value.length - 20)}`
+        }
+
         if (user.name) {
             label = `${user.name} (${value})`
         } else {
