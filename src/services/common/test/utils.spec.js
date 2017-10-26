a57787bd239b41de778cb5a052bcfaf8caeec32d
Ionuț Colceriu <contact@ghinda.net>
2017-10-26 16:47:46 -0700

    Port checkbox-focus + hotkeys fixes to the new master branch. (#2657)
    The old fix was overwritten by a merge of a different branch.

--- src/services/common/test/utils.spec.js
+++ src/services/common/test/utils.spec.js
@@ -2,21 +2,37 @@ import * as utils from '../utils'
 
 describe('Services common utils', () => {
     describe('is editable', () => {
-        const input = document.createElement('input')
-        const select = document.createElement('select')
-        const textarea = document.createElement('textarea')
+        let input
+        beforeEach(() => {
+            input = document.createElement('input')
+        })
 
-        const notEditable = document.createElement('div')
-
-        it('input', () => {
+        it('default input', () => {
             expect(utils.isEditable(input)).toBe(true)
         })
 
+        it('text input', () => {
+            input.type = 'text'
+            expect(utils.isEditable(input)).toBe(true)
+        })
+
+        it('checkbox', () => {
+            input.type = 'checkbox'
+            expect(utils.isEditable(input)).toBe(false)
+        })
+
+        it('radio', () => {
+            input.type = 'radio'
+            expect(utils.isEditable(input)).toBe(false)
+        })
+
         it('select', () => {
+            const select = document.createElement('select')
             expect(utils.isEditable(select)).toBe(true)
         })
 
         it('textarea', () => {
+            const textarea = document.createElement('textarea')
             expect(utils.isEditable(textarea)).toBe(true)
         })
 
@@ -24,6 +40,7 @@ describe('Services common utils', () => {
         // jsdom does not support it.
 
         it('not editable', () => {
+            const notEditable = document.createElement('div')
             expect(utils.isEditable(notEditable)).toBe(false)
         })
     })
