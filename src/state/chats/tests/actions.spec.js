450348aae9322ed650d63ff1bc4228357df39dbe
Louis Barranqueiro <LouisBarranqueiro@users.noreply.github.com>
2018-01-29 20:37:20 +0100

    Chat - Chats via websocket (#2940)
    * remove chat polling
    
    * front-end: update chats via websocket
    
    * rename `activity` reducer as `chats` reducer
    
    * back-end: send updates of chat tickets
    
    * back-end: add `getattr_in` util function
    
    * front-end: add action - markChatTicketAsRead
    
    * rename chat events
    
    * front-end: refresh chat tickets when the current user update his availability for chats
    
    * front-end: update snapshots
    
    * front-end: update the polling manager
    
    * back-end: fix tests
    
    * improvements
    
    * fix tests
    
    * lint code
    
    * code review
    
    * back-end: add new user util functions
    
    * back-end: fix for new message
    
    * back-end: update due to rebase
    
    * improvements
    
    * add tests for `send_chat_ticket` function
    
    * add tests for chat actions
    
    * add tests for chat reducers
    
    * add tests for current user actions
    
    * add tests for object util functions and ticket util functions
    
    * add tests for new user util functions
    
    * fix snap
    
    * lint code
    
    * improvements
    
    * fix activity tests
    
    * improvements
    
    * fix bug: sort and limit recent chats
    
    * back-end: do not send notifications when old ticket is not provided
    
    * front-end: sort and limit recent chats
    
    * front-end: sort and limit recent chats
    
    * back-end: remove facebook message from activity
    
    * front-end: fix bug when active/deactivate available for chat
    
    * remove ticket util functions due to rebase
    
    * back-end: fix facebook tests
    
    * back-end: fix bug
    
    * back-end: fix unread chats
    
    * back-end: improve code quality
    
    * front-end: fetch new chats in case we did'nt fetch them all
    
    * back-end: increase number of chats returned by the chats endpoint
    
    * create a browser notifications service
    
    * update throttling of browser notifications

---
+++ src/state/chats/tests/actions.spec.js
@@ -0,0 +1,91 @@
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+import axios from 'axios'
+import MockAdapter from 'axios-mock-adapter'
+import * as actions from '../actions'
+import {initialState} from '../reducers'
+import browserNotification from 'push.js'
+
+const middlewares = [thunk]
+const mockStore = configureMockStore(middlewares)
+
+jest.mock('../../notifications/actions', () => {
+    return {
+        notify: jest.fn(() => (args) => args),
+    }
+})
+
+describe('actions', () => {
+    let store
+    let mockServer
+
+    beforeEach(() => {
+        store = mockStore({currentAccount: initialState})
+        mockServer = new MockAdapter(axios)
+    })
+
+    describe('chats', () => {
+        describe('fetchChats()', () => {
+            it('should fetch chats', () => {
+                const chats = {
+                    tickets: [{id: 123}, {id: 2}]
+                }
+
+                mockServer.onGet('/api/activity/chats/').reply(200, chats)
+
+                return store.dispatch(actions.fetchChats())
+                    .then(() => expect(store.getActions()).toMatchSnapshot())
+            })
+        })
+
+        describe('setChats()', () => {
+            it('should add chats with notifications', () => {
+                const chats = [{
+                    id: 1,
+                    requester_name: 'Mark Frizeli',
+                    last_message_body_text: 'Hi'
+                }]
+                store.dispatch(actions.setChats(chats))
+                expect(store.getActions()).toMatchSnapshot()
+                expect(browserNotification.getAll()).toMatchSnapshot()
+            })
+        })
+
+        describe('addChat()', () => {
+            beforeEach(() => {
+                browserNotification.clear()
+            })
+
+            const chat = {
+                id: 1,
+                requester_name: 'Mark Frizeli',
+                last_message_body_text: 'Hi'
+            }
+
+            it('should add chat with notifications', () => {
+                store.dispatch(actions.addChat(chat, true))
+                expect(store.getActions()).toMatchSnapshot()
+                expect(browserNotification.getAll()).toMatchSnapshot()
+            })
+
+            it('should add chat without notifications', () => {
+                store.dispatch(actions.addChat(chat, false))
+                expect(store.getActions()).toMatchSnapshot()
+                expect(browserNotification.getAll()).toMatchSnapshot()
+            })
+        })
+
+        describe('removeChat()', () => {
+            it('should remove chat', () => {
+                expect(actions.removeChat(12)).toMatchSnapshot()
+            })
+        })
+
+        describe('markChatAsRead()', () => {
+            it('should mark chat as read', () => {
+                expect(actions.markChatAsRead(12)).toMatchSnapshot()
+            })
+        })
+    })
+
+})
