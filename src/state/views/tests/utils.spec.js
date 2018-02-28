10f5a0e47c2b39510ea8019f8281079d117f858a
Louis Barranqueiro <louis.barranqueiro@gmail.com>
2018-02-28 15:54:08 -0800

    fix random failing tests

--- src/state/views/tests/utils.spec.js
+++ src/state/views/tests/utils.spec.js
@@ -26,7 +26,7 @@ describe('utils', () => {
 
                 const beforeGetDt = moment.utc()
                 const views = utils.recentViewsStorage.get()
-                const now = moment.utc().add(5, 's')
+                const now = moment.utc().add(10, 's')
 
                 expect(Object.keys(views)).toEqual(['1', '2'])
 
