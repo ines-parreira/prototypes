f971f00d1254ef362e13f1c6dcd4e2da403c50a2
Louis Barranqueiro <LouisBarranqueiro@users.noreply.github.com>
2018-03-08 16:32:11 -0800

    Fix fallbacks of browser notifications (#3088)
    * fix notification fallbacks
    
    * only accept strings

--- src/services/browserNotification.js
+++ src/services/browserNotification.js
@@ -1,22 +1,44 @@
+// @flow
 import notification from 'push.js'
 import {browserHistory} from 'react-router'
 import _throttle from 'lodash/throttle'
+import _isString from 'lodash/isString'
 
 const icon = `${window.GORGIAS_ASSETS_URL || ''}/static/private/img/icons/logo.png`
+// $FlowFixMe
 const sound = new Audio(require('../../audio/notification.mp3'))
 sound.load()
 
+
+type NotificationType = {
+    title?: string,
+    body?: string,
+    ticketId?: number | string
+}
+
 class BrowserNotification {
-    newMessage = _throttle(({title = 'Gorgias', body = 'You received an answer', ticketId}) => {
+    newMessage = _throttle((notif: NotificationType) => {
+        if (!notif) {
+            notif = {}
+        }
+
+        if (!_isString(notif.title) || !notif.title) {
+            notif.title = 'Gorgias'
+        }
+
+        if (!_isString(notif.body) || !notif.body) {
+            notif.body = 'You received an answer'
+        }
+
         sound.play()
-        notification.create(title, {
-            body,
+        notification.create(notif.title, {
+            body: notif.body,
             icon,
             timeout: 5000,
             onClick: function() {
                 // go to the ticket
-                if (ticketId) {
-                    browserHistory.push(`/app/ticket/${ticketId}`)
+                if (notif.ticketId) {
+                    browserHistory.push(`/app/ticket/${notif.ticketId}`)
                 }
                 window.focus()
                 this.close()
--- src/services/tests/browserNotification.spec.js
+++ src/services/tests/browserNotification.spec.js
@@ -4,6 +4,33 @@ import PushJS from 'push.js'
 describe('services', () => {
     describe('browserNotification', () => {
         describe('newMessage()', () => {
+            beforeEach(() => {
+                PushJS.clear()
+                browserNotification.newMessage.cancel()
+            })
+
+            it('should create a browser notification with default values (empty)', () => {
+                browserNotification.newMessage()
+                expect(PushJS.getAll()).toMatchSnapshot()
+            })
+
+            it('should create a browser notification with default values (null values)', () => {
+                browserNotification.newMessage({
+                    title: null,
+                    body: null,
+                    ticketId: null
+                })
+                expect(PushJS.getAll()).toMatchSnapshot()
+            })
+
+            it('should create a browser notification with default values (invalid values)', () => {
+                browserNotification.newMessage({
+                    title: 1234,
+                    body: 1234
+                })
+                expect(PushJS.getAll()).toMatchSnapshot()
+            })
+
             it('should create a browser notification', () => {
                 browserNotification.newMessage({
                     title: 'title',
@@ -11,7 +38,6 @@ describe('services', () => {
                     ticketId: 12
                 })
                 expect(PushJS.getAll()).toMatchSnapshot()
-                PushJS.clear()
             })
         })
     })
