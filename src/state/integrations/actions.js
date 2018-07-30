830defbdda0745c890849da2f455d4c9fc52e354
MartinLatrille <martinlatrille@live.fr>
2018-07-30 20:18:08 +0200

    Chat - Use Fastly for chat config (#3542)
    * purge fastly cache after uploading new chat script
    
    * added FASTLY_CONFIG_PURGE_TOKEN in secrets for production usage
    
    * dont purge profile pictures
    
    * add test
    
    * add test when purge fails
    
    * remove 5mn message
    
    * update snaps
    
    * add a max_retry and a timeout to the purge request
    
    * put fastly purge args variables next to the function

--- src/state/integrations/actions.js
+++ src/state/integrations/actions.js
@@ -107,8 +107,7 @@ export function onUpdateSuccess(dispatch: dispatchType, resp: {type: string}, no
     let message = 'Integration successfully updated'
 
     if (resp.type === 'smooch_inside' && didInvalidateCache) {
-        message = 'Integration successfully updated. It may take up to 5 minutes for the changes to reflect on your ' +
-            'website.'
+        message = 'Integration successfully updated.'
     }
 
     return dispatch(notify({
