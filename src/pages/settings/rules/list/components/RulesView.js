361da9cc872298c79a1aa7d6c36e080bd73f3a66
Martin Latrille <martin@gorgias.io>
2019-07-09 15:20:50 -0700

    fix(c/rules): fix doc link in rules list

--- src/pages/settings/rules/list/components/RulesView.js
+++ src/pages/settings/rules/list/components/RulesView.js
@@ -146,7 +146,7 @@ export default class RulesView extends React.Component<Props, State> {
                         <p>
                             Learn more about how to setup rules{' '}
                             <a
-                                href="https://docs.gorgias.io/rules/rules"
+                                href="https://docs.gorgias.io/rules-auto/rules"
                                 rel="noopener noreferrer"
                                 target="_blank"
                             >
