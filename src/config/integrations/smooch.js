3789cb5750c786ca8f42e6964748f8b1baf5b942
MartinLatrille <martin.latrille@outlook.com>
2020-06-03 09:31:06 -0700

    feat(i/chat): add norwegian language (#5843)
    Co-authored-by: Martin Latrille <martin@gorgias.io>

--- src/config/integrations/smooch.js
+++ src/config/integrations/smooch.js
@@ -1,9 +1,13 @@
 import {fromJS} from 'immutable'
 
 import {
-    DANISH_LANGUAGE, DUTCH_LANGUAGE,
+    DANISH_LANGUAGE,
+    DUTCH_LANGUAGE,
     ENGLISH_US_LANGUAGE,
-    FRENCH_LANGUAGE, GERMAN_LANGUAGE, ITALIAN_LANGUAGE,
+    FRENCH_LANGUAGE,
+    GERMAN_LANGUAGE,
+    ITALIAN_LANGUAGE,
+    NORWEGIAN_LANGUAGE,
     SPANISH_LANGUAGE,
     SWEDISH_LANGUAGE
 } from '../../constants/languages'
@@ -19,4 +23,5 @@ export const SMOOCH_LANGUAGE_OPTIONS = fromJS([
     {value: ITALIAN_LANGUAGE, label: 'Italian'},
     {value: DUTCH_LANGUAGE, label: 'Dutch'},
     {value: GERMAN_LANGUAGE, label: 'German'},
+    {value: NORWEGIAN_LANGUAGE, label: 'Norwegian'},
 ])
--- src/config/integrations/smooch_inside.js
+++ src/config/integrations/smooch_inside.js
@@ -8,7 +8,8 @@ import {
     ITALIAN_LANGUAGE,
     SPANISH_LANGUAGE,
     SWEDISH_LANGUAGE,
-    GERMAN_LANGUAGE
+    GERMAN_LANGUAGE,
+    NORWEGIAN_LANGUAGE
 } from '../../constants/languages'
 
 
@@ -24,6 +25,7 @@ export const SMOOCH_INSIDE_WIDGET_LANGUAGE_OPTIONS = fromJS([
     {value: ITALIAN_LANGUAGE, label: 'Italian'},
     {value: DUTCH_LANGUAGE, label: 'Dutch'},
     {value: GERMAN_LANGUAGE, label: 'German'},
+    {value: NORWEGIAN_LANGUAGE, label: 'Norwegian'},
 ])
 
 export const SMOOCH_INSIDE_WIDGET_TEXTS = require('../../../../../integrations/smooch_inside/texts/widget_texts.json')
