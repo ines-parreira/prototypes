e6031640096bec5f963899e1127f90932bd040ee
Jean Jacobi <12011794+j-jacobi@users.noreply.github.com>
2021-02-05 09:54:16 -0800

    style(c/paywalls): Updating testimonials (#7466)

--- src/config/paywalls.tsx
+++ src/config/paywalls.tsx
@@ -31,16 +31,16 @@ export const paywallConfigs: {
             'As your team grows, free for all doesn’t work anymore. You waste time manually assigning tickets and cherry picking becomes an issue. \nStart using teams to automatically assign tickets to agents.',
         testimonial: {
             author: {
-                name: 'Alyse Nathan',
-                avatar: `${assetsURL}/static/private/img/paywalls/avatars/alyse-nathan.png`,
-                position: 'Director of customer Service',
+                name: 'Amir Mot',
+                avatar: `${assetsURL}/static/private/img/paywalls/avatars/amir-mot.png`,
+                position: 'CEO',
                 company: {
-                    name: 'Steve Madden',
-                    href: 'https://www.stevemadden.com/',
+                    name: 'Bradza',
+                    href: 'https://www.bradza.com/',
                 },
             },
             text:
-                'With Auto-assign, tickets are automatically routed to the right person, based on tags and brand. This way, we respond to customers much faster.',
+                'With Auto-assign, we have been able to utilize rules to assign tickets to certain teams based on the ticket parameters. It has saved us time instead of having our lead agents manually assigning tickets to teams based on the ticket request type.',
         },
         preview: `${assetsURL}/static/private/img/paywalls/screens/auto-assignment.png`,
     },
@@ -50,16 +50,16 @@ export const paywallConfigs: {
             'Send surveys to customers after your team has assisted them to monitor how well your support team is doing.',
         testimonial: {
             author: {
-                name: 'Jonathan Ward',
-                avatar: `${assetsURL}/static/private/img/paywalls/avatars/jonathan-ward.png`,
-                position: 'Customer service lead',
+                name: 'Katelyn Chaffee',
+                avatar: `${assetsURL}/static/private/img/paywalls/avatars/katelyn-chaffee.png`,
+                position: 'Customer Experience Manager',
                 company: {
-                    name: 'GrooveLife',
-                    href: 'https://groovelife.com/',
+                    name: 'jane iredale',
+                    href: 'https://janeiredale.com/',
                 },
             },
             text:
-                'Satisfaction surveys allows us to track the individual performance of each agent and to track the quality of our customer service.',
+                'We recognize the importance of above and beyond customer service. The satisfaction survey ensures we’re staying on track with our service expectations, and also gives the team the pick-me-up kudos they deserve!',
         },
         preview: `${assetsURL}/static/private/img/paywalls/screens/satisfaction-surveys.png`,
     },
@@ -67,19 +67,6 @@ export const paywallConfigs: {
         header: 'Track revenue from support',
         description:
             'Uncover upsell opportunities, track revenue generated per agents to compensate your team based on actual sales.',
-        testimonial: {
-            author: {
-                name: 'Jonathan Ward',
-                avatar: `${assetsURL}/static/private/img/paywalls/avatars/jonathan-ward.png`,
-                position: 'Customer service lead',
-                company: {
-                    name: 'GrooveLife',
-                    href: 'https://groovelife.com/',
-                },
-            },
-            text:
-                'At Groovelife, our agents upsell customers when they start a return or exchange. We’ve been able to offset a third of the customer service team budget.',
-        },
         preview: `${assetsURL}/static/private/img/paywalls/screens/revenue-statistics.png`,
     },
     [AccountFeatures.Teams]: {
@@ -111,16 +98,16 @@ export const paywallConfigs: {
             'Drive revenue through proactive customer engagement with Chat campaigns.',
         testimonial: {
             author: {
-                name: 'Alyse Nathan',
-                avatar: `${assetsURL}/static/private/img/paywalls/avatars/alyse-nathan.png`,
-                position: 'Director of customer Service',
+                name: 'Ashley Gonzalez',
+                avatar: `${assetsURL}/static/private/img/paywalls/avatars/ashley-gonzalez.png`,
+                position: 'Community Manager',
                 company: {
-                    name: 'Steve Madden',
-                    href: 'https://www.stevemadden.com/',
+                    name: 'Posh Peanut',
+                    href: 'https://poshpeanut.com/',
                 },
             },
             text:
-                'With Chat campaigns, we’ve increased out website conversation by 5% and identified the top objections customers have when they see a product.',
+                'Chat Campaigns have made it easy for us to reach our customers when they need help the most. We saw 4X the chat tickets after implementing a couple of chat campaigns.',
         },
         preview: `${assetsURL}/static/private/img/paywalls/screens/chat-campaigns.png`,
     },
