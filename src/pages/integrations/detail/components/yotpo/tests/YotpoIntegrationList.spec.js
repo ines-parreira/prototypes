85746cb55ba072a5a1950a30b0cfb3359ad5c7a2
dawalt <walterdangalante@gmail.com>
2021-04-23 15:59:40 +0200

    tests(i/yotpo): add tests to integration list component (#7895)
    Co-authored-by: Walter Danilo Galante <walter@gorgias.com>

---
+++ src/pages/integrations/detail/components/yotpo/tests/YotpoIntegrationList.spec.js
@@ -0,0 +1,115 @@
+import React from 'react'
+import {fromJS} from 'immutable'
+import {shallow} from 'enzyme'
+
+import YotpoIntegrationList from '../YotpoIntegrationList.tsx'
+import {
+    YOTPO_INTEGRATION_TYPE,
+    SMILE_INTEGRATION_TYPE,
+    SUCCESS_AUTHENTICATION_STATUS,
+} from '../../../../../../constants/integration.ts'
+
+describe('<YotpoIntegrationList/>', () => {
+    beforeEach(() => {
+        jest.resetAllMocks()
+    })
+
+    describe('render()', () => {
+        it('should render the Yotpo integration', () => {
+            const component = shallow(
+                <YotpoIntegrationList
+                    integrations={fromJS([
+                        {
+                            type: YOTPO_INTEGRATION_TYPE,
+                            id: 1,
+                            name: 'Yotpo',
+                            meta: {
+                                oauth: {status: SUCCESS_AUTHENTICATION_STATUS},
+                                import_state: {is_over: true},
+                            },
+                        },
+                    ])}
+                    loading={fromJS({})}
+                    redirectUri={
+                        'https://reviews.yotpo.com/#/app_market_authorization?app_market_mode&application_id=test'
+                    }
+                />
+            )
+
+            expect(component).toMatchSnapshot()
+        })
+
+        it('should render a loader because the integration is loading', () => {
+            const component = shallow(
+                <YotpoIntegrationList
+                    integrations={fromJS([
+                        {
+                            type: YOTPO_INTEGRATION_TYPE,
+                            id: 1,
+                            name: 'Yotpo',
+                            meta: {
+                                oauth: {status: SUCCESS_AUTHENTICATION_STATUS},
+                                import_state: {is_over: false},
+                            },
+                        },
+                    ])}
+                    loading={fromJS({updateIntegration: true})}
+                    redirectUri={
+                        'https://reviews.yotpo.com/#/app_market_authorization?app_market_mode&application_id=test'
+                    }
+                />
+            )
+
+            expect(component).toMatchSnapshot()
+        })
+
+        it('should not render an integration cause the one passed is not a Yotpo integration', () => {
+            const component = shallow(
+                <YotpoIntegrationList
+                    integrations={fromJS([
+                        {
+                            type: SMILE_INTEGRATION_TYPE,
+                            id: 1,
+                            name: 'Invalid',
+                            meta: {
+                                oauth: {status: SUCCESS_AUTHENTICATION_STATUS},
+                                import_state: {is_over: false},
+                            },
+                        },
+                    ])}
+                    loading={fromJS({})}
+                    redirectUri={
+                        'https://reviews.yotpo.com/#/app_market_authorization?app_market_mode&application_id=test'
+                    }
+                />
+            )
+
+            expect(component).toMatchSnapshot()
+        })
+
+        it('should render a deactivated integration that can be reenabled', () => {
+            const component = shallow(
+                <YotpoIntegrationList
+                    integrations={fromJS([
+                        {
+                            type: YOTPO_INTEGRATION_TYPE,
+                            id: 1,
+                            name: 'Yotpo',
+                            meta: {
+                                oauth: {status: SUCCESS_AUTHENTICATION_STATUS},
+                                import_state: {is_over: true},
+                            },
+                            deactivated_datetime: '2018-01-01 10:12',
+                        },
+                    ])}
+                    loading={fromJS({})}
+                    redirectUri={
+                        'https://reviews.yotpo.com/#/app_market_authorization?app_market_mode&application_id=test'
+                    }
+                />
+            )
+
+            expect(component).toMatchSnapshot()
+        })
+    })
+})
