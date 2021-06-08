9f098ef110dd971ec46dcd11c7563c0ba9d0e812
Xuan-thi Nguyen <xuan-thi@gorgias.com>
2021-06-08 15:07:16 +0200

    fix(Billing): update tooltip of the legacy plan label (#8285)
    * fix(LegacyPlanBadge): update text and css
    
    * fix(LegacyPlanBadge): remove wrapper and make it a cursor on hover
    
    * test: update snapshots

--- src/pages/common/components/LegacyPlanBadge.tsx
+++ src/pages/common/components/LegacyPlanBadge.tsx
@@ -2,6 +2,7 @@ import React, {useCallback, useState} from 'react'
 import {Badge} from 'reactstrap'
 
 import Tooltip from './Tooltip'
+import css from './LegacyPlanBadge.less'
 
 export default function LegacyPlanBadge() {
     const [tagLegacyRef, setTagLegacyRef] = useState<HTMLDivElement | null>(
@@ -14,19 +15,22 @@ export default function LegacyPlanBadge() {
     }, [])
 
     return (
-        <>
-            <div className="d-inline-flex" ref={handleRef}>
-                <Badge className="d-inline-flex flex-row" color="danger">
-                    <i className="material-icons">warning</i>
-                    <div className="ml-1">LEGACY PLAN</div>
-                </Badge>
-                {tagLegacyRef && (
-                    <Tooltip target={tagLegacyRef} placement="top-start">
-                        Plan no longer supported. Please update to a more recent
-                        option or contact support for questions.
-                    </Tooltip>
-                )}
-            </div>
-        </>
+        <div className="d-inline-flex cursor-pointer" ref={handleRef}>
+            <Badge className="d-inline-flex flex-row" color="danger">
+                <i className="material-icons">warning</i>
+                <div className="ml-1">LEGACY PLAN</div>
+            </Badge>
+            {tagLegacyRef && (
+                <Tooltip
+                    target={tagLegacyRef}
+                    placement="top-start"
+                    innerClassName={css.tooltip}
+                >
+                    This subscription plan is no longer supported, note that new
+                    features will not be available to legacy plans. Please
+                    update to a more recent option.
+                </Tooltip>
+            )}
+        </div>
     )
 }
