4e18f34f6232f1aab136fda7b5a2598604f8c7b2
NikolaMijovic93 <56353185+NikolaMijovic93@users.noreply.github.com>
2021-10-06 11:06:37 +0200

    chore(ui): merge ESLint configs (#9203)
    * chore(eslint): merge linter configs
    
    * chore: remove deleted file references
    
    * chore: remove .flowconfig reference
    
    * refactor: review feedback
    
    * refactor: review feedback 2

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,106 @@
+const commonRules = {
+    'consistent-return': ['off'],
+    'jsx-a11y/no-autofocus': ['off'],
+    'jsx-a11y/tabindex-no-positive': ['off'],
+    'jsx-a11y/click-events-have-key-events': ['off'],
+    'jsx-a11y/onclick-has-role': ['off'],
+    'jsx-a11y/label-has-for': ['off'],
+    'jsx-a11y/no-onchange': ['off'],
+    'jsx-a11y/anchor-is-valid': ['off'],
+    'jsx-a11y/no-static-element-interactions': ['off'],
+    'jsx-a11y/no-noninteractive-tabindex': ['off'],
+    'jsx-a11y/no-noninteractive-element-interactions': ['off'],
+    'react/display-name': ['off'],
+    'react/no-find-dom-node': ['off'],
+    'react/no-deprecated': ['off'],
+}
+
+const commonConfigs = [
+    require.resolve('@gorgias/javascript-shared-config/eslint-base'),
+    require.resolve('@gorgias/javascript-shared-config/eslint-react'),
+    'plugin:prettier/recommended',
+]
+
 module.exports = {
+    rules: {
+        ...commonRules,
+        'no-prototype-builtins': ['off'],
+        'no-async-promise-executor': ['off'],
+    },
+    extends: commonConfigs,
+    globals: {
+        $: false,
+        FB: false,
+        Stripe: false,
+        amplitude: false,
+    },
+    plugins: ['prettier'],
     overrides: [
         {
             files: ['*.ts', '*.tsx'],
-            extends: ['.eslintrc.typescript.js'],
-        },
-        {
-            files: ['*.js', '*.jsx'],
-            extends: ['.eslintrc.javascript.js'],
+            rules: {
+                ...commonRules,
+                'no-unused-vars': 'off',
+                'import/default': ['off'],
+                'import/namespace': ['off'],
+                'import/no-named-as-default-member': ['off'],
+                'import/no-named-as-default': ['off'],
+                '@typescript-eslint/no-unsafe-assignment': ['off'],
+                '@typescript-eslint/no-floating-promises': [
+                    2,
+                    {
+                        ignoreVoid: true,
+                    },
+                ],
+                '@typescript-eslint/ban-ts-comment': ['off'],
+                '@typescript-eslint/no-unused-vars': ['error'],
+                'import/no-unresolved': [2, {ignore: ['estree']}],
+                'no-restricted-imports': [
+                    'error',
+                    {
+                        paths: [
+                            {
+                                name: 'react-redux',
+                                importNames: ['useDispatch'],
+                                message:
+                                    'Please use useAppDispatch from /hooks/useAppDispatch instead.',
+                            },
+                        ],
+                    },
+                ],
+            },
+            extends: [
+                ...commonConfigs,
+                'plugin:@typescript-eslint/recommended',
+                'plugin:@typescript-eslint/recommended-requiring-type-checking',
+                'plugin:import/typescript',
+                'prettier/@typescript-eslint',
+            ],
+            plugins: ['prettier', '@typescript-eslint'],
+            settings: {
+                'import/parsers': {
+                    '@typescript-eslint/parser': ['.ts', '.tsx'],
+                },
+                'import/ignore': [/\*.js$/],
+            },
+            parser: '@typescript-eslint/parser',
+            parserOptions: {
+                sourceType: 'module',
+                ecmaFeatures: {
+                    jsx: true,
+                    experimentalObjectRestSpread: true,
+                    legacyDecorators: true,
+                },
+                project: './tsconfig.json',
+            },
+            overrides: [
+                {
+                    files: ['**/*.spec.ts', '**/*.spec.tsx'],
+                    rules: {
+                        '@typescript-eslint/unbound-method': ['off'],
+                    },
+                },
+            ],
         },
     ],
 }
