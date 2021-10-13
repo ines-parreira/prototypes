b4cc8cf8e9ae8dcbf8c0e24107d3a93a973c4e8d
Clément Sirieix <clem.sirieix@gmail.com>
2021-10-13 10:09:19 +0200

    Integrate Zero Height API to our colour system (#9254)
    * chore(ui): link color design tokens
    
    * enhancement(ui): improve handling of tokens
    
    * chore(ui): update naming
    
    * fix(ui): fix prettier for tokens

---
+++ scripts/generateDesignTokens.js
@@ -0,0 +1,30 @@
+const path = require('path')
+
+const _ = require('lodash')
+
+const outputPath = path.join(process.cwd(), 'g/static/private/css/new/')
+
+const StyleDictionary = require('style-dictionary').extend({
+    source: [
+        `${path.join(process.cwd(), 'g/static/private/tokens')}/**/*.json`,
+    ],
+    platforms: {
+        less: {
+            transformGroup: 'less',
+            buildPath: outputPath,
+            files: [
+                {
+                    destination: 'colorTokens.less',
+                    format: 'less/variables',
+                },
+            ],
+        },
+    },
+})
+
+StyleDictionary.registerTransform({
+    name: 'name/cti/kebab',
+    type: 'name',
+    transformer: (prop) => _.kebabCase(`${prop.attributes.type}-${prop.name}`),
+})
+StyleDictionary.buildAllPlatforms()
