4a5964a38a4e5621a509bc309d12f1786f0c2e94
Arthur Fauquenot <arthurfauquenot@gmail.com>
2021-11-08 17:35:11 +0100

    feat(helpcenter): remove unused editor files (#9568)

--- src/pages/settings/helpCenter/components/articles/HelpCenterEditor/components/HelpCenterEditorToolbarSeparator.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-import css from './HelpCenterEditorToolbar.less'
-
-export const HelpCenterEditorToolbarSeparator = () => {
-    return <div className={css.separator} />
-}
