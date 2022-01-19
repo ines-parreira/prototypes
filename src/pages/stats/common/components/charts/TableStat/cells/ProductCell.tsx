27c26b2eee2246096355ccc13541368b9ca0cb8c
Guillaume Fay <guillaume_fay@hotmail.com>
2022-01-19 19:33:16 +0100

    fix(stats): check if the product image is found or display the placeh… (#9980)

--- src/pages/stats/common/components/charts/TableStat/cells/ProductCell.tsx
+++ src/pages/stats/common/components/charts/TableStat/cells/ProductCell.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
 
 import imagePlaceholder from 'assets/img/icons/insert-photo.svg'
 
@@ -9,27 +10,45 @@ type Props = {
     imageUrl: string | null
 }
 
-const ProductCell = ({name, imageUrl}: Props) => (
-    <div className={css.container}>
-        {imageUrl ? (
-            <div
-                className={css['product-image']}
-                style={{
-                    backgroundImage: `url('${imageUrl}')`,
-                }}
-            />
-        ) : (
-            <div className={css['product-image']}>
-                <img
-                    src={imagePlaceholder}
-                    alt="Placeholder"
-                    className={css['placeholder']}
-                />
-            </div>
-        )}
+const ProductCell = ({name, imageUrl}: Props) => {
+    const [isImageNotFound, setIsImageNotFound] = useState(!imageUrl)
 
-        <h5 className={css['product-name']}>{name}</h5>
-    </div>
-)
+    useEffect(() => {
+        const checkImageFound = async (url: string) => {
+            try {
+                await axios.head(url)
+            } catch (err) {
+                setIsImageNotFound(true)
+            }
+        }
+
+        if (imageUrl) {
+            void checkImageFound(imageUrl)
+        }
+    }, [imageUrl])
+
+    return (
+        <div className={css.container}>
+            {!imageUrl || isImageNotFound ? (
+                <div className={css['product-image']}>
+                    <img
+                        src={imagePlaceholder}
+                        alt="Placeholder"
+                        className={css['placeholder']}
+                        data-testid="placeholder"
+                    />
+                </div>
+            ) : (
+                <div
+                    className={css['product-image']}
+                    style={{
+                        backgroundImage: `url('${imageUrl}')`,
+                    }}
+                />
+            )}
+            <h5 className={css['product-name']}>{name}</h5>
+        </div>
+    )
+}
 
 export default ProductCell
--- src/pages/stats/common/components/charts/TableStat/cells/tests/ProductCell.spec.tsx
+++ src/pages/stats/common/components/charts/TableStat/cells/tests/ProductCell.spec.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
-import {render} from '@testing-library/react'
+import {render, waitFor} from '@testing-library/react'
+import axios from 'axios'
 
 import ProductCell from '../ProductCell'
 
 describe('ProductCell', () => {
+    beforeEach(() => {
+        jest.resetAllMocks()
+    })
+
     it('should render a product cell with image url', () => {
         const {container} = render(
             <ProductCell
@@ -19,5 +24,25 @@ describe('ProductCell', () => {
             <ProductCell name="Sunglasses" imageUrl={null} />
         )
         expect(container.firstChild).toMatchSnapshot()
+    })
+
+    it('should display a placeholder if the check returns a 404 error', async () => {
+        jest.spyOn(axios, 'head').mockRejectedValue({
+            response: {
+                status: 404,
+            },
+        })
+
+        const {getByTestId} = render(
+            <ProductCell
+                name="Sunglasses"
+                imageUrl="https://domain.com/my-image.png"
+            />
+        )
+
+        await waitFor(() => {
+            const placeholder = getByTestId('placeholder')
+            expect(placeholder).toBeDefined()
+        })
     })
 })
