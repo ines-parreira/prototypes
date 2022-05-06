ecd67ccd225896422ce329e051815934527d472f
Manu <manuel@gorgias.com>
2022-05-06 10:44:24 +0200

    fix(infobar): http custom icons not displaying in some case (#11332)

--- src/pages/common/components/infobar/Infobar/InfobarCustomerInfo/InfobarWidgets/widgets/http/index.js
+++ src/pages/common/components/infobar/Infobar/InfobarCustomerInfo/InfobarWidgets/widgets/http/index.js
@@ -2,9 +2,16 @@ import Root from './Root.tsx'
 
 const http = (args) => {
     const path = args.template.get('absolutePath', []).join('.')
-
-    // path must match when there is a root array / list too
-    if (path.match(/integrations\.[0-9]+(\.[^.]+\.\[])?$/)) {
+    /** path must match when there is a root array / list too
+     * OK
+     * ticket.customer.integrations.10
+     * ticket.customer.integrations.10.data
+     * ticket.customer.integrations.10.[data]
+     * KO
+     * ticket.customer.integrations.10.data.[vdxvx]
+     * ticket.customer.integrations.10.data.else
+     */
+    if (path.match(/integrations\.[0-9]+(\.[^.]+(\.\[])?)?$/)) {
         return Root()
     }
 
