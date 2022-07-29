5eeafa150fe6f3e3599af6402d12b7dca48f8799
Xuan-thi Nguyen <xuan-thi@gorgias.com>
2022-07-29 18:20:30 +0200

    fix: check existence of fields before access (#12315)

--- src/pages/tickets/detail/components/TicketMessages/Actions.tsx
+++ src/pages/tickets/detail/components/TicketMessages/Actions.tsx
@@ -94,17 +94,19 @@ export default class Actions extends Component<Props, State> {
         return (
             <ModalBody>
                 <h3>Headers</h3>
-                {Object.keys(action.arguments!.headers!).map((arg, idx) => (
-                    <p key={idx}>
-                        <b>{arg}:</b> {action.arguments!.headers![arg]}
-                    </p>
-                ))}
+                {!!action.arguments?.headers &&
+                    Object.keys(action.arguments.headers).map((arg, idx) => (
+                        <p key={idx}>
+                            <b>{arg}:</b> {action.arguments!.headers![arg]}
+                        </p>
+                    ))}
                 <h3>URL Parameters</h3>
-                {Object.keys(action.arguments!.params!).map((arg, idx) => (
-                    <p key={idx}>
-                        <b>{arg}:</b> {action.arguments!.params![arg]}
-                    </p>
-                ))}
+                {!!action.arguments?.params &&
+                    Object.keys(action.arguments.params).map((arg, idx) => (
+                        <p key={idx}>
+                            <b>{arg}:</b> {action.arguments!.params![arg]}
+                        </p>
+                    ))}
                 {!!action.arguments!.form && contentType === ContentType.Form && (
                     <div className="mt-3">
                         <h3>Form Data</h3>
