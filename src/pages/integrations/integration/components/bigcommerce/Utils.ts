6c90cdf34d2823646466a34eace60f0954de7953
Alex Daineanu <alexdaineanu@users.noreply.github.com>
2022-08-01 19:01:46 +0300

    feat(bigcommerce): set the right app urls depending on the environment (#12320)
    * feat(bigcommerce): set the right app urls depending on the environment
    
    * feat(bigcommerce): change the email for auth fail
    
    * feat(bigcommerce): address code review change requests
    
    * feat(bigcommerce): small improvement for text
    
    * feat(bigcommerce): fix tests
    
    * feat(bigcommerce): Make the email more explicit
    
    * feat(bigcommerce): Address code review change requests

--- src/pages/integrations/integration/components/bigcommerce/Utils.ts
+++ src/pages/integrations/integration/components/bigcommerce/Utils.ts
@@ -1,14 +1,21 @@
 import {isProduction, isStaging} from '../../../../../utils/environment'
 
 export function getConnectUrl() {
-    let connectUrl =
+    const BIGCOMMERCE_MARKETPLACE_BASE_URL =
+        'https://apps.bigcommerce.com/details/'
+    const BIGCOMMERCE_STAGING_APP_ID = '39647'
+    const BIGCOMMERCE_PRODUCTION_APP_ID = '39645'
+    const BIGCOMMERCE_DEVELOPMENT_APP_URL =
         'https://store-pk360c6roo.mybigcommerce.com/manage/marketplace/apps/my-apps'
+    // compute the right url based on the environment
+    let connectUrl = BIGCOMMERCE_DEVELOPMENT_APP_URL
     if (isStaging()) {
-        connectUrl = 'https://apps.bigcommerce.com/details/39647'
+        connectUrl =
+            BIGCOMMERCE_MARKETPLACE_BASE_URL + BIGCOMMERCE_STAGING_APP_ID
     }
     if (isProduction()) {
         connectUrl =
-            'https://store-pk360c6roo.mybigcommerce.com/manage/marketplace/apps/38723' // will replace with published app
+            BIGCOMMERCE_MARKETPLACE_BASE_URL + BIGCOMMERCE_PRODUCTION_APP_ID
     }
     return connectUrl
 }
