9be58c5477ab613144f503ad792473e99d20738f
Matthieu-Gorgias <87850462+Matthieu-Gorgias@users.noreply.github.com>
2022-09-26 10:59:02 +0200

    feat(self-service): remove feature flag self-service-standalone-portal (#12884)

--- src/config/featureFlags.ts
+++ src/config/featureFlags.ts
@@ -2,7 +2,6 @@ export enum FeatureFlagKey {
     SelfServiceArticleRecommendation = 'self-service-article-recommendation',
     EarlyAdopter = 'early-adopter',
     DefaultMacroToSearch = 'default-macro-to-search',
-    SelfServiceStandalonePortal = 'self-service-standalone-portal',
     MacroResponseTextCcBcc = 'macro-response-text-cc-bcc',
     PrefillBestMacro = 'pre-fill-best-macro',
     NewPhoneErrorHandling = 'new-phone-error-handling',
