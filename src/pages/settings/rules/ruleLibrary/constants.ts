744b5a82a840c30a27046cb150f313cebe1f97d9
Botond Dombi <104018159+gorgias-botond-dombi@users.noreply.github.com>
2022-10-07 11:07:30 +0300

    feat(rules): update max rules reached text (#13005)

--- src/pages/settings/rules/ruleLibrary/constants.ts
+++ src/pages/settings/rules/ruleLibrary/constants.ts
@@ -7,5 +7,5 @@ export const InstallationErrorMessage = {
     [InstallationError.NoHelpCenter]:
         'This rule requires an active Help Center.',
     [InstallationError.MaxRulesReached]:
-        'You reached the maximum number of allowed rules. Remove unused rules to proceed.',
+        'You’ve reached the maximum number of rules. Delete an existing rule to install this one.',
 }
