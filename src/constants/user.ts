2cfc8fa0556b28173003e2d63bfede169ba0e3af
teoZait <38578110+teoZait@users.noreply.github.com>
2022-10-07 09:41:46 +0200

    APPCH-2034: expose agents availability status via users endpoint (#12828)

--- src/constants/user.ts
+++ src/constants/user.ts
@@ -7,3 +7,10 @@ export const FACEBOOK_CUSTOMER_CHANNEL_TYPE = 'facebook'
 export const INSTAGRAM_CUSTOMER_CHANNEL_TYPE = 'instagram'
 export const INSTAGRAM_DIRECT_MESSAGE_CUSTOMER_CHANNEL_TYPE =
     'instagram-direct-message'
+
+export const AvailabilityStatusDetailCodeToDescription = {
+    1: 'User has no open tabs',
+    2: 'User availability setting toggle set to offline',
+    3: 'User phone is ringing',
+    4: 'User is currently in a phone call',
+}
