4f294593319b290f414fa746d730ea7b2f85b746
Iustina Dumitru <32546295+iustinadumitru@users.noreply.github.com>
2022-10-17 15:15:01 +0300

    fix(access-management-page): Improve Joining the helpdesk section design (#12157)

--- src/pages/settings/access/Access.tsx
+++ src/pages/settings/access/Access.tsx
@@ -1,13 +1,12 @@
-import React, {FormEvent, useCallback, useEffect, useState} from 'react'
+import React, {FormEvent, useCallback, useState} from 'react'
 import classnames from 'classnames'
 import {connect, ConnectedProps} from 'react-redux'
 import {Button, Container, Form} from 'reactstrap'
 
+import {List} from 'immutable'
 import googleLogo from 'assets/img/integrations/google.svg'
 import microsoftLogo from 'assets/img/integrations/microsoft.svg'
-import DEPRECATED_InputField from 'pages/common/forms/DEPRECATED_InputField'
 import PageHeader from 'pages/common/components/PageHeader'
-import RadioFieldSet from 'pages/common/forms/RadioFieldSet'
 import {submitSetting} from 'state/currentAccount/actions'
 import {
     getAccessSettings,
@@ -21,6 +20,9 @@ import {
 } from 'state/currentAccount/types'
 import TwoFactorAuthenticationEnforcement from 'pages/settings/access/TwoFactorAuthenticationEnforcement'
 import useAppSelector from 'hooks/useAppSelector'
+import ToggleInput from 'pages/common/forms/ToggleInput'
+import DEPRECATED_InputField from 'pages/common/forms/DEPRECATED_InputField'
+import Label from 'pages/common/forms/Label/Label'
 import css from '../settings.less'
 import SsoToggleButton from './SsoToggleButton'
 
@@ -37,7 +39,7 @@ const FORBIDDEN_DOMAINS = ['gmail.com', 'outlook.com']
 
 function splitDomains(domains: string): string[] {
     return domains
-        .split('\n')
+        .split(/[,\n]/)
         .map((val) => val.trim())
         .filter((val) => val.length > 0)
 }
@@ -45,6 +47,9 @@ function splitDomains(domains: string): string[] {
 function validateDomain(domain: string): string | undefined {
     if (FORBIDDEN_DOMAINS.includes(domain)) {
         return 'Generic domain: ' + domain
+    }
+    if (!domain.match(/^[\w\d\-.*]+$/)) {
+        return 'Invalid special character(s). Only “*” and “,” are allowed.'
     }
 }
 
@@ -59,7 +64,9 @@ export const AccessContainer = (props: Props) => {
     const {accountDomain, accessSettings, submitSetting} = props
 
     const allowedDomainsSetting: string[] =
-        accessSettings.getIn(['data', 'allowed_domains']) || []
+        (
+            accessSettings.getIn(['data', 'allowed_domains']) as List<string>
+        )?.toArray() || []
     const signupModeSetting: SignupMode =
         accessSettings.getIn(['data', 'signup_mode']) || SignupMode.Invite
     const googleSsoEnabled: boolean = accessSettings.getIn(
@@ -77,13 +84,12 @@ export const AccessContainer = (props: Props) => {
     const [allowedDomains, setAllowedDomains] = useState(
         allowedDomainsSetting.join('\n')
     )
-    const [domainError, setDomainError] = useState(
-        validateDomains(allowedDomains)
-    )
 
-    useEffect(() => {
-        setDomainError(validateDomains(allowedDomains))
-    }, [allowedDomains])
+    const domainError =
+        validateDomains(allowedDomains) ||
+        (signupMode === SignupMode.AllowedDomains && !allowedDomains
+            ? 'This field is required if Auto-join is enabled.'
+            : null)
 
     const signupLink = `https://${accountDomain}.gorgias.com/signup`
 
@@ -149,6 +155,14 @@ export const AccessContainer = (props: Props) => {
         [saveSettings]
     )
 
+    const toggleSignupModeAllowedDomains = useCallback((val: boolean) => {
+        if (val) {
+            setSignupMode(SignupMode.AllowedDomains)
+        } else {
+            setSignupMode(SignupMode.Invite)
+        }
+    }, [])
+
     const toggle2FAEnforcement = useCallback(
         (val: boolean) => {
             return saveSettings(
@@ -183,28 +197,27 @@ export const AccessContainer = (props: Props) => {
             <Container fluid className={css.pageContainer}>
                 <div className={css.contentWrapper}>
                     <Form onSubmit={handleSubmit}>
-                        <h4 className="mb-2">
-                            <i className="material-icons mr-1">vpn_key</i>
-                            Single Sign-On (SSO)
-                        </h4>
+                        <h4 className="mb-2">Single Sign-On (SSO)</h4>
                         <p>
-                            SSO allows team members to access Gorgias using
-                            their Google or Microsoft 365 accounts. Once you
-                            enable SSO, a Google or Microsoft login button will
-                            be added to the login page.
+                            SSO allows agents to access Gorgias using their
+                            Google or Microsoft accounts. Once you enable a SSO
+                            method, a respective button will appear on the
+                            log-in page.
                         </p>
-                        <SsoToggleButton
-                            id="google"
-                            name="Google"
-                            logo={googleLogo}
-                            value={googleSsoEnabled}
-                            loading={isLoading === LoadingKey.GoogleSSO}
-                            disabled={
-                                !!isLoading &&
-                                isLoading !== LoadingKey.GoogleSSO
-                            }
-                            setValue={toggleGoogleSso}
-                        />
+                        <div className="mb-4">
+                            <SsoToggleButton
+                                id="google"
+                                name="Google"
+                                logo={googleLogo}
+                                value={googleSsoEnabled}
+                                loading={isLoading === LoadingKey.GoogleSSO}
+                                disabled={
+                                    !!isLoading &&
+                                    isLoading !== LoadingKey.GoogleSSO
+                                }
+                                setValue={toggleGoogleSso}
+                            />
+                        </div>
                         <SsoToggleButton
                             id="office365"
                             name="Microsoft 365"
@@ -218,42 +231,54 @@ export const AccessContainer = (props: Props) => {
                             setValue={toggleOffice365Sso}
                         />
 
-                        <h4 className="mt-5 mb-2">
-                            <i className="material-icons mr-1">mail</i>
-                            Joining the helpdesk
-                        </h4>
+                        <h4 className="mt-5 mb-2">Auto-join helpdesk</h4>
+                        <p className="mb-1">
+                            Allows people with email addresses in the approved
+                            email domains to use the link below to sign up.
+                        </p>
                         <p>
-                            If you allow people to join automatically, anyone
-                            with an approved email address can use this link to
-                            confirm their email and sign up:{' '}
                             <a href={signupLink}>{signupLink}</a>
                         </p>
-                        <RadioFieldSet
-                            className="mb-3"
-                            onChange={(value) =>
-                                setSignupMode(value as SignupMode)
-                            }
-                            selectedValue={signupMode}
-                            options={[
-                                {
-                                    value: SignupMode.Invite,
-                                    label: 'Invite people manually',
-                                },
-                                {
-                                    value: SignupMode.AllowedDomains,
-                                    label: 'Let people sign up automatically, and only accept email addresses from specific domains',
-                                },
-                            ]}
-                        />
-                        {signupMode === SignupMode.AllowedDomains && (
+                        <div className="mb-3">
+                            <ToggleInput
+                                isToggled={
+                                    signupMode === SignupMode.AllowedDomains
+                                }
+                                onClick={toggleSignupModeAllowedDomains}
+                            >
+                                Auto-join for approved email domains
+                            </ToggleInput>
+                        </div>
+                        <div className="mb-10">
+                            <Label
+                                htmlFor={'approved-email-domains'}
+                                isRequired
+                                className="mb-2"
+                            >
+                                {'Approved email domains'}
+                            </Label>
                             <DEPRECATED_InputField
+                                id={'approved-email-domains'}
                                 error={domainError}
                                 type="textarea"
-                                help="Ex: domain.com, *.domain.com. Wildcards allowed. Use separate lines for multiple entries."
+                                rows={Math.max(
+                                    allowedDomainsSetting.length + 1,
+                                    1
+                                )}
+                                help={
+                                    !domainError
+                                        ? 'Wildcards allowed (e.g. *domain.com)'
+                                        : null
+                                }
+                                placeholder="domain.com, mydomain.com"
                                 value={allowedDomains}
                                 onChange={setAllowedDomains}
+                                isRequired
+                                readOnly={
+                                    signupMode !== SignupMode.AllowedDomains
+                                }
                             />
-                        )}
+                        </div>
 
                         <TwoFactorAuthenticationEnforcement
                             is2FAEnforced={!!twoFAEnforcedDatetime}
