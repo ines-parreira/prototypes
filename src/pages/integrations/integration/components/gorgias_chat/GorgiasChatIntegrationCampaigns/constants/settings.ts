b84f48e9ea8a131b9078e8917558c001cdfbafd9
Robert Pamfile <robert@gorgias.com>
2022-12-05 10:25:45 +0200

    REV-225 Campaigns - Add delay select (#240)
    * feat: add delay select
    
    * refactor: add tooltip for delay

---
+++ src/pages/integrations/integration/components/gorgias_chat/GorgiasChatIntegrationCampaigns/constants/settings.ts
@@ -0,0 +1,26 @@
+export const CAMPAIGN_DELAY = [
+    {
+        label: '0 seconds',
+        value: 0,
+    },
+    {
+        label: '5 seconds',
+        value: 5000,
+    },
+    {
+        label: '15 seconds',
+        value: 15000,
+    },
+    {
+        label: '30 seconds',
+        value: 30000,
+    },
+    {
+        label: '45 seconds',
+        value: 45000,
+    },
+    {
+        label: '60 seconds',
+        value: 60000,
+    },
+]
