9d5ece7766605f4a3ff8150e27e1042b70b2c44c
Marcel Lefter <marcellefter@outlook.com>
2022-12-07 00:31:49 +0200

    Migrate ViewFilters to a functional component (#230)
    * Migrate ViewFilters to a functional component
    
    * Remove connector from ViewFilters
    
    Co-authored-by: Marcel Lefter <marcel.lefter@ebs-integrator.com>

--- src/pages/common/components/ViewTable/Filters/ViewFilters.tsx
+++ src/pages/common/components/ViewTable/Filters/ViewFilters.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import {connect, ConnectedProps} from 'react-redux'
 import {List, Map} from 'immutable'
 import {
     Expression,
@@ -8,97 +7,85 @@ import {
     ExpressionStatement,
 } from 'estree'
 
-import {getAgents} from '../../../../../state/agents/selectors'
-import {getSchemas} from '../../../../../state/schemas/selectors'
-import {getTeams} from '../../../../../state/teams/selectors'
-import {getActiveView} from '../../../../../state/views/selectors'
+import {getAgents} from 'state/agents/selectors'
+import {getSchemas} from 'state/schemas/selectors'
+import {getTeams} from 'state/teams/selectors'
+import {getActiveView} from 'state/views/selectors'
 import {
     removeFieldFilter,
     updateFieldFilter,
     updateFieldFilterOperator,
-} from '../../../../../state/views/actions'
-import {RootState} from '../../../../../state/types'
+} from 'state/views/actions'
+import useAppSelector from 'hooks/useAppSelector'
+import useAppDispatch from 'hooks/useAppDispatch'
 
 import CallExpression from './CallExpression'
 
-type Props = ConnectedProps<typeof connector>
+export default function ViewFilters() {
+    const dispatch = useAppDispatch()
+    const agents = useAppSelector(getAgents)
+    const schemas = useAppSelector(getSchemas)
+    const teams = useAppSelector(getTeams)
+    const view = useAppSelector(getActiveView)
 
-export class ViewFilters extends React.Component<Props> {
-    removeCondition = (index: number) => {
-        this.props.removeFieldFilter(index)
+    if (!view || !schemas || schemas.isEmpty()) {
+        return null
     }
 
-    updateOperator = (index: number, value: string) => {
-        this.props.updateFieldFilterOperator(index, value)
+    if (
+        !view.get('filters_ast') ||
+        !(view.getIn(['filters_ast', 'body']) as List<any>).size
+    ) {
+        return <p className="text-muted mt-2">No filters selected</p>
     }
 
-    render() {
-        const {view, schemas, agents, teams, updateFieldFilter} = this.props
+    const exp: LogicalExpression = (
+        ((view.get('filters_ast') as Map<any, any>).toJS() as Program)
+            .body[0] as ExpressionStatement
+    ).expression as LogicalExpression
 
-        if (!view || !schemas || schemas.isEmpty()) {
-            return null
+    // counting call expressions so we can delete them later
+    let callExprCounter = 0
+
+    const walk = (node: Expression, parentNode?: LogicalExpression) => {
+        switch (node.type) {
+            case 'CallExpression':
+                return (
+                    <div className="view-filters-item">
+                        <CallExpression
+                            node={node}
+                            parentNode={parentNode!}
+                            view={view}
+                            schemas={schemas}
+                            index={callExprCounter++}
+                            removeCondition={(index) =>
+                                dispatch(removeFieldFilter(index))
+                            }
+                            updateOperator={(index, value) =>
+                                dispatch(
+                                    updateFieldFilterOperator(index, value)
+                                )
+                            }
+                            agents={agents}
+                            teams={teams}
+                            updateFieldFilter={(index, value) =>
+                                dispatch(updateFieldFilter(index, value))
+                            }
+                        />
+                    </div>
+                )
+            case 'LogicalExpression':
+                return (
+                    <div className="view-filters-item">
+                        {walk(node.left, node)}
+
+                        {walk(node.right, node)}
+                    </div>
+                )
+            default:
+                throw Error(`Unknown type: ${node.type}`)
         }
-
-        if (
-            !view.get('filters_ast') ||
-            !(view.getIn(['filters_ast', 'body']) as List<any>).size
-        ) {
-            return <p className="text-muted mt-2">No filters selected</p>
-        }
-
-        const exp: LogicalExpression = (
-            ((view.get('filters_ast') as Map<any, any>).toJS() as Program)
-                .body[0] as ExpressionStatement
-        ).expression as LogicalExpression
-        // counting call expressions so we can delete them later
-        let callExprCounter = 0
-        const walk = (node: Expression, parentNode?: LogicalExpression) => {
-            switch (node.type) {
-                case 'CallExpression':
-                    return (
-                        <div className="view-filters-item">
-                            <CallExpression
-                                node={node}
-                                parentNode={parentNode!}
-                                view={view}
-                                schemas={schemas}
-                                index={callExprCounter++}
-                                removeCondition={this.removeCondition}
-                                updateOperator={this.updateOperator}
-                                agents={agents}
-                                teams={teams}
-                                updateFieldFilter={updateFieldFilter}
-                            />
-                        </div>
-                    )
-                case 'LogicalExpression':
-                    return (
-                        <div className="view-filters-item">
-                            {walk(node.left, node)}
-
-                            {walk(node.right, node)}
-                        </div>
-                    )
-                default:
-                    throw Error(`Unknown type: ${node.type}`)
-            }
-        }
-        return walk(exp)
     }
+
+    return walk(exp)
 }
-
-const connector = connect(
-    (state: RootState) => ({
-        agents: getAgents(state),
-        schemas: getSchemas(state),
-        teams: getTeams(state),
-        view: getActiveView(state),
-    }),
-    {
-        removeFieldFilter,
-        updateFieldFilter,
-        updateFieldFilterOperator,
-    }
-)
-
-export default connector(ViewFilters)
