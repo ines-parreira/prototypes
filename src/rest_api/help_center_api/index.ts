8e8448dadb077edadc9e89507a1e8a9d6e0e0889
Philippe Diep <philippe.diep@gorgias.com>
2023-01-02 16:33:47 +0100

    chore: use the right staging hc api entry point (#391)

--- src/rest_api/help_center_api/index.ts
+++ src/rest_api/help_center_api/index.ts
@@ -13,7 +13,7 @@ import {AppAbility, AbilityRules, createAbility} from './ability'
 export function getHelpCenterApiBaseUrl(): string {
     // Use helpdesk's host
     if (isStaging()) {
-        return ''
+        return 'https://acme.gorgias.xyz'
     }
     if (isProduction()) {
         return 'https://internal-help-center-api.gorgias.com'
