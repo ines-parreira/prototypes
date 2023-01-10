643102b35773a4b6d63490d819da1f507ad66d9f
Clément Sirieix <clem.sirieix@gmail.com>
2023-01-10 16:08:40 +0100

    PLTDA-601 Add ticket heatmap page content (#418)

--- src/pages/stats/SupportPerformanceWeeklyTicketLoad.tsx
+++ src/pages/stats/SupportPerformanceWeeklyTicketLoad.tsx
@@ -1,11 +1,101 @@
-import React from 'react'
+import React, {useMemo} from 'react'
 
-import StatsPage from './StatsPage'
+import {TicketChannel} from 'business/types/ticket'
+import {
+    FIRST_RESPONSE_TIME,
+    stats as statsConfig,
+    TICKETS_CREATED_PER_HOUR_PER_WEEKDAY,
+} from 'config/stats'
+import useAppSelector from 'hooks/useAppSelector'
+import AgentsStatsFilter from 'pages/stats/AgentsStatsFilter'
+import ChannelsStatsFilter from 'pages/stats/ChannelsStatsFilter'
+import PerHourPerWeekTableStat from 'pages/stats/common/components/charts/PerHourPerWeekTableStat'
+import IntegrationsStatsFilter from 'pages/stats/IntegrationsStatsFilter'
+import PeriodStatsFilter from 'pages/stats/PeriodStatsFilter'
+import StatsPage from 'pages/stats/StatsPage'
+import StatWrapper from 'pages/stats/StatWrapper'
+import TagsStatsFilter from 'pages/stats/TagsStatsFilter'
+import useStatResource from 'pages/stats/useStatResource'
+import {StatsFilters, TwoDimensionalChart} from 'models/stat/types'
+import {
+    getMessagingIntegrationsStatsFilter,
+    getStatsFilters,
+    getStatsMessagingIntegrations,
+} from 'state/stats/selectors'
+
+const SUPPORT_PERFORMANCE_OVERVIEW_STAT_NAME = 'support-performance-overview'
 
 export default function SupportPerformanceWeeklyTicketLoad() {
+    const messagingIntegrations = useAppSelector(getStatsMessagingIntegrations)
+    const integrationsStatsFilter = useAppSelector(
+        getMessagingIntegrationsStatsFilter
+    )
+    const statsFilters = useAppSelector(getStatsFilters)
+
+    const pageStatsFilters = useMemo<StatsFilters>(() => {
+        const {channels, agents, tags, period} = statsFilters
+        return {
+            channels,
+            agents,
+            tags,
+            period,
+            integrations: integrationsStatsFilter,
+        }
+    }, [integrationsStatsFilter, statsFilters])
+
+    const [
+        ticketsCreatedPerHourPerWeekday,
+        isFetchingTicketsCreatedPerHourPerWeekday,
+    ] = useStatResource<TwoDimensionalChart>({
+        statName: SUPPORT_PERFORMANCE_OVERVIEW_STAT_NAME,
+        resourceName: TICKETS_CREATED_PER_HOUR_PER_WEEKDAY,
+        statsFilters: pageStatsFilters,
+    })
+
     return (
-        <StatsPage title="Weekly Ticket Load" filters={null}>
-            SupportPerformanceWeeklyTicketLoad
+        <StatsPage
+            title="Weekly Ticket Load"
+            filters={
+                pageStatsFilters && (
+                    <>
+                        <IntegrationsStatsFilter
+                            value={pageStatsFilters.integrations}
+                            integrations={messagingIntegrations}
+                            isMultiple
+                        />
+
+                        <ChannelsStatsFilter
+                            value={pageStatsFilters.channels}
+                            channels={Object.values(TicketChannel)}
+                        />
+
+                        <AgentsStatsFilter value={pageStatsFilters.agents} />
+
+                        <TagsStatsFilter value={pageStatsFilters.tags} />
+
+                        <PeriodStatsFilter value={pageStatsFilters.period} />
+                    </>
+                )
+            }
+        >
+            {pageStatsFilters && (
+                <StatWrapper
+                    stat={ticketsCreatedPerHourPerWeekday}
+                    isFetchingStat={isFetchingTicketsCreatedPerHourPerWeekday}
+                    resourceName={TICKETS_CREATED_PER_HOUR_PER_WEEKDAY}
+                    statsFilters={pageStatsFilters}
+                    helpText="Tickets created per hour per day of the week"
+                    isDownloadable
+                >
+                    {(stat) => (
+                        <PerHourPerWeekTableStat
+                            data={stat.getIn(['data', 'data'])}
+                            meta={stat.get('meta')}
+                            config={statsConfig.get(FIRST_RESPONSE_TIME)}
+                        />
+                    )}
+                </StatWrapper>
+            )}
         </StatsPage>
     )
 }
--- src/pages/stats/tests/SupportPerformanceWeeklyTicketLoad.spec.tsx
+++ src/pages/stats/tests/SupportPerformanceWeeklyTicketLoad.spec.tsx
@@ -1,11 +1,76 @@
-import React from 'react'
+import React, {ComponentProps} from 'react'
 import {render} from '@testing-library/react'
+import {Provider} from 'react-redux'
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+import {fromJS} from 'immutable'
+import _noop from 'lodash/noop'
 
+import {TicketChannel} from 'business/types/ticket'
+import {account} from 'fixtures/account'
+import {agents} from 'fixtures/agents'
+import {integrationsState} from 'fixtures/integrations'
+import {ticketsCreatedPerHourPerWeekday} from 'fixtures/stats'
+import {teams} from 'fixtures/teams'
+import {StatsFilters} from 'models/stat/types'
+import TagsStatsFilter from 'pages/stats/TagsStatsFilter'
+import useStatResource from 'pages/stats/useStatResource'
+import {RootState, StoreDispatch} from 'state/types'
 import SupportPerformanceWeeklyTicketLoad from '../SupportPerformanceWeeklyTicketLoad'
 
+jest.mock(
+    'pages/stats/TagsStatsFilter',
+    () =>
+        ({value}: ComponentProps<typeof TagsStatsFilter>) =>
+            <div>TagsStatsFilterMock, value: {JSON.stringify(value)}</div>
+)
+jest.mock('pages/stats/useStatResource')
+jest.spyOn(Date, 'now').mockImplementation(() => 1487076708000)
+
 describe('<SupportPerformanceWeeklyTicketLoad />', () => {
+    const mockStore = configureMockStore<Partial<RootState>, StoreDispatch>([
+        thunk,
+    ])
+    const useStatResourceMock = useStatResource as jest.MockedFunction<
+        typeof useStatResource
+    >
+    const defaultState = {
+        currentAccount: fromJS(account),
+        integrations: fromJS(integrationsState),
+        stats: fromJS({
+            filters: {
+                period: {
+                    start_datetime: '2021-02-03T00:00:00.000Z',
+                    end_datetime: '2021-02-03T23:59:59.999Z',
+                },
+                channels: [TicketChannel.Chat],
+                integrations: [integrationsState.integrations[0].id],
+                agents: [agents[0].id],
+                tags: [1],
+            } as StatsFilters,
+        }),
+        agents: fromJS({
+            all: agents,
+        }),
+        teams: fromJS({
+            all: teams,
+        }),
+    } as RootState
+
+    beforeEach(() => {
+        jest.clearAllMocks()
+        useStatResourceMock.mockReturnValue([null, true, _noop])
+    })
+
     it('should render the page', () => {
-        const {container} = render(<SupportPerformanceWeeklyTicketLoad />)
+        useStatResourceMock.mockImplementation(() => {
+            return [ticketsCreatedPerHourPerWeekday, false, _noop]
+        })
+        const {container} = render(
+            <Provider store={mockStore(defaultState)}>
+                <SupportPerformanceWeeklyTicketLoad />
+            </Provider>
+        )
 
         expect(container.firstChild).toMatchSnapshot()
     })
