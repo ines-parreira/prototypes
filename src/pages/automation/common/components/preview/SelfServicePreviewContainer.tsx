ac6546f4a3fa1f57d958a33bf1bfd4a11ffabd2a
Jan Szybowski <112182006+janszybowski@users.noreply.github.com>
2023-02-16 18:46:53 +0100

    feat: use Dropdown component for preview (#619)
    * feat: use Dropdown component for preview
    
    * fix: lint

--- src/pages/automation/common/components/preview/SelfServicePreviewContainer.tsx
+++ src/pages/automation/common/components/preview/SelfServicePreviewContainer.tsx
@@ -1,11 +1,15 @@
-import React, {ReactNode, useEffect, useMemo, useState} from 'react'
+import React, {ReactNode, useEffect, useRef, useState} from 'react'
 import {useHistory} from 'react-router-dom'
 
 import {GorgiasChatIntegration, IntegrationType} from 'models/integration/types'
-import SelectField from 'pages/common/forms/SelectField/SelectField'
-import {Option, Value} from 'pages/common/forms/SelectField/types'
 import Alert, {AlertType} from 'pages/common/components/Alert/Alert'
 import Button from 'pages/common/components/button/Button'
+import SelectInputBox, {
+    SelectInputBoxContext,
+} from 'pages/common/forms/input/SelectInputBox'
+import Dropdown from 'pages/common/components/dropdown/Dropdown'
+import DropdownBody from 'pages/common/components/dropdown/DropdownBody'
+import DropdownItem from 'pages/common/components/dropdown/DropdownItem'
 import SourceIcon from 'pages/common/components/SourceIcon'
 
 import css from './SelfServicePreviewContainer.less'
@@ -26,25 +30,10 @@ const SelfServicePreviewContainer = <T extends GorgiasChatIntegration>({
     children,
 }: Props<T>) => {
     const history = useHistory()
+    const targetRef = useRef<HTMLDivElement>(null)
+    const floatingRef = useRef<HTMLDivElement>(null)
+    const [isSelectOpen, setIsSelectOpen] = useState(false)
     const [channel, setChannel] = useState<T | undefined>(channels[0])
-
-    const options = useMemo<Option[]>(
-        () =>
-            channels.map((option) => ({
-                value: option.id,
-                text: option.name,
-                label: (
-                    <span className={css.channelOption}>
-                        <SourceIcon
-                            type={IntegrationType.GorgiasChat}
-                            className={css.channelOptionIcon}
-                        />
-                        {option.name}
-                    </span>
-                ),
-            })),
-        [channels]
-    )
 
     useEffect(() => {
         if (!channel || !channels.includes(channel)) {
@@ -52,7 +41,7 @@ const SelfServicePreviewContainer = <T extends GorgiasChatIntegration>({
         }
     }, [channel, channels])
 
-    const handleChange = (nextValue: Value) => {
+    const handleChange = (nextValue: number) => {
         setChannel(channels.find((channel) => channel.id === nextValue))
     }
     const handleAlertActionClick = () => {
@@ -62,14 +51,56 @@ const SelfServicePreviewContainer = <T extends GorgiasChatIntegration>({
     return (
         <div className={css.container}>
             <div className={css.header}>Customer preview</div>
-            <SelectField
-                className={css.selectField}
-                value={channel?.id}
-                options={options}
-                onChange={handleChange}
+            <SelectInputBox
+                className={css.selectInput}
                 placeholder="Channel"
-                fullWidth
-            />
+                floating={floatingRef}
+                label={channel?.name}
+                onToggle={setIsSelectOpen}
+                ref={targetRef}
+                prefix={
+                    channel && (
+                        <SourceIcon
+                            type={IntegrationType.GorgiasChat}
+                            className={css.selectInputIcon}
+                        />
+                    )
+                }
+            >
+                <SelectInputBoxContext.Consumer>
+                    {(context) => (
+                        <Dropdown
+                            isOpen={isSelectOpen}
+                            isDisabled={!channels.length}
+                            onToggle={() => context!.onBlur()}
+                            ref={floatingRef}
+                            target={targetRef}
+                            value={channel?.id}
+                        >
+                            <DropdownBody>
+                                {channels.map((channel) => (
+                                    <DropdownItem
+                                        key={channel.id}
+                                        option={{
+                                            label: channel.name,
+                                            value: channel.id,
+                                        }}
+                                        onClick={handleChange}
+                                        shouldCloseOnSelect
+                                        autoFocus
+                                    >
+                                        <SourceIcon
+                                            type={IntegrationType.GorgiasChat}
+                                            className={css.dropdownItemIcon}
+                                        />
+                                        {channel.name}
+                                    </DropdownItem>
+                                ))}
+                            </DropdownBody>
+                        </Dropdown>
+                    )}
+                </SelectInputBoxContext.Consumer>
+            </SelectInputBox>
             {channel ? (
                 children(channel)
             ) : (
