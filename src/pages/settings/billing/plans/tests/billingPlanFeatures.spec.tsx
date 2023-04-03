dd907ab306f246b553dfac1b592a4f918abe6947
François Allain <frallain@gmail.com>
2023-04-03 17:16:30 +0200

    refactor(tests/billing): merge billingPlanFeatures specs into one file (#947)

--- src/pages/settings/billing/plans/tests/billingPlanFeatures.spec.tsx
+++ src/pages/settings/billing/plans/tests/billingPlanFeatures.spec.tsx
@@ -1,8 +1,17 @@
 import {
+    advancedMonthlyHelpdeskPrice,
+    basicMonthlyHelpdeskPrice,
+    customHelpdeskPrice,
+    legacyBasicHelpdeskPrice,
     proMonthlyAutomationPrice,
     proMonthlyHelpdeskPrice,
+    starterHelpdeskPrice,
 } from 'fixtures/productPrices'
-import {getPlanCardFeaturesForPrices} from '../billingPlanFeatures'
+import {HelpdeskPrice} from 'models/billing/types'
+import {
+    getEnterprisePlanCardFeatures,
+    getPlanCardFeaturesForPrices,
+} from '../billingPlanFeatures'
 
 describe('billingPlanFeatures', () => {
     describe('getPlanCardFeaturesForPrices', () => {
@@ -22,6 +31,36 @@ describe('billingPlanFeatures', () => {
                     true
                 )
             ).toMatchSnapshot()
+        })
+
+        it.each<[string, HelpdeskPrice]>([
+            ['Starter price', starterHelpdeskPrice],
+            ['Basic price', basicMonthlyHelpdeskPrice],
+            ['Pro price', proMonthlyHelpdeskPrice],
+            ['Advanced price', advancedMonthlyHelpdeskPrice],
+            ['Custom price', customHelpdeskPrice],
+        ])('should return plan card features for %s', (suiteName, price) => {
+            expect(
+                getPlanCardFeaturesForPrices([price], true)
+            ).toMatchSnapshot()
+        })
+
+        it(`should return plan card features for legacy price`, () => {
+            expect(
+                getPlanCardFeaturesForPrices([legacyBasicHelpdeskPrice], false)
+            ).toMatchSnapshot()
+        })
+
+        it(`should return plan card features with disabled help center`, () => {
+            expect(
+                getPlanCardFeaturesForPrices([basicMonthlyHelpdeskPrice], false)
+            ).toMatchSnapshot()
+        })
+    })
+
+    describe('getEnterprisePlanCardFeatures', () => {
+        it('should return plan card features for the enterprise plan', () => {
+            expect(getEnterprisePlanCardFeatures()).toMatchSnapshot()
         })
     })
 })
