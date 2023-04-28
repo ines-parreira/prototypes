cad595b818c20c5dc668958c18ad83907a3a886b
Jan Balaz <jan.balaz@gorgias.com>
2023-04-28 09:37:05 +0200

    feat(revenue): Hide the uplift chart. (#1127)

--- src/pages/stats/revenue/containers/RevenueStatsContent/RevenueStatsContent.tsx
+++ src/pages/stats/revenue/containers/RevenueStatsContent/RevenueStatsContent.tsx
@@ -3,14 +3,12 @@ import React from 'react'
 import DashboardSection from 'pages/stats/DashboardSection'
 
 import {CampaignTotalsStat} from 'pages/stats/revenue/components/CampaignTotalsStat'
-import {CampaignRevenueUpliftStat} from 'pages/stats/revenue/components/CampaignRevenueUpliftStat'
 import {CampaignPerformanceTable} from '../CampaignPerformanceTable'
 
 export const RevenueStatsContent = () => {
     return (
         <DashboardSection title="">
             <CampaignTotalsStat />
-            <CampaignRevenueUpliftStat />
             <CampaignPerformanceTable />
         </DashboardSection>
     )
