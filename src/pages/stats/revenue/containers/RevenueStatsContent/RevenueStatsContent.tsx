77c8bbb2abb3caaa84218f57e32067c65c44206e
Jan Balaz <jan.balaz@gorgias.com>
2023-05-15 10:55:56 +0200

    chore(revenue): remove LD flag for hiding the revenue uplift chart (#1196)

--- src/pages/stats/revenue/containers/RevenueStatsContent/RevenueStatsContent.tsx
+++ src/pages/stats/revenue/containers/RevenueStatsContent/RevenueStatsContent.tsx
@@ -1,22 +1,16 @@
 import React from 'react'
 
-import {useFlags} from 'launchdarkly-react-client-sdk'
 import DashboardSection from 'pages/stats/DashboardSection'
 
 import {CampaignTotalsStat} from 'pages/stats/revenue/components/CampaignTotalsStat'
 import {CampaignRevenueUpliftStat} from 'pages/stats/revenue/components/CampaignRevenueUpliftStat'
-import {FeatureFlagKey} from 'config/featureFlags'
 import {CampaignPerformanceTable} from '../CampaignPerformanceTable'
 
 export const RevenueStatsContent = () => {
-    const isRevenueUpliftChartHidden = Boolean(
-        useFlags()[FeatureFlagKey.RevenueAttributionHideUpliftChart]
-    )
-
     return (
         <DashboardSection title="">
             <CampaignTotalsStat />
-            {!isRevenueUpliftChartHidden && <CampaignRevenueUpliftStat />}
+            <CampaignRevenueUpliftStat />
             <CampaignPerformanceTable />
         </DashboardSection>
     )
