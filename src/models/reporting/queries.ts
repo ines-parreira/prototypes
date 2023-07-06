c70a76b8b8302116f96c95f6fb4ab448b01be2fb
Maciej Mikulski <maciej.mikulski@gorgias.com>
2023-07-06 11:35:00 +0200

    feature(analytics): [pltda-1101] Don't retry 4XX errors on reporting … (#1466)
    * feature(analytics): [pltda-1101] Don't retry 4XX errors on reporting endpoints.
    
    * feature(analytics): [pltda-1101] Don't retry 4XX errors on reporting endpoints. Post review fixes.

--- src/models/reporting/queries.ts
+++ src/models/reporting/queries.ts
@@ -1,12 +1,27 @@
 import {useQuery, UseQueryOptions} from '@tanstack/react-query'
-import {AxiosResponse} from 'axios'
+import axios, {AxiosResponse} from 'axios'
 
 import {postReporting} from './resources'
 import {ReportingParams, ReportingResponse} from './types'
 
+export const doNotRetry40XErrorsHandler = (
+    failureCount: number,
+    error: unknown
+) => {
+    if (axios.isAxiosError(error)) {
+        const statusCode = error?.response?.status
+        if (statusCode && statusCode >= 400 && statusCode < 500) {
+            return false
+        }
+    }
+
+    return failureCount < 3
+}
+
 const defaultOptions = {
     staleTime: 5 * 60 * 1000, // 5 minutes
     cacheTime: 10 * 60 * 1000, // 10 minutes
+    retry: doNotRetry40XErrorsHandler,
 }
 
 export type UsePostReportingQueryData<TData extends unknown[]> = AxiosResponse<
--- src/models/reporting/tests/queries.spec.ts
+++ src/models/reporting/tests/queries.spec.ts
@@ -1,10 +1,11 @@
 import {renderHook} from '@testing-library/react-hooks'
+import {AxiosError, AxiosResponse} from 'axios'
 
 import {createTestQueryClientProvider} from 'tests/reactQueryTestingUtils'
+import {doNotRetry40XErrorsHandler, usePostReporting} from '../queries'
+import {postReporting} from '../resources'
 
 import {ReportingParams} from '../types'
-import {postReporting} from '../resources'
-import {usePostReporting} from '../queries'
 
 jest.mock('../resources')
 const postReportingMock = postReporting as jest.Mock
@@ -48,5 +49,78 @@ describe('Reporting queries', () => {
             expect(postReportingMock).toHaveBeenCalledWith(payload)
             expect(result.current.data?.data.data).toEqual([42])
         })
+    })
+
+    describe('doNotRetry40XErrorsHandler', () => {
+        const response: AxiosResponse = {
+            data: {},
+            status: 200,
+            statusText: 'some text',
+            headers: [],
+            config: {},
+        }
+
+        const defaultAxiosErrorTemplate: AxiosError = {
+            isAxiosError: true,
+            response,
+            config: {},
+            name: 'someName',
+            message: 'someMessage',
+            toJSON: jest.fn(),
+        }
+
+        const axiosErrorWithCode = (code: number): AxiosError => ({
+            ...defaultAxiosErrorTemplate,
+            response: {
+                ...response,
+                status: code,
+            },
+        })
+
+        it.each([
+            {
+                statusCode: 300,
+                failureCount: 0,
+                shouldRetry: true,
+            },
+            {
+                statusCode: 400,
+                failureCount: 0,
+                shouldRetry: false,
+            },
+            {
+                statusCode: 404,
+                failureCount: 0,
+                shouldRetry: false,
+            },
+            {
+                statusCode: 500,
+                failureCount: 0,
+                shouldRetry: true,
+            },
+            {
+                statusCode: 500,
+                failureCount: 3,
+                shouldRetry: false,
+            },
+        ])(
+            `Should retry: $shouldRetry error with status $statusCode and failure count: $failureCount`,
+            ({
+                statusCode,
+                failureCount,
+                shouldRetry,
+            }: {
+                statusCode: number
+                failureCount: number
+                shouldRetry: boolean
+            }) => {
+                expect(
+                    doNotRetry40XErrorsHandler(
+                        failureCount,
+                        axiosErrorWithCode(statusCode)
+                    )
+                ).toEqual(shouldRetry)
+            }
+        )
     })
 })
