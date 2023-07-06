ec274e8ef4e293edf4a6a9338b3b7022685c8ca4
Marcel Lefter <marcellefter@outlook.com>
2023-07-06 16:32:15 +0300

    chore(stats/TableStat): add TableStat stories (#1472)
    Co-authored-by: Marcel Lefter <marcel.lefter@ebs-integrator.com>

---
+++ src/pages/stats/common/components/charts/TableStat/TableStat.stories.tsx
@@ -0,0 +1,43 @@
+import React, {ComponentProps} from 'react'
+import {Meta, Story} from '@storybook/react'
+import {MemoryRouter} from 'react-router-dom'
+import {fromJS} from 'immutable'
+import {revenuePerAgent, ticketsClosedPerAgent} from 'fixtures/stats'
+
+import TableStat from './TableStat'
+
+const storyConfig: Meta = {
+    title: 'Stats/TableStat',
+    component: TableStat,
+    decorators: [(story) => <MemoryRouter>{story()}</MemoryRouter>],
+}
+
+const Template: Story<ComponentProps<typeof TableStat>> = (props) => (
+    <TableStat {...props} />
+)
+
+const defaultProps: ComponentProps<typeof TableStat> = {
+    context: {tagColors: null},
+    data: fromJS(revenuePerAgent.data.data),
+    meta: fromJS(revenuePerAgent.meta),
+    config: fromJS({}),
+}
+
+export const Default = Template.bind({})
+Default.args = defaultProps
+
+export const WithExpand = Template.bind({})
+WithExpand.args = {
+    ...defaultProps,
+    data: fromJS(ticketsClosedPerAgent.data.data),
+    meta: fromJS(ticketsClosedPerAgent.meta),
+    config: fromJS({tableOptions: {showLines: 1}}),
+}
+
+export const NoData = Template.bind({})
+NoData.args = {
+    ...defaultProps,
+    data: fromJS({lines: []}),
+}
+
+export default storyConfig
