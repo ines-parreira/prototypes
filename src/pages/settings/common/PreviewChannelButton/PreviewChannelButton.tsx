a5520e47d037d020592fe968b363ffcca89fcdf9
Ulad <130975351+ulad-ramanovich@users.noreply.github.com>
2023-07-27 13:10:23 +0200

    feat(help-center): add preview mode tooltip (#1641)
    * feat(help-center): add preview mode tooltip
    
    * feat(help-center): fix after MC
    
    * feat(help-center): fix tooltip styles
    
    * feat(help-center): remove !important

--- src/pages/settings/common/PreviewChannelButton/PreviewChannelButton.tsx
+++ src/pages/settings/common/PreviewChannelButton/PreviewChannelButton.tsx
@@ -3,11 +3,13 @@ import {useFlags} from 'launchdarkly-react-client-sdk'
 import Button from 'pages/common/components/button/Button'
 import ButtonIconLabel from 'pages/common/components/button/ButtonIconLabel'
 import {FeatureFlagKey} from 'config/featureFlags'
+import Tooltip from 'pages/common/components/Tooltip'
 import {
     logEvent,
     SegmentEvent,
 } from '../../../../store/middlewares/segmentTracker'
 import {SelfServiceChannel} from '../../../automation/common/hooks/useSelfServiceChannels'
+import styles from './PreviewChannelButton.less'
 
 const DEFAULT_CHANNEL_TYPE = 'unknown'
 
@@ -15,6 +17,8 @@ type PreviewChannelButtonProps = {
     url: string
     channel?: SelfServiceChannel
 }
+
+const TOOLTIP_TARGET_ID = 'toggle-widgets-edition-button'
 
 export const PreviewChannelButton = ({
     url,
@@ -35,8 +39,34 @@ export const PreviewChannelButton = ({
     }
 
     return isAAOPreviewModeEnabled ? (
-        <Button fillStyle="ghost" intent="secondary" onClick={onPreview}>
-            <ButtonIconLabel icon="open_in_new">Try it live</ButtonIconLabel>
-        </Button>
+        <>
+            <Tooltip
+                placement="bottom"
+                target={TOOLTIP_TARGET_ID}
+                autohide={false}
+            >
+                <div className={styles.tooltipContainer}>
+                    Test your flows without generating tickets.{' '}
+                    <a
+                        href="https://docs.gorgias.com/en-US/286870-86c09fc4f4444b289d1a486eb0beba16"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn more
+                    </a>
+                </div>
+            </Tooltip>
+
+            <Button
+                fillStyle="ghost"
+                intent="secondary"
+                onClick={onPreview}
+                id={TOOLTIP_TARGET_ID}
+            >
+                <ButtonIconLabel icon="open_in_new">
+                    Try it live
+                </ButtonIconLabel>
+            </Button>
+        </>
     ) : null
 }
