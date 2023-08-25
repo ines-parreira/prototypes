67a11497759ba9dcf896b89f5d21159274588632
Daniel Suruceanu <58149663+H7GhosT@users.noreply.github.com>
2023-08-25 14:01:50 +0300

    feat(components): [PLTDA-1213] Add Avatar storybook preview (#1825)
    Co-authored-by: Daniel Suruceanu <daniel.suruceanu@ebs-integrator.com>

---
+++ src/pages/common/components/Avatar/Avatar.stories.tsx
@@ -0,0 +1,98 @@
+import React, {ComponentProps} from 'react'
+import {Meta, Story} from '@storybook/react'
+
+import personAvatarUrl from 'assets/img/avatar-example.png'
+
+import Avatar from './Avatar'
+
+const storyConfig: Meta = {
+    title: 'General/Avatar',
+    component: Avatar,
+    argTypes: {
+        url: {
+            control: {
+                type: 'text',
+            },
+        },
+    },
+}
+
+const AllSizesTemplate: Story<ComponentProps<typeof Avatar>> = ({...props}) => (
+    <div style={{display: 'flex', gap: 24, alignItems: 'flex-start'}}>
+        {[24, 36, 48, 100].map((size) => (
+            <Avatar key={size} {...props} size={size} />
+        ))}
+    </div>
+)
+
+export const WithImage = AllSizesTemplate.bind({})
+WithImage.args = {
+    name: 'John Born',
+    url: personAvatarUrl,
+    shape: 'round',
+}
+
+export const WithImageSquared = AllSizesTemplate.bind({})
+WithImageSquared.args = {
+    name: 'John Born',
+    url: personAvatarUrl,
+    shape: 'square',
+}
+
+export const WithInitials = AllSizesTemplate.bind({})
+WithInitials.args = {
+    name: 'John Born',
+    shape: 'round',
+}
+
+export const WithInitialsSquared = AllSizesTemplate.bind({})
+WithInitialsSquared.args = {
+    name: 'John Born',
+    shape: 'square',
+}
+
+const TwoAvatarsTemplate: Story<ComponentProps<typeof Avatar>> = ({
+    ...props
+}) => (
+    <div style={{display: 'flex', gap: 24, alignItems: 'flex-start'}}>
+        <Avatar url={personAvatarUrl} {...props} />
+        <Avatar {...props} />
+    </div>
+)
+
+export const WithStatus: Story<ComponentProps<typeof Avatar>> =
+    TwoAvatarsTemplate.bind({})
+
+WithStatus.args = {
+    size: 48,
+    name: 'John Born',
+    shape: 'round',
+    badgeColor: '#24D69D',
+    badgeBorderColor: 'transparent',
+}
+
+export const WithStatusSquared: Story<ComponentProps<typeof Avatar>> =
+    TwoAvatarsTemplate.bind({})
+
+WithStatusSquared.args = {
+    size: 48,
+    name: 'John Born',
+    shape: 'square',
+    badgeColor: '#24D69D',
+    badgeBorderColor: 'transparent',
+}
+
+export const WithStatusAndToltip: Story<ComponentProps<typeof Avatar>> =
+    TwoAvatarsTemplate.bind({})
+
+WithStatusAndToltip.args = {
+    size: 48,
+    name: 'John Born',
+    shape: 'round',
+    badgeColor: '#24D69D',
+    badgeBorderColor: 'transparent',
+    withTooltip: true,
+    tooltipText: 'Online',
+}
+
+export default storyConfig
