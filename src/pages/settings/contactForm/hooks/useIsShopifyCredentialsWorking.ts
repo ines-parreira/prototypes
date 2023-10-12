0845f2d17ad8397d83ef58de8a38b81f84326166
Kyrylo Chebotarov <kyrylo.chebotarov@gorgias.com>
2023-10-12 13:18:35 +0300

    fix: don't verify shopify credentials if not needed (#2173)
    * fix: don't verify shopify credentials if not needed
    
    * fix: shop name constant

--- src/pages/settings/contactForm/hooks/useIsShopifyCredentialsWorking.ts
+++ src/pages/settings/contactForm/hooks/useIsShopifyCredentialsWorking.ts
@@ -1,11 +1,30 @@
 import {useCurrentContactForm} from 'pages/settings/contactForm/hooks/useCurrentContactForm'
 import {useGetShopifyPages} from 'pages/settings/contactForm/queries'
+import useAppSelector from 'hooks/useAppSelector'
+import {getShopifyIntegrationByShopName} from 'state/integrations/selectors'
+
+// It's used to be type safe when contact form is missing the shop name
+const SHOP_NAME_PLACEHOLDER = 'no shop'
 
 // TODO: this is a temporary solution, we should use the same logic as in the API
 // https://gorgias.slack.com/archives/C01JDUKLEEQ/p1696523140686499
 export function useIsShopifyCredentialsWorking() {
     const contactForm = useCurrentContactForm()
-    const getShopifyPages = useGetShopifyPages(contactForm.id)
+    const shopifyIntegration = useAppSelector(
+        getShopifyIntegrationByShopName(
+            contactForm?.shop_name || SHOP_NAME_PLACEHOLDER
+        )
+    )
+
+    const needScopeUpdate = !shopifyIntegration.isEmpty()
+        ? Boolean(
+              shopifyIntegration.getIn(['meta', 'need_scope_update'], false)
+          )
+        : false
+
+    const getShopifyPages = useGetShopifyPages(contactForm.id, {
+        enabled: !needScopeUpdate && Boolean(contactForm.shop_name),
+    })
 
     return {
         isLoading: getShopifyPages.isLoading,
