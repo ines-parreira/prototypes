0c5c0e6ace5943bc67e35d2e112431a33985ef4c
Chiel Kunkels <chiel@kunkels.me>
2023-10-18 12:41:21 +0200

    feat(core): extract remaining shared logic from app (#2256)
    * feat(core): move remaining shared logic from app
    
    * test: add test for useSharedLogic hook
    
    ---------
    
    Co-authored-by: Xuan-Thi Nguyen <xuan-thi@gorgias.com>

---
+++ src/core/hooks/useSharedLogic.ts
@@ -0,0 +1,24 @@
+import {useEffect} from 'react'
+
+import useAppDispatch from 'hooks/useAppDispatch'
+import useAppSelector from 'hooks/useAppSelector'
+import userActivityManager from 'services/userActivityManager'
+import {handle2FAEnforced} from 'state/currentUser/actions'
+import {fetchVisibleViewsCounts} from 'state/views/actions'
+import {identifyUser} from 'store/middlewares/segmentTracker'
+
+export default function useSharedLogic() {
+    const dispatch = useAppDispatch()
+    const currentUser = useAppSelector((state) => state.currentUser)
+
+    useEffect(() => {
+        userActivityManager.watch()
+
+        // ask for the newest view counts
+        dispatch(fetchVisibleViewsCounts())
+
+        identifyUser(currentUser.toJS())
+
+        dispatch(handle2FAEnforced())
+    }, [currentUser, dispatch])
+}
