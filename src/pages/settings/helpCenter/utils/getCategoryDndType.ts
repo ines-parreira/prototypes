8bae8cebdd1849da057a8340e595a2e6d3191baf
Chiel Kunkels <chiel@kunkels.me>
2023-11-01 13:18:20 +0100

    fix: circular dep in `getCategoryDndType` (#2365)

--- src/pages/settings/helpCenter/utils/getCategoryDndType.ts
+++ src/pages/settings/helpCenter/utils/getCategoryDndType.ts
@@ -1,4 +1,4 @@
-import {DND_ENTITIES} from '../components/CategoriesTable'
+import {DND_ENTITIES} from '../components/CategoriesTable/constants'
 
 export const getCategoryDndType = (id: number | null) => {
     return `${DND_ENTITIES.CATEGORY}${id || ''}`
