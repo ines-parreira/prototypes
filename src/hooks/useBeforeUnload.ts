c8aefe1f16fc4b10359bf1dc822e7f1d596481c6
Daniel Suruceanu <58149663+H7GhosT@users.noreply.github.com>
2023-12-19 09:19:16 +0200

    refactor: [PLTOF-348] Migrate useBeforeUnload to use local implementation (#2766)

---
+++ src/hooks/useBeforeUnload.ts
@@ -0,0 +1,36 @@
+import {useCallback, useEffect} from 'react'
+
+export default function useBeforeUnload(
+    enabled: boolean | (() => boolean) = true,
+    message?: string
+) {
+    const handler = useCallback(
+        (event: BeforeUnloadEvent) => {
+            const finalEnabled =
+                typeof enabled === 'function' ? enabled() : true
+
+            if (!finalEnabled) {
+                return
+            }
+
+            event.preventDefault()
+
+            if (message) {
+                event.returnValue = message
+            }
+
+            return message
+        },
+        [enabled, message]
+    )
+
+    useEffect(() => {
+        if (!enabled) {
+            return
+        }
+
+        window.addEventListener('beforeunload', handler)
+
+        return () => window.removeEventListener('beforeunload', handler)
+    }, [enabled, handler])
+}
--- src/pages/common/components/PhoneIntegrationBar/PhoneIntegrationBar.tsx
+++ src/pages/common/components/PhoneIntegrationBar/PhoneIntegrationBar.tsx
@@ -1,7 +1,4 @@
 import React, {useCallback, useEffect, useState} from 'react'
-// [PLTOF-48] Please avoid importing more hooks from 'react-use', prefer using your own implementation of the hook rather than depending on external library
-// eslint-disable-next-line no-restricted-imports
-import {useBeforeUnload} from 'react-use'
 import {useFlags} from 'launchdarkly-react-client-sdk'
 
 import {FeatureFlagKey} from 'config/featureFlags'
@@ -9,6 +6,7 @@ import useAppSelector from 'hooks/useAppSelector'
 import useTimeoutFn from 'hooks/useTimeoutFn'
 import {useDevice} from 'hooks/integrations/phone/useDevice'
 import {useDevice_DEPRECATED} from 'hooks/integrations/phone/useDevice_DEPRECATED'
+import useBeforeUnload from 'hooks/useBeforeUnload'
 
 import OngoingPhoneCall from './OngoingPhoneCall/OngoingPhoneCall'
 import IncomingPhoneCall from './IncomingPhoneCall/IncomingPhoneCall'
