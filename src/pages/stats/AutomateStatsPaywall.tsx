86b422b8c4e3be3366f229071fb9b8f27c426a9a
Matthieu-Gorgias <87850462+Matthieu-Gorgias@users.noreply.github.com>
2024-01-10 11:51:29 +0100

    feat(stats): make the new automate stats page directly accessible via URL even with feature flag disabled (#2909)

--- src/pages/stats/AutomateStatsPaywall.tsx
+++ src/pages/stats/AutomateStatsPaywall.tsx
@@ -1,24 +1,17 @@
 import React from 'react'
-import {useFlags} from 'launchdarkly-react-client-sdk'
 import {AutomateFeatures} from 'pages/automate/common/types'
 import {getHasAutomate} from 'state/billing/selectors'
 import useAppSelector from 'hooks/useAppSelector'
-import {FeatureFlagKey} from 'config/featureFlags'
 import AutomatePaywallView from '../automate/common/components/AutomatePaywallView'
 import AutomateOverview from './AutomateOverview'
-import SelfServiceStatsPage from './self-service/SelfServiceStatsPage'
 
 const AutomateStatsPaywall: React.FC = () => {
-    const isNewAutomateEnabled: boolean | undefined =
-        useFlags()[FeatureFlagKey.NewAutomationAddon]
     const hasAutomate = useAppSelector(getHasAutomate)
 
     return !hasAutomate ? (
         <AutomatePaywallView automateFeature={AutomateFeatures.AutomateStats} />
-    ) : isNewAutomateEnabled ? (
-        <AutomateOverview />
     ) : (
-        <SelfServiceStatsPage />
+        <AutomateOverview />
     )
 }
 
