6c93c615165a03fa8cadd15ffc8435700a8bf998
Sufiyan <132895712+sufiyangorgias@users.noreply.github.com>
2024-02-01 12:30:52 +0100

    fix(automate): [AUTEN-2873] fraction (#3059)

--- src/pages/automate/common/hooks/useGetCostPerAutomatedInteraction.ts
+++ src/pages/automate/common/hooks/useGetCostPerAutomatedInteraction.ts
@@ -7,7 +7,10 @@ export const useGetCostPerAutomatedInteraction = () => {
     const automationProduct = useAppSelector(getCurrentAutomationProduct)
 
     if (automationProduct?.num_quota_tickets) {
-        return automationProduct.amount / automationProduct.num_quota_tickets
+        return (
+            // because the amount is in cents we need to devide it by 100
+            automationProduct.amount / 100 / automationProduct.num_quota_tickets
+        )
     }
 
     return FIXED_COST_PER_AUTOMATED_INTERACTION
