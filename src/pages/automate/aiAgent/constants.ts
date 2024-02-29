22fc04f4b81de7a2a59cfdf4d936756d880276dc
Victor Duprez <victor.duprez@gorgias.com>
2024-02-29 14:36:55 -0800

    fix: tone of voice conversation

--- src/pages/automate/aiAgent/constants.ts
+++ src/pages/automate/aiAgent/constants.ts
@@ -8,14 +8,14 @@ export const TONE_OF_VOICE_LABELS = [
 ]
 export const TONE_OF_VOICE_LABEL_TO_VALUE: Record<string, string> = {
     Friendly:
-        'Be concise and empathetic. Call the shopper by their name. Keep it casual.',
+        'Be concise and empathetic. Call the shopper by their first name. Keep it casual.',
     Professional:
         'Maintain a formal and respectful tone throughout interactions. Stick to professional language. Be concise.',
     Sophisticated:
         'Use a refined and articulate tone. Ensure the language is sophisticated. Be concise.',
 }
 export const TONE_OF_VOICE_VALUE_TO_LABEL: Record<string, string> = {
-    'Be concise and empathetic. Call the shopper by their name. Keep it casual.':
+    'Be concise and empathetic. Call the shopper by their first name. Keep it casual.':
         'Friendly',
     'Maintain a formal and respectful tone throughout interactions. Stick to professional language. Be concise.':
         'Professional',
