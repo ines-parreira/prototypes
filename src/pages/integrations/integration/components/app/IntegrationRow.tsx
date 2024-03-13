2ec991362707d5e2efcef8697e7c2efcaaa0c9ad
dyacoub <114911701+dyacoub@users.noreply.github.com>
2024-03-13 11:32:11 +0100

    Danyyacoub/apps 1479 add a detail page when clicking on a connection 1 (#3475)
    * feat(i/app): new design for the connections page + add the ability to delete

--- src/pages/integrations/integration/components/app/IntegrationRow.tsx
+++ src/pages/integrations/integration/components/app/IntegrationRow.tsx
@@ -1,5 +1,5 @@
-import {Button} from 'reactstrap'
 import React from 'react'
+import {fromJS} from 'immutable'
 import {
     EcommerceIntegrationMeta,
     Integration,
@@ -10,6 +10,12 @@ import {getCurrentDomain} from 'state/currentAccount/selectors'
 import TableBodyRow from 'pages/common/components/table/TableBodyRow'
 import BodyCell from 'pages/common/components/table/cells/BodyCell'
 import css from 'pages/integrations/integration/components/app/IntegrationRow.less'
+import ConfirmationPopover from 'pages/common/components/popover/ConfirmationPopover'
+import {deleteIntegration} from 'state/integrations/actions'
+import useAppDispatch from 'hooks/useAppDispatch'
+import Tooltip from 'pages/common/components/Tooltip'
+import {getIntegrationsLoading} from 'state/integrations/selectors'
+import IconButton from 'pages/common/components/button/IconButton'
 import {getReconnectUrl} from './helpers'
 
 type Props = {
@@ -21,11 +27,17 @@ export default function IntegrationRow({integration, connectUrl}: Props) {
     const domain = useAppSelector(getCurrentDomain)
     const isDisabled = integration.deactivated_datetime
     const reconnectUrl = getReconnectUrl(connectUrl, domain, integration)
+    const dispatch = useAppDispatch()
+    const loading = useAppSelector(getIntegrationsLoading)
+    const isDeleting =
+        typeof loading?.delete === 'number' &&
+        loading?.delete === integration.id
+
     return (
         <TableBodyRow key={integration.id}>
             <BodyCell className={css.row}>
                 <strong>{integration.name}</strong>
-                <span>
+                <span className="text-secondary">
                     {isAppIntegration(integration)
                         ? integration.meta?.address
                         : (integration.meta as EcommerceIntegrationMeta)
@@ -33,11 +45,64 @@ export default function IntegrationRow({integration, connectUrl}: Props) {
                 </span>
             </BodyCell>
             <BodyCell className={css.row} justifyContent={'right'}>
-                {isDisabled && (
-                    <a href={reconnectUrl} className={css.actionLink}>
-                        <Button type="button">Reconnect</Button>
-                    </a>
-                )}
+                <>
+                    {isDisabled && (
+                        <>
+                            <span className="mr-4">
+                                <i className="material-icons text-danger mr-1">
+                                    cancel
+                                </i>
+                                Disconnected
+                            </span>
+                            <IconButton
+                                id={`reconnect-${integration.id}`}
+                                intent="secondary"
+                                fillStyle="ghost"
+                                className="mr-2"
+                                onClick={() =>
+                                    (window.location.href = reconnectUrl)
+                                }
+                            >
+                                cached
+                            </IconButton>
+                            <Tooltip
+                                placement="top"
+                                target={`reconnect-${integration.id}`}
+                            >
+                                Reconnect
+                            </Tooltip>
+                        </>
+                    )}
+                    <ConfirmationPopover
+                        id={`delete-${integration.id}`}
+                        buttonProps={{intent: 'destructive'}}
+                        cancelButtonProps={{intent: 'secondary'}}
+                        content="Are you sure you want to delete this account? All associated views and rules will be disabled."
+                        title={<b>Delete account?</b>}
+                        onConfirm={() =>
+                            dispatch(deleteIntegration(fromJS(integration)))
+                        }
+                        confirmLabel="Delete"
+                        showCancelButton
+                    >
+                        {({uid, onDisplayConfirmation}) => (
+                            <>
+                                <IconButton
+                                    id={uid}
+                                    onClick={onDisplayConfirmation}
+                                    fillStyle="ghost"
+                                    isLoading={isDeleting}
+                                    intent="secondary"
+                                >
+                                    {isDeleting ? '' : 'delete'}
+                                </IconButton>
+                                <Tooltip placement="top" target={uid}>
+                                    Delete
+                                </Tooltip>
+                            </>
+                        )}
+                    </ConfirmationPopover>
+                </>
             </BodyCell>
         </TableBodyRow>
     )
--- src/pages/integrations/integration/components/app/tests/IntegrationRow.spec.tsx
+++ src/pages/integrations/integration/components/app/tests/IntegrationRow.spec.tsx
@@ -3,6 +3,7 @@ import configureMockStore from 'redux-mock-store'
 import {render} from '@testing-library/react'
 import {Provider} from 'react-redux'
 
+import {fromJS} from 'immutable'
 import {RootState, StoreDispatch} from 'state/types'
 import {Integration} from 'models/integration/types'
 import {assumeMock} from 'utils/testing'
@@ -19,88 +20,123 @@ const mockStore = configureMockStore<Partial<RootState>, StoreDispatch>()
 const appStore = mockStore()
 
 describe('<IntegrationRow/>', () => {
-    describe('render()', () => {
-        it('should render a connected integration with the right data from meta for app type', () => {
-            const integration = {
-                id: 1,
-                type: 'app',
-                application_id: '1',
-                name: 'my app',
-                meta: {address: '@myapp'},
-            } as Integration
-            const {getByText} = render(
-                <Provider store={appStore}>
-                    <TableWrapper>
-                        <TableBody>
-                            <IntegrationRow
-                                integration={integration}
-                                connectUrl="https://test.test"
-                            />
-                        </TableBody>
-                    </TableWrapper>
-                </Provider>
-            )
+    it('should render a connected integration with the right data from meta for app type', () => {
+        const integration = {
+            id: 1,
+            type: 'app',
+            application_id: '1',
+            name: 'my app',
+            meta: {address: '@myapp'},
+        } as Integration
+        const {container, getByText} = render(
+            <Provider store={appStore}>
+                <TableWrapper>
+                    <TableBody>
+                        <IntegrationRow
+                            integration={integration}
+                            connectUrl="https://test.test"
+                        />
+                    </TableBody>
+                </TableWrapper>
+            </Provider>
+        )
 
-            expect(getByText('my app')).toBeDefined()
-            expect(getByText('@myapp')).toBeDefined()
+        expect(getByText('my app')).toBeDefined()
+        expect(getByText('@myapp')).toBeDefined()
+        const deleteIcon = container.querySelector('i.material-icons')
+        expect(deleteIcon).toBeInTheDocument()
+        if (deleteIcon) {
+            expect(deleteIcon.textContent).toBe('delete')
+        }
+    })
+    it('should render a connected integration with the right data from meta for ecom type', () => {
+        const integration = {
+            id: 1,
+            type: 'ecom',
+            application_id: '1',
+            name: 'my ecom store',
+            meta: {store_uuid: '@store'},
+        } as Integration
+
+        const {container, getByText} = render(
+            <Provider store={appStore}>
+                <TableWrapper>
+                    <TableBody>
+                        <IntegrationRow
+                            integration={integration}
+                            connectUrl="https://test.test"
+                        />
+                    </TableBody>
+                </TableWrapper>
+            </Provider>
+        )
+
+        expect(getByText('my ecom store')).toBeDefined()
+        expect(getByText('@store')).toBeDefined()
+        const deleteIcon = container.querySelector('i.material-icons')
+        expect(deleteIcon).toBeInTheDocument()
+        if (deleteIcon) {
+            expect(deleteIcon.textContent).toBe('delete')
+        }
+    })
+    it('should show loading icon when delete button is clicked', () => {
+        const integration = {
+            id: 1,
+            type: 'ecom',
+            application_id: '1',
+            name: 'my ecom store',
+            meta: {store_uuid: '@store'},
+        } as Integration
+        const loadingIntegrationAppStore = mockStore({
+            integrations: fromJS({
+                state: {
+                    loading: {delete: 1},
+                },
+            }),
         })
-    }),
-        describe('render()', () => {
-            it('should render a connected integration with the right data from meta for ecom type', () => {
-                const integration = {
-                    id: 1,
-                    type: 'ecom',
-                    application_id: '1',
-                    name: 'my ecom store',
-                    meta: {store_uuid: '@store'},
-                } as Integration
 
-                const {getByText} = render(
-                    <Provider store={appStore}>
-                        <TableWrapper>
-                            <TableBody>
-                                <IntegrationRow
-                                    integration={integration}
-                                    connectUrl="https://test.test"
-                                />
-                            </TableBody>
-                        </TableWrapper>
-                    </Provider>
-                )
+        const {container} = render(
+            <Provider store={loadingIntegrationAppStore}>
+                <TableWrapper>
+                    <TableBody>
+                        <IntegrationRow
+                            integration={integration}
+                            connectUrl="https://test.test"
+                        />
+                    </TableBody>
+                </TableWrapper>
+            </Provider>
+        )
 
-                expect(getByText('my ecom store')).toBeDefined()
-                expect(getByText('@store')).toBeDefined()
-            })
-        }),
-        describe('render()', () => {
-            it('should render a disconnected integration with a button to reconnect', () => {
-                const integration = {
-                    id: 2,
-                    type: 'app',
-                    application_id: '1',
-                    name: 'my deactivated app',
-                    meta: {address: '@deactivatedapp'},
-                    deactivated_datetime: '2021-01-01T00:00:00Z',
-                } as Integration
+        const loadingIcon = container.querySelector('svg')
+        expect(loadingIcon).toBeInTheDocument()
+    })
+    it('should render a disconnected integration with a button to reconnect', () => {
+        const integration = {
+            id: 2,
+            type: 'app',
+            application_id: '1',
+            name: 'my deactivated app',
+            meta: {address: '@deactivatedapp'},
+            deactivated_datetime: '2021-01-01T00:00:00Z',
+        } as Integration
 
-                const {container, getByText} = render(
-                    <Provider store={appStore}>
-                        <TableWrapper>
-                            <TableBody>
-                                <IntegrationRow
-                                    integration={integration}
-                                    connectUrl="https://test.test"
-                                />
-                            </TableBody>
-                        </TableWrapper>
-                    </Provider>
-                )
+        const {container, getByText} = render(
+            <Provider store={appStore}>
+                <TableWrapper>
+                    <TableBody>
+                        <IntegrationRow
+                            integration={integration}
+                            connectUrl="https://test.test"
+                        />
+                    </TableBody>
+                </TableWrapper>
+            </Provider>
+        )
 
-                expect(getByText('my deactivated app')).toBeDefined()
-                expect(getByText('@deactivatedapp')).toBeDefined()
-                expect(container.querySelector('a')?.href).toBe(
-                    'https://test.test/'
-                )
-            })
-        })
+        expect(getByText('my deactivated app')).toBeDefined()
+        expect(getByText('@deactivatedapp')).toBeDefined()
+        expect(getByText('Disconnected')).toBeDefined()
+        expect(container.querySelector('button')?.id).toBe('reconnect-2')
+    })
 })
