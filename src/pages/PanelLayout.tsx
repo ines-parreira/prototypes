4be4cddf218ff50c6e6bee9ac7d5c41fe10c68dc
Chiel Kunkels <chiel@kunkels.me>
2024-03-22 10:08:18 +0100

    feat(panels): add `PanelLayout` component (#3590)

---
+++ src/pages/PanelLayout.tsx
@@ -0,0 +1,58 @@
+import React, {ReactElement, useCallback, useMemo} from 'react'
+
+import {Panel, PanelConfig, Panels} from 'panels'
+import {LayoutKeys} from 'split-ticket-view/constants'
+import createInitialConfig from 'split-ticket-view/utils/createInitialConfig'
+import storePanelWidths from 'split-ticket-view/utils/storePanelWidths'
+
+export type PanelLayoutConfig = {
+    key: string
+    content: ReactElement
+    panelConfig: PanelConfig
+}[]
+
+type Props = {
+    config: PanelLayoutConfig
+    fallbackComponent?: ReactElement
+    fallbackWidth?: number
+    layoutKey: LayoutKeys
+}
+
+export default function PanelLayout({
+    config,
+    fallbackComponent,
+    fallbackWidth,
+    layoutKey,
+}: Props) {
+    const panelsConfig = useMemo(
+        () =>
+            createInitialConfig(
+                layoutKey,
+                config.map((c) => c.panelConfig)
+            ),
+        [config, layoutKey]
+    )
+
+    const children = useMemo(
+        () => config.map((c) => <Panel key={c.key}>{c.content}</Panel>),
+        [config]
+    )
+
+    const handleResize = useCallback(
+        (widths: number[]) => {
+            storePanelWidths(layoutKey, widths)
+        },
+        [layoutKey]
+    )
+
+    return (
+        <Panels
+            config={panelsConfig}
+            fallbackComponent={fallbackComponent}
+            fallbackWidth={fallbackWidth}
+            onResize={handleResize}
+        >
+            {children}
+        </Panels>
+    )
+}
---
+++ src/pages/tests/PanelLayout.spec.tsx
@@ -0,0 +1,69 @@
+import {render} from '@testing-library/react'
+import React, {ReactNode} from 'react'
+
+import {Panel, Panels} from 'panels'
+import {LayoutKeys} from 'split-ticket-view/constants'
+
+import PanelLayout, {PanelLayoutConfig} from '../PanelLayout'
+
+jest.mock('panels', () => ({
+    Panel: jest.fn(),
+    Panels: jest.fn(),
+}))
+const PanelMock = Panel as jest.Mock
+const PanelsMock = Panels as jest.Mock
+
+describe('PanelLayout', () => {
+    beforeEach(() => {
+        PanelMock.mockReturnValue(<div>Panel</div>)
+        PanelsMock.mockImplementation(({children}: {children: ReactNode}) => (
+            <div>
+                <p>Panels</p>
+                {children}
+            </div>
+        ))
+    })
+
+    it('should convert the given config', () => {
+        const config: PanelLayoutConfig = [
+            {
+                key: 'navbar-panel',
+                content: <p>Navbar</p>,
+                panelConfig: [250, 200, 300],
+            },
+            {
+                key: 'ticket-panel',
+                content: <p>Ticket</p>,
+                panelConfig: [Infinity, 400],
+            },
+        ]
+
+        const {getByText} = render(
+            <PanelLayout config={config} layoutKey={LayoutKeys.TICKET} />
+        )
+
+        expect(PanelMock).toHaveBeenCalledWith(
+            expect.objectContaining({
+                children: <p>Navbar</p>,
+            }),
+            expect.anything()
+        )
+
+        expect(PanelMock).toHaveBeenCalledWith(
+            expect.objectContaining({
+                children: <p>Ticket</p>,
+            }),
+            expect.anything()
+        )
+        expect(PanelsMock).toHaveBeenCalledWith(
+            expect.objectContaining({
+                config: [
+                    [250, 200, 300],
+                    [Infinity, 400],
+                ],
+            }),
+            expect.anything()
+        )
+        expect(getByText('Panels')).toBeInTheDocument()
+    })
+})
