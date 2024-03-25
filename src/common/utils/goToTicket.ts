e6ca25c6e5848ab31b3b6d34aa235d70370bcd1a
NikolaMijovic93 <56353185+NikolaMijovic93@users.noreply.github.com>
2024-03-25 12:46:01 +0100

    fix(ticket-panel): remerge - fix merge + UNDO actions (#3615)
    * fix: history push on ticket merge
    
    * chore: add LD check
    
    * refactor: replace with helper fn
    
    * fix: spam undo
    
    * fix: trashing
    
    * test: add and adjust suites
    
    * style: lint
    
    * fix: void goToTicket in ticket actions
    
    * refactor: review feedback

---
+++ src/common/utils/goToTicket.ts
@@ -0,0 +1,19 @@
+import {matchPath} from 'react-router'
+
+import history from 'pages/history'
+
+export default function goToTicket(ticketId: number | string) {
+    const match = matchPath<{viewId?: string}>(window.location.pathname, {
+        path: '/app/views/:viewId',
+        exact: false,
+        strict: false,
+    })
+
+    const viewId = match?.params.viewId
+
+    if (viewId) {
+        history.push(`/app/views/${viewId}/${ticketId}`)
+    } else {
+        history.push(`/app/ticket/${ticketId}`)
+    }
+}
