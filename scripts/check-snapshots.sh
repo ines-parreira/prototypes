22efabe98f5e4b44e2b5639077b6bfebe585948f
mdenisadumitru <55971422+mdenisadumitru@users.noreply.github.com>
2024-03-27 17:31:42 +0200

    chore: compare current commit to the common ancestor commit for snapshots (#3585)

--- scripts/check-snapshots.sh
+++ scripts/check-snapshots.sh
@@ -1,5 +1,10 @@
+# Get the commit hash of the most recent common commit between the source and target branches
+COMMON_ANCESTOR_COMMIT=$(git merge-base $SOURCE_BRANCH $TARGET_BRANCH)
+
+echo "Comparing commit hash $SOURCE_BRANCH with common ancestor commit hash $COMMON_ANCESTOR_COMMIT"
+
 # Get the list of added snapshot files in the current merge request
-NEW_SNAP_FILES=$(git diff --name-only --diff-filter=A $TARGET_BRANCH $SOURCE_BRANCH -- "*.snap")
+NEW_SNAP_FILES=$(git diff --name-only --diff-filter=A $COMMON_ANCESTOR_COMMIT $SOURCE_BRANCH -- "*.snap")
 
 # Check if any new .snap files were found
 if [[ -n "$NEW_SNAP_FILES" && ! "$SOURCE_BRANCH_NAME" =~ revert ]]; then
