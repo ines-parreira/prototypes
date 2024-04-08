56775d4c88d492ac37d59da2fbd4aa144c067d6b
Xuan-thi Nguyen <xuan-thi@gorgias.com>
2024-04-08 16:55:14 +0200

    fix: add trimming to color value (#3750)

--- src/pages/common/components/TicketTag.tsx
+++ src/pages/common/components/TicketTag.tsx
@@ -3,6 +3,7 @@ import classNames from 'classnames'
 import {fromJS, Map} from 'immutable'
 import {parseToHsla} from 'color2k'
 import colors from '@gorgias/design-tokens/dist/tokens/colors.json'
+import _trim from 'lodash/trim'
 
 import {getEnoughContrastedColor} from 'utils/colors'
 import {Theme, ThemeContext} from 'theme'
@@ -18,13 +19,13 @@ type Props = {
 const TicketTag = ({children, className, decoration, title}: Props) => {
     const context = useContext(ThemeContext)
     const color =
-        (((decoration || fromJS({})) as Map<any, any>).get('color') as
+        ((((decoration || fromJS({})) as Map<any, any>).get('color') as
             | string
-            | null) ??
-        context?.colorTokens.Main.Secondary.value ??
+            | null) ||
+            context?.colorTokens.Main.Secondary.value) ??
         colors['🖥 Modern'].Main.Secondary.value
 
-    const [hue, saturation] = useMemo(() => parseToHsla(color), [color])
+    const [hue, saturation] = useMemo(() => parseToHsla(_trim(color)), [color])
     const backgroundColor = `hsla(${hue}, ${saturation * 100}%,`
     const textColorDark = useMemo(
         () => getEnoughContrastedColor(color, `${backgroundColor} 10%)`),
