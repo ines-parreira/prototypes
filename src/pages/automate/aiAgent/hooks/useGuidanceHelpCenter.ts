31ac010d80dd7a49417e905ef72bffa6772e8259
Ulad <130975351+ulad-ramanovich@users.noreply.github.com>
2024-05-17 15:57:13 +0200

    chore(ai-agent): notify about no guidance (#3951)
    * chore(ai-agent): notify aobut no guidance
    
    * fix(ai-agent): update text of notification
    
    * fix(ai-agent): typo

--- src/pages/automate/aiAgent/hooks/useGuidanceHelpCenter.ts
+++ src/pages/automate/aiAgent/hooks/useGuidanceHelpCenter.ts
@@ -1,5 +1,10 @@
+import {AI_AGENT_SENTRY_TEAM} from 'common/const/sentryTeamNames'
+import useAppDispatch from 'hooks/useAppDispatch'
 import {useGetHelpCenterList} from 'models/helpCenter/queries'
 import {HelpCenter} from 'models/helpCenter/types'
+import {notify} from 'state/notifications/actions'
+import {NotificationStatus} from 'state/notifications/types'
+import {reportError} from 'utils/errors'
 
 const FIVE_MINUTES = 1000 * 60 * 5
 
@@ -8,12 +13,36 @@ export const useGuidanceHelpCenter = ({
 }: {
     shopName: string
 }): HelpCenter | undefined => {
+    const dispatch = useAppDispatch()
+
     // We expect to handle only 1 guidance help center
     const {data} = useGetHelpCenterList(
         {type: 'guidance', per_page: 1, shop_name: shopName},
         {
             // Guidance Help Center is not expected to change frequently
             staleTime: FIVE_MINUTES,
+            onSuccess: (data) => {
+                const guidanceHelpCenter = data?.data.data[0]
+
+                if (!guidanceHelpCenter) {
+                    reportError(
+                        `Guidance Help Center not found for shop: ${shopName}`,
+                        {
+                            extra: {
+                                context:
+                                    'Error during fetching of guidance help center',
+                                tags: [AI_AGENT_SENTRY_TEAM],
+                            },
+                        }
+                    )
+                    void dispatch(
+                        notify({
+                            status: NotificationStatus.Error,
+                            message: `Guidance Help Center not found for shop: ${shopName}. Please try again or contact support.`,
+                        })
+                    )
+                }
+            },
         }
     )
     const guidanceHelpCenter = data?.data.data[0]
