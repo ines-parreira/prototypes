f5bc739cab534c559741103ba17baf825a67bd5c
Sufiyan <132895712+sufiyangorgias@users.noreply.github.com>
2024-05-27 11:43:37 +0200

    fix: update alert warnining (#4173)

--- src/pages/automate/workflows/editor/visualBuilder/publisher/helper/utils.ts
+++ src/pages/automate/workflows/editor/visualBuilder/publisher/helper/utils.ts
@@ -5,22 +5,21 @@ export const ChannelConfig = {
     [TicketChannel.Chat]: {
         settingsUrl: `/app/settings/channels/gorgias_chat`,
         linkText: 'Connect a Chat',
-        description: ' to this store to enable this Flow.',
+        description: ' to enable Automate features.',
         assetsUrl: assetsUrl('/img/workflows/chat_preview.png'),
         label: 'Chat',
     },
     [TicketChannel.HelpCenter]: {
         settingsUrl: `/app/settings/help-center`,
         linkText: 'Connect a Help Center',
-        description:
-            ' to this store to enable this Flow. Only available for Shopify stores.',
+        description: ' to enable Automate features.',
         assetsUrl: assetsUrl('/img/workflows/helpcenter_preview.png'),
         label: 'Help Center',
     },
     [TicketChannel.ContactForm]: {
         settingsUrl: `/app/settings/contact-form/forms`,
         linkText: 'Connect a Contact Form',
-        description: ' to your store to enable this Flow.',
+        description: ' to enable Automate features.',
         assetsUrl: assetsUrl('/img/workflows/contact_form_preview.png'),
         label: 'Contact Form',
     },
