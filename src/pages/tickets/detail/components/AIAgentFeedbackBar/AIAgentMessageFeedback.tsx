4cd71e2ce8c63be36eef759e06df81e8a7436c89
Botond Dombi <104018159+gorgias-botond-dombi@users.noreply.github.com>
2024-06-05 15:13:17 +0200

    feat: refactor resource links, add title for buttons, adjust css (#4285)

--- src/pages/tickets/detail/components/AIAgentFeedbackBar/AIAgentMessageFeedback.tsx
+++ src/pages/tickets/detail/components/AIAgentFeedbackBar/AIAgentMessageFeedback.tsx
@@ -9,6 +9,9 @@ import {
     SubmitMessageFeedback,
     isIssueFeedbackOnMessage,
     MessageFeedback,
+    Knowledge,
+    Guidance,
+    Action,
 } from 'models/aiAgentFeedback/types'
 
 import {getSelectedAIMessage} from 'state/ui/ticketAIAgentFeedback'
@@ -16,6 +19,7 @@ import {getSelectedAIMessage} from 'state/ui/ticketAIAgentFeedback'
 import IconButton from 'pages/common/components/button/IconButton'
 
 import useAppSelector from 'hooks/useAppSelector'
+import useHasAgentPrivileges from 'hooks/useHasAgentPrivileges'
 
 import {useAIAgentResources} from '../../hooks/useAIAgentResources'
 import {useAIAgentSendFeedback} from '../../hooks/useAIAgentSendFeedback'
@@ -32,6 +36,101 @@ import css from './AIAgentFeedbackBar.less'
 export const FEEDBACK_MESSAGE_ACTIONS_TEST_ID = 'feedback-message-actions'
 export const FEEDBACK_MESSAGE_GUIDANCE_TEST_ID = 'feedback-message-guidance'
 export const FEEDBACK_MESSAGE_KNOWLEDGE_TEST_ID = 'feedback-message-knowledge'
+
+type FeedbackResourceSectionProps = {
+    resource: (Knowledge | Guidance | Action) & {feedback: Feedback}
+    resourceType: FeedbackOnResource['resourceType']
+    handleSubmitFeedback: (
+        resourceId: number | string,
+        resourceType: FeedbackOnResource['resourceType'],
+        feedback: Feedback
+    ) => void
+    href?: string
+    dataTestId?: string
+}
+
+const FeedbackResourceSection: React.FC<FeedbackResourceSectionProps> = ({
+    resource,
+    resourceType,
+    handleSubmitFeedback,
+    href,
+    dataTestId,
+}) => {
+    const hasAgentPrivileges = useHasAgentPrivileges()
+
+    return (
+        <a
+            href={hasAgentPrivileges ? href : undefined}
+            target="_blank"
+            rel="noreferrer noopener"
+            className={classNames(css.section, {
+                [css.clickable]: hasAgentPrivileges,
+            })}
+            data-testid={dataTestId}
+        >
+            <div className={css.sectionText}>
+                <div>{resource.name}</div>
+                <i className={classNames('material-icons', css.openIcon)}>
+                    open_in_new
+                </i>
+            </div>
+            <div className={css.feedback}>
+                <IconButton
+                    fillStyle="fill"
+                    intent="secondary"
+                    size="small"
+                    iconClassName={
+                        resource.feedback === 'thumbs_up'
+                            ? 'material-icons'
+                            : 'material-icons-outlined'
+                    }
+                    className={classNames({
+                        [css.positiveFeedback]:
+                            resource.feedback === 'thumbs_up',
+                    })}
+                    onClick={() =>
+                        handleSubmitFeedback(
+                            resource.id,
+                            resourceType,
+                            resource.feedback === 'thumbs_up'
+                                ? null
+                                : 'thumbs_up'
+                        )
+                    }
+                    title="Mark as Correct"
+                >
+                    thumb_up
+                </IconButton>
+                <IconButton
+                    fillStyle="fill"
+                    intent="secondary"
+                    size="small"
+                    iconClassName={
+                        resource.feedback === 'thumbs_down'
+                            ? 'material-icons'
+                            : 'material-icons-outlined'
+                    }
+                    className={classNames({
+                        [css.negativeFeedback]:
+                            resource.feedback === 'thumbs_down',
+                    })}
+                    onClick={() =>
+                        handleSubmitFeedback(
+                            resource.id,
+                            resourceType,
+                            resource.feedback === 'thumbs_down'
+                                ? null
+                                : 'thumbs_down'
+                        )
+                    }
+                    title="Mark as Incorrect"
+                >
+                    thumb_down
+                </IconButton>
+            </div>
+        </a>
+    )
+}
 
 type Props = {
     messageFeedback: MessageFeedback
@@ -132,165 +231,37 @@ const AIAgentMessageFeedback: React.FC<Props> = ({messageFeedback}) => {
                 <div className={css.sectionContainer}>
                     <div className={css.subtitle}>Actions</div>
                     {actions?.map((action) => (
-                        <a
+                        <FeedbackResourceSection
                             key={action.id}
+                            resource={action}
+                            resourceType="action"
+                            handleSubmitFeedback={handleSubmitFeedback}
                             href={getActionUrl(
                                 action,
                                 messageFeedback.shopType,
                                 messageFeedback.shopName
                             )}
-                            target="_blank"
-                            rel="noreferrer noopener"
-                            className={classNames(css.section, css.clickable)}
-                            data-testid={FEEDBACK_MESSAGE_ACTIONS_TEST_ID}
-                        >
-                            <div className={css.sectionText}>
-                                <div>{action.name}</div>
-                                <i
-                                    className={classNames(
-                                        'material-icons',
-                                        css.openIcon
-                                    )}
-                                >
-                                    open_in_new
-                                </i>
-                            </div>
-                            <div className={css.feedback}>
-                                <IconButton
-                                    fillStyle="fill"
-                                    intent="secondary"
-                                    size="small"
-                                    iconClassName={
-                                        action.feedback === 'thumbs_up'
-                                            ? 'material-icons'
-                                            : 'material-icons-outlined'
-                                    }
-                                    className={classNames({
-                                        [css.positiveFeedback]:
-                                            action.feedback === 'thumbs_up',
-                                    })}
-                                    onClick={() =>
-                                        handleSubmitFeedback(
-                                            action.id,
-                                            'action',
-                                            action.feedback === 'thumbs_up'
-                                                ? null
-                                                : 'thumbs_up'
-                                        )
-                                    }
-                                >
-                                    thumb_up
-                                </IconButton>
-                                <IconButton
-                                    fillStyle="fill"
-                                    intent="secondary"
-                                    size="small"
-                                    iconClassName={
-                                        action.feedback === 'thumbs_down'
-                                            ? 'material-icons'
-                                            : 'material-icons-outlined'
-                                    }
-                                    className={classNames({
-                                        [css.negativeFeedback]:
-                                            action.feedback === 'thumbs_down',
-                                    })}
-                                    onClick={() =>
-                                        handleSubmitFeedback(
-                                            action.id,
-                                            'action',
-                                            action.feedback === 'thumbs_down'
-                                                ? null
-                                                : 'thumbs_down'
-                                        )
-                                    }
-                                >
-                                    thumb_down
-                                </IconButton>
-                            </div>
-                        </a>
+                            dataTestId={FEEDBACK_MESSAGE_ACTIONS_TEST_ID}
+                        />
                     ))}
                 </div>
             )}
             {guidance && guidance.length > 0 && (
                 <div className={css.sectionContainer}>
                     <div className={css.subtitle}>Guidance</div>
-                    {guidance?.map((guide) => (
-                        <a
-                            key={guide.id}
+                    {guidance?.map((guidance) => (
+                        <FeedbackResourceSection
+                            key={guidance.id}
+                            resource={guidance}
+                            resourceType="guidance"
+                            handleSubmitFeedback={handleSubmitFeedback}
                             href={getGuidanceUrl(
-                                guide,
+                                guidance,
                                 messageFeedback.shopType,
                                 messageFeedback.shopName
                             )}
-                            target="_blank"
-                            rel="noreferrer noopener"
-                            className={classNames(css.section, css.clickable)}
-                            data-testid={FEEDBACK_MESSAGE_GUIDANCE_TEST_ID}
-                        >
-                            <div className={css.sectionText}>
-                                <div>{guide.name}</div>
-                                <i
-                                    className={classNames(
-                                        'material-icons',
-                                        css.openIcon
-                                    )}
-                                >
-                                    open_in_new
-                                </i>
-                            </div>
-                            <div className={css.feedback}>
-                                <IconButton
-                                    fillStyle="fill"
-                                    intent="secondary"
-                                    size="small"
-                                    iconClassName={
-                                        guide.feedback === 'thumbs_up'
-                                            ? 'material-icons'
-                                            : 'material-icons-outlined'
-                                    }
-                                    className={classNames({
-                                        [css.positiveFeedback]:
-                                            guide.feedback === 'thumbs_up',
-                                    })}
-                                    onClick={() =>
-                                        handleSubmitFeedback(
-                                            guide.id,
-                                            'guidance',
-                                            guide.feedback === 'thumbs_up'
-                                                ? null
-                                                : 'thumbs_up'
-                                        )
-                                    }
-                                >
-                                    thumb_up
-                                </IconButton>
-                                <IconButton
-                                    fillStyle="fill"
-                                    intent="secondary"
-                                    size="small"
-                                    iconClassName={
-                                        guide.feedback === 'thumbs_down'
-                                            ? 'material-icons'
-                                            : 'material-icons-outlined'
-                                    }
-                                    className={classNames({
-                                        [css.negativeFeedback]:
-                                            guide.feedback === 'thumbs_down',
-                                    })}
-                                    onClick={() =>
-                                        handleSubmitFeedback(
-                                            guide.id,
-                                            'guidance',
-                                            guide.feedback === 'thumbs_down'
-                                                ? null
-                                                : 'thumbs_down'
-                                        )
-                                    }
-                                >
-                                    thumb_down
-                                </IconButton>
-                            </div>
-                        </a>
+                            dataTestId={FEEDBACK_MESSAGE_GUIDANCE_TEST_ID}
+                        />
                     ))}
                 </div>
             )}
@@ -298,83 +269,18 @@ const AIAgentMessageFeedback: React.FC<Props> = ({messageFeedback}) => {
                 <div className={css.sectionContainer}>
                     <div className={css.subtitle}>Knowledge</div>
                     {knowledge?.map((knowledge) => (
-                        <a
+                        <FeedbackResourceSection
                             key={knowledge.id}
+                            resource={knowledge}
+                            resourceType={knowledge.type}
+                            handleSubmitFeedback={handleSubmitFeedback}
                             href={getKnowledgeUrl(
                                 knowledge,
                                 messageFeedback.shopType,
                                 messageFeedback.shopName
                             )}
-                            target="_blank"
-                            rel="noreferrer noopener"
-                            className={classNames(css.section, css.clickable)}
-                            data-testid={FEEDBACK_MESSAGE_KNOWLEDGE_TEST_ID}
-                        >
-                            <div className={css.sectionText}>
-                                <div>{knowledge.name}</div>
-                                <i
-                                    className={classNames(
-                                        'material-icons',
-                                        css.openIcon
-                                    )}
-                                >
-                                    open_in_new
-                                </i>
-                            </div>
-                            <div className={css.feedback}>
-                                <IconButton
-                                    fillStyle="fill"
-                                    intent="secondary"
-                                    size="small"
-                                    iconClassName={
-                                        knowledge.feedback === 'thumbs_up'
-                                            ? 'material-icons'
-                                            : 'material-icons-outlined'
-                                    }
-                                    className={classNames({
-                                        [css.positiveFeedback]:
-                                            knowledge.feedback === 'thumbs_up',
-                                    })}
-                                    onClick={() =>
-                                        handleSubmitFeedback(
-                                            knowledge.id,
-                                            knowledge.type,
-                                            knowledge.feedback === 'thumbs_up'
-                                                ? null
-                                                : 'thumbs_up'
-                                        )
-                                    }
-                                >
-                                    thumb_up
-                                </IconButton>
-                                <IconButton
-                                    fillStyle="fill"
-                                    intent="secondary"
-                                    size="small"
-                                    iconClassName={
-                                        knowledge.feedback === 'thumbs_down'
-                                            ? 'material-icons'
-                                            : 'material-icons-outlined'
-                                    }
-                                    className={classNames({
-                                        [css.negativeFeedback]:
-                                            knowledge.feedback ===
-                                            'thumbs_down',
-                                    })}
-                                    onClick={() =>
-                                        handleSubmitFeedback(
-                                            knowledge.id,
-                                            knowledge.type,
-                                            knowledge.feedback === 'thumbs_down'
-                                                ? null
-                                                : 'thumbs_down'
-                                        )
-                                    }
-                                >
-                                    thumb_down
-                                </IconButton>
-                            </div>
-                        </a>
+                            dataTestId={FEEDBACK_MESSAGE_KNOWLEDGE_TEST_ID}
+                        />
                     ))}
                 </div>
             )}
