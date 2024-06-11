18ae801f4795d552dd5b2a2923ae2782baf269ec
Ulad <130975351+ulad-ramanovich@users.noreply.github.com>
2024-06-11 10:16:00 +0200

    chore(ai-agent): remove notif when no Help Center (#4329)

--- src/pages/automate/aiAgent/hooks/useAiAgentHelpCenter.ts
+++ src/pages/automate/aiAgent/hooks/useAiAgentHelpCenter.ts
@@ -1,9 +1,6 @@
 import {AI_AGENT_SENTRY_TEAM} from 'common/const/sentryTeamNames'
-import useAppDispatch from 'hooks/useAppDispatch'
 import {useGetHelpCenterList} from 'models/helpCenter/queries'
 import {HelpCenter} from 'models/helpCenter/types'
-import {notify} from 'state/notifications/actions'
-import {NotificationStatus} from 'state/notifications/types'
 import {reportError} from 'utils/errors'
 
 const FIVE_MINUTES = 1000 * 60 * 5
@@ -15,8 +12,6 @@ export const useAiAgentHelpCenter = ({
     shopName: string
     helpCenterType: 'guidance' | 'snippet'
 }): HelpCenter | undefined => {
-    const dispatch = useAppDispatch()
-
     // We expect to handle only 1 guidance help center
     const {data} = useGetHelpCenterList(
         {type: helpCenterType, per_page: 1, shop_name: shopName},
@@ -38,12 +33,6 @@ export const useAiAgentHelpCenter = ({
                             },
                             level: 'error',
                         }
-                    )
-                    void dispatch(
-                        notify({
-                            status: NotificationStatus.Error,
-                            message: `${helpCenterType} Help Center not found for shop: ${shopName}. Please try again or contact support.`,
-                        })
                     )
                 }
             },
