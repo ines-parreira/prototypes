a6d1ab57c3a7879ac177fe85faa1ddb24f200534
vladimir-barbuta-gorgias <156432573+vladimir-barbuta-gorgias@users.noreply.github.com>
2024-08-20 14:39:47 +0300

    fix(SLAs): [PLTDA-2770] changed svg to be a background and not actually absolute positioned (#4911)

--- src/pages/common/components/EmptyState/ImageContainer.tsx
+++ src/pages/common/components/EmptyState/ImageContainer.tsx
@@ -4,19 +4,6 @@ import css from 'pages/common/components/EmptyState/ImageContainer.less'
 export default function ImageContainer({children}: PropsWithChildren<unknown>) {
     return (
         <div className={css.container}>
-            <svg
-                className={css.svg}
-                xmlns="http://www.w3.org/2000/svg"
-                width="585"
-                height="781"
-                viewBox="0 0 585 781"
-                fill="none"
-            >
-                <path
-                    d="M585 390C585 638.528 383.528 840 135 840C6.19508 840 -238 840 -238 840V-60C-238 -60 -113.528 -60 135 -60C383.528 -60 585 141.472 585 390Z"
-                    fill="#F9F9F9"
-                />
-            </svg>
             <div className={css.content}>{children}</div>
         </div>
     )
--- src/pages/common/components/EmptyState/tests/ImageContainer.spec.tsx
+++ src/pages/common/components/EmptyState/tests/ImageContainer.spec.tsx
@@ -3,14 +3,6 @@ import {render, screen} from '@testing-library/react'
 import ImageContainer from 'pages/common/components/EmptyState/ImageContainer'
 
 describe('ImageContainer', () => {
-    it('should render svg', () => {
-        const children = <p>Children</p>
-        render(<ImageContainer>{children}</ImageContainer>)
-
-        const svg = document.querySelector('svg')
-        expect(svg).toBeInTheDocument()
-    })
-
     it('should render children', () => {
         const children = <p data-testid="image-container-children">Children</p>
         render(<ImageContainer>{children}</ImageContainer>)
