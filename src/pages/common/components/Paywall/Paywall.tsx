d1bf16760c6b12cedcdbedb44cde66991d016a5e
Xuan-thi Nguyen <xuan-thi@gorgias.com>
2024-08-29 11:29:20 +0200

    fix(Paywall): put svg in css (#5013)
    * refactor: put svg in css
    
    * test: update snapshots

--- src/pages/common/components/Paywall/Paywall.tsx
+++ src/pages/common/components/Paywall/Paywall.tsx
@@ -70,24 +70,17 @@ const Paywall = ({
                 ) : (
                     <PageHeader title={pageHeader} />
                 ))}
-
+            {!hideBubble && (
+                <div
+                    className={classnames(
+                        css.svg,
+                        css[paywallTheme?.toLowerCase()]
+                    )}
+                />
+            )}
             <Container fluid className={classnames(css.page)}>
                 <div className={css.paywallContainer}>
                     <Row className="align-items-center mx-0">
-                        {!hideBubble && (
-                            <svg
-                                className={classnames(
-                                    css.svg,
-                                    css[paywallTheme?.toLowerCase()]
-                                )}
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="824"
-                                height="auto"
-                                viewBox="0 0 824 900"
-                            >
-                                <path d="M0 0H374C622.528 0 824 201.472 824 450V450C824 698.528 622.528 900 374 900H0V0Z" />
-                            </svg>
-                        )}
                         <Col
                             xs={12}
                             lg={6}
