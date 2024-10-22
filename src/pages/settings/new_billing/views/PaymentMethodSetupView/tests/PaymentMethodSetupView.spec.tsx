8e7a9baa6c01e9a2b5c766e3930369674deb829d
Francisco Ignácio Fontoura Solla <francisco.solla@gmail.com>
2024-10-22 16:06:05 +0200

    chore(c/billing): [GROW-598] Improve PaymentMethodSetupView test (#5574)

--- src/pages/settings/new_billing/views/PaymentMethodSetupView/tests/PaymentMethodSetupView.spec.tsx
+++ src/pages/settings/new_billing/views/PaymentMethodSetupView/tests/PaymentMethodSetupView.spec.tsx
@@ -1,175 +1,144 @@
 import React from 'react'
-import {render, screen, waitFor} from '@testing-library/react'
+import {screen, waitFor} from '@testing-library/react'
+import MockAdapter from 'axios-mock-adapter'
+import {createBillingPaymentMethodSetup} from '@gorgias/api-client'
+import {fromJS} from 'immutable'
 import {assumeMock} from 'utils/testing'
-import {useBillingContact} from 'models/billing/queries'
-import {StripeElementsProvider} from 'pages/settings/new_billing/components/StripeElementsProvider/StripeElementsProvider'
-import {useHasCreditCard} from 'pages/settings/new_billing/views/PaymentMethodSetupView/hooks/useHasCreditCard'
-import {useSetupIntent} from 'pages/settings/new_billing/views/PaymentMethodSetupView/hooks/useSetupIntent'
+import client from 'models/api/resources'
+import {renderWithStoreAndQueryClientProvider} from 'tests/renderWithStoreAndQueryClientProvider'
+import {account} from 'fixtures/account'
+import {products} from 'fixtures/productPrices'
 import {PaymentMethodSetupView} from '../PaymentMethodSetupView'
 
-jest.mock(
-    'pages/settings/new_billing/views/PaymentMethodSetupView/hooks/useSetupIntent'
-)
-jest.mock(
-    'pages/settings/new_billing/views/PaymentMethodSetupView/hooks/useHasCreditCard'
-)
-jest.mock('models/billing/queries', () => ({
-    useBillingContact: jest.fn(),
+jest.mock('@stripe/react-stripe-js', () => ({
+    useStripe: jest.fn(() => ({confirmSetup: jest.fn()})),
+    useElements: jest.fn(() => ({getElement: jest.fn()})),
+    Elements: jest.fn(({children}) => (
+        <div data-testid="stripe-elements">{children}</div>
+    )),
+    PaymentElement: jest.fn(() => <div data-testid="stripe-payment-element" />),
+    AddressElement: jest.fn(() => <div data-testid="stripe-address-element" />),
 }))
-jest.mock(
-    'pages/settings/new_billing/components/StripeElementsProvider/StripeElementsProvider',
-    () => ({
-        StripeElementsProvider: jest.fn(({children}) => <div>{children}</div>),
-    })
-)
-jest.mock('pages/settings/new_billing/components/BackLink', () => () => (
-    <div>BackLink Component</div>
-))
-jest.mock(
-    'pages/settings/new_billing/components/EmailInputField/EmailInputField',
-    () => ({
-        EmailInputField: jest.fn(() => <div>EmailInputField Component</div>),
-    })
-)
-jest.mock(
-    'pages/settings/new_billing/components/StripeAddressElement/StripeAddressElement',
-    () => ({
-        StripeAddressElement: jest.fn(() => (
-            <div>StripeAddressElement Component</div>
-        )),
-    })
-)
-jest.mock(
-    'pages/settings/new_billing/views/PaymentMethodSetupView/components/StripePaymentElement/StripePaymentElement',
-    () => ({
-        StripePaymentElement: jest.fn(() => (
-            <div>StripePaymentElement Component</div>
-        )),
-    })
-)
-jest.mock(
-    'pages/settings/new_billing/views/PaymentMethodSetupView/components/VerificationChargeDisclaimer/VerificationChargeDisclaimer',
-    () => ({
-        VerificationChargeDisclaimer: jest.fn(() => (
-            <div>VerificationChargeDisclaimer Component</div>
-        )),
-    })
-)
-jest.mock(
-    'pages/settings/new_billing/views/PaymentMethodSetupView/components/Form/Form',
-    () => ({
-        Form: jest.fn(({children}) => <div>Form Component {children}</div>),
-    })
-)
+
 jest.mock('pages/common/components/Loader/Loader', () => () => (
-    <div>Loading...</div>
+    <div data-testid="loader" />
 ))
 
-assumeMock(useBillingContact).mockReturnValue({isLoading: false} as any)
+jest.mock('@gorgias/api-client')
+
+const mockedServer = new MockAdapter(client)
+
+const mockInitialStoreState = {
+    currentAccount: fromJS({account}),
+    billing: fromJS({
+        invoices: [],
+        products,
+        currentProductsUsage: {},
+    }),
+}
 
 describe('PaymentMethodSetupView', () => {
     it('should render Loader when setup intent is loading', () => {
-        assumeMock(useSetupIntent).mockReturnValue({isLoading: true} as any)
+        mockedServer.onGet('/api/billing/credit-card/').reply(200, {})
+        mockedServer.onGet('/api/billing/contact/').reply(200, {})
+        assumeMock(createBillingPaymentMethodSetup).mockResolvedValue(
+            new Promise(() => {})
+        )
 
-        assumeMock(useHasCreditCard).mockReturnValue({isSuccess: true} as any)
-
-        render(
+        renderWithStoreAndQueryClientProvider(
             <PaymentMethodSetupView
                 contactBilling={jest.fn()}
                 dispatchBillingError={jest.fn()}
             />
         )
 
-        expect(screen.getByText('Loading...')).toBeInTheDocument()
+        expect(screen.getByTestId('loader')).toBeInTheDocument()
     })
 
     it('should render Loader when hasCreditCard is loading', () => {
-        assumeMock(useSetupIntent).mockReturnValue({
-            isLoading: false,
-            clientSecret: 'mockClientSecret',
+        mockedServer
+            .onGet('/api/billing/credit-card/')
+            .reply(() => new Promise(() => {}))
+        mockedServer.onGet('/api/billing/contact/').reply(200, {})
+        assumeMock(createBillingPaymentMethodSetup).mockResolvedValue({
+            data: {client_secret: 'client-secret', id: 'id'},
         } as any)
-        assumeMock(useHasCreditCard).mockReturnValue({isLoading: true} as any)
 
-        render(
+        renderWithStoreAndQueryClientProvider(
             <PaymentMethodSetupView
                 contactBilling={jest.fn()}
                 dispatchBillingError={jest.fn()}
             />
         )
 
-        expect(screen.getByText('Loading...')).toBeInTheDocument()
+        expect(screen.getByTestId('loader')).toBeInTheDocument()
     })
 
     it('should render StripeElementsProvider and form when setup intent and credit card are available', async () => {
-        assumeMock(useSetupIntent).mockReturnValue({
-            isLoading: false,
-            clientSecret: 'mockClientSecret',
-        } as any)
-        assumeMock(useHasCreditCard).mockReturnValue({
-            isSuccess: true,
-            data: false,
+        mockedServer.onGet('/api/billing/credit-card/').reply(200, {})
+        mockedServer.onGet('/api/billing/contact/').reply(200, {})
+        assumeMock(createBillingPaymentMethodSetup).mockResolvedValue({
+            data: {client_secret: 'client-secret', id: 'id'},
         } as any)
 
-        render(
+        renderWithStoreAndQueryClientProvider(
             <PaymentMethodSetupView
                 contactBilling={jest.fn()}
                 dispatchBillingError={jest.fn()}
-            />
+            />,
+            mockInitialStoreState
         )
 
+        expect(screen.getByTestId('loader')).toBeInTheDocument()
+
         await waitFor(() => {
-            expect(StripeElementsProvider).toHaveBeenCalledWith(
-                expect.objectContaining({clientSecret: 'mockClientSecret'}),
-                {}
-            )
-            expect(screen.getByText('Form Component')).toBeInTheDocument()
+            expect(screen.queryByTestId('loader')).not.toBeInTheDocument()
+        })
+
+        await waitFor(() => {
+            expect(screen.getByTestId('stripe-elements')).toBeInTheDocument()
             expect(
-                screen.getByText('StripePaymentElement Component')
+                screen.getByTestId('stripe-payment-element')
             ).toBeInTheDocument()
             expect(
-                screen.getByText('VerificationChargeDisclaimer Component')
+                screen.getByTestId('stripe-address-element')
             ).toBeInTheDocument()
-            expect(
-                screen.getByText('EmailInputField Component')
-            ).toBeInTheDocument()
-            expect(
-                screen.getByText('StripeAddressElement Component')
-            ).toBeInTheDocument()
+            expect(screen.getByText('Email')).toBeVisible()
         })
     })
 
     it('should not render EmailInputField and StripeAddressElement if user has credit card', async () => {
-        assumeMock(useSetupIntent).mockReturnValue({
-            isLoading: false,
-            clientSecret: 'mockClientSecret',
+        mockedServer
+            .onGet('/api/billing/credit-card/')
+            .reply(200, {brand: 'visa'})
+        mockedServer.onGet('/api/billing/contact/').reply(200, {})
+        assumeMock(createBillingPaymentMethodSetup).mockResolvedValue({
+            data: {client_secret: 'client-secret', id: 'id'},
         } as any)
 
-        assumeMock(useHasCreditCard).mockReturnValue({
-            isSuccess: true,
-            data: true,
-        } as any)
-
-        render(
+        renderWithStoreAndQueryClientProvider(
             <PaymentMethodSetupView
                 contactBilling={jest.fn()}
                 dispatchBillingError={jest.fn()}
-            />
+            />,
+            mockInitialStoreState
         )
 
+        expect(screen.getByTestId('loader')).toBeInTheDocument()
+
         await waitFor(() => {
-            expect(screen.getByText('Form Component')).toBeInTheDocument()
+            expect(screen.queryByTestId('loader')).not.toBeInTheDocument()
+        })
+
+        await waitFor(() => {
+            expect(screen.getByTestId('stripe-elements')).toBeInTheDocument()
             expect(
-                screen.getByText('StripePaymentElement Component')
+                screen.getByTestId('stripe-payment-element')
             ).toBeInTheDocument()
             expect(
-                screen.getByText('VerificationChargeDisclaimer Component')
-            ).toBeInTheDocument()
-            expect(
-                screen.queryByText('EmailInputField Component')
+                screen.queryByTestId('stripe-address-element')
             ).not.toBeInTheDocument()
-            expect(
-                screen.queryByText('StripeAddressElement Component')
-            ).not.toBeInTheDocument()
+            expect(screen.queryByText('Email')).not.toBeInTheDocument()
         })
     })
 })
---
+++ src/tests/renderWithStoreAndQueryClientProvider.tsx
@@ -0,0 +1,26 @@
+import {render} from '@testing-library/react'
+import React, {ReactElement} from 'react'
+import {Provider} from 'react-redux'
+import {RootState} from 'state/types'
+import {mockQueryClientProvider} from 'tests/reactQueryTestingUtils'
+import {mockStore} from 'utils/testing'
+
+export const renderWithStoreAndQueryClientProvider = (
+    element: ReactElement,
+    state: Partial<RootState> = {}
+) => {
+    const store = mockStore(state)
+    const MockQueryClientProvider = mockQueryClientProvider()
+    return {
+        ...render(element, {
+            wrapper: ({children}: any) => (
+                <Provider store={store}>
+                    <MockQueryClientProvider>
+                        {children}
+                    </MockQueryClientProvider>
+                </Provider>
+            ),
+        }),
+        store,
+    }
+}
