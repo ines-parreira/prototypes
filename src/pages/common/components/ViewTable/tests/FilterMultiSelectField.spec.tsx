e7878c795c757aaf174c63b26a2e866908ec0f50
Maciej Mikulski <maciej.mikulski@gorgias.com>
2024-10-29 09:50:59 +0100

    chore(enzyme): [FE-447] Replaced enzyme with RTL test for FilterMultiSelectField. (#5585)

--- src/pages/common/components/ViewTable/tests/FilterMultiSelectField.spec.tsx
+++ src/pages/common/components/ViewTable/tests/FilterMultiSelectField.spec.tsx
@@ -1,13 +1,16 @@
-import {mount} from 'enzyme'
-import {Map} from 'immutable'
+import {render, screen, waitFor} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import {fromJS, Map} from 'immutable'
 import _noop from 'lodash/noop'
+
 import React from 'react'
 import {InferThunkActionCreatorType} from 'react-redux'
 
-import {fieldEnumSearch} from '../../../../../state/views/actions'
-import MultiSelectOptionsField from '../../../forms/MultiSelectOptionsField/MultiSelectOptionsField'
-import {Option} from '../../../forms/MultiSelectOptionsField/types'
-import {FilterMultiSelectField} from '../FilterMultiSelectField'
+import {FilterMultiSelectField} from 'pages/common/components/ViewTable/FilterMultiSelectField'
+
+import {fieldEnumSearch} from 'state/views/actions'
+
+import {FieldSearchResult} from 'state/views/types'
 
 describe('FilterMultiSelectField', () => {
     const defaultProps = {
@@ -18,67 +21,78 @@ describe('FilterMultiSelectField', () => {
         field: Map(),
         fieldEnumSearchCancellable: () => Promise.resolve(),
         cancelFieldEnumSearchCancellable: _noop,
-        mapSearchResults: () => [],
+        mapSearchResults: (searchResults: FieldSearchResult[]) =>
+            searchResults.map((res) => ({
+                value: res.id,
+                label: res.name,
+            })),
     }
 
-    it('should search for the values on mount', () => {
+    beforeEach(() => {
+        jest.useFakeTimers()
+    })
+    it('should search for the values on mount', async () => {
         const fieldEnumSearchSpy = (
             jest.fn() as jest.MockedFunction<
                 InferThunkActionCreatorType<typeof fieldEnumSearch>
             >
         ).mockResolvedValue(null)
-        mount(
+
+        render(
             <FilterMultiSelectField
                 {...defaultProps}
                 fieldEnumSearchCancellable={fieldEnumSearchSpy}
             />
         )
-        expect(fieldEnumSearchSpy).toBeCalled()
+
+        await waitFor(() => {
+            expect(fieldEnumSearchSpy).toBeCalled()
+        })
     })
 
-    // https://github.com/facebook/jest/issues/3465
-    it.skip('should debounce search on input change', () => {
-        type OnInputChange = (name: string) => void
+    it('should debounce search on input change', () => {
         const fieldEnumSearchSpy = (
             jest.fn() as jest.MockedFunction<
                 InferThunkActionCreatorType<typeof fieldEnumSearch>
             >
         ).mockResolvedValue(null)
-        const wrapper = mount(
+
+        render(
             <FilterMultiSelectField
                 {...defaultProps}
                 fieldEnumSearchCancellable={fieldEnumSearchSpy}
             />
         )
-        wrapper
-            .find(MultiSelectOptionsField)
-            .prop<OnInputChange>('onInputChange')('foo')
-        wrapper
-            .find(MultiSelectOptionsField)
-            .prop<OnInputChange>('onInputChange')('bar')
-        wrapper
-            .find(MultiSelectOptionsField)
-            .prop<OnInputChange>('onInputChange')('baz')
+        userEvent.paste(screen.getByRole('textbox'), 'foo')
+        userEvent.clear(screen.getByRole('textbox'))
+        userEvent.paste(screen.getByRole('textbox'), 'bar')
+        userEvent.clear(screen.getByRole('textbox'))
+        userEvent.paste(screen.getByRole('textbox'), 'baz')
         jest.runAllTimers()
+
         expect(fieldEnumSearchSpy).toHaveBeenCalledTimes(2) // two times, because first time on mount
         expect(fieldEnumSearchSpy).toHaveBeenLastCalledWith(Map(), 'baz')
     })
 
-    it('should reset search on change', () => {
+    it('should reset search on change', async () => {
         const fieldEnumSearchSpy = (
             jest.fn() as jest.MockedFunction<
                 InferThunkActionCreatorType<typeof fieldEnumSearch>
             >
-        ).mockResolvedValue(null)
-        const wrapper = mount(
+        ).mockResolvedValue(fromJS([{id: 123, name: '123'}]))
+
+        render(
             <FilterMultiSelectField
                 {...defaultProps}
                 fieldEnumSearchCancellable={fieldEnumSearchSpy}
             />
         )
-        wrapper
-            .find(MultiSelectOptionsField)
-            .prop<(options: Option[]) => void>('onChange')([])
+
+        await waitFor(() => {
+            userEvent.clear(screen.getByRole('textbox'))
+            userEvent.click(screen.getByRole('menuitem'))
+        })
+
         expect(fieldEnumSearchSpy).toHaveBeenCalledTimes(2) // first time on mount
     })
 })
