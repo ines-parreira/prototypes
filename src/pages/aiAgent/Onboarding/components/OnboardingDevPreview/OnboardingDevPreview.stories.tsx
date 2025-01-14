a2ce7606d8604f16fcc20ff6ad50abc23319959f
Jan Balaz <jan.balaz@gorgias.com>
2025-01-14 12:35:00 +0100

    feat(convai): add example structure for onboarding storybooks (#6246)

---
+++ src/pages/aiAgent/Onboarding/components/OnboardingDevPreview/OnboardingDevPreview.stories.tsx
@@ -0,0 +1,15 @@
+import {Meta, StoryObj} from '@storybook/react'
+
+import OnboardingDevPreview from 'pages/aiAgent/Onboarding/components/OnboardingDevPreview/OnboardingDevPreview'
+
+const storyConfig: Meta<typeof OnboardingDevPreview> = {
+    title: 'AI Agent/Onboarding/OnboardingDevPreview',
+    component: OnboardingDevPreview,
+}
+
+type Story = StoryObj<typeof OnboardingDevPreview>
+
+/** Default onboarding */
+export const Primary: Story = {}
+
+export default storyConfig
---
+++ src/pages/aiAgent/Onboarding/components/OnboardingDevPreview/OnboardingDevPreview.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const OnboardingDevPreview = () => {
+    return <div>I will once contain components here!</div>
+}
+
+export default OnboardingDevPreview
