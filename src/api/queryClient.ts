f7c426d43da9331f61f2cd5c98840c2a3eeb194f
Nicolas Toulemont <40027895+nicolastoulemont@users.noreply.github.com>
2025-02-11 15:35:15 +0100

    feat: add error status based retry policy for the query client (#6577)

--- src/api/queryClient.ts
+++ src/api/queryClient.ts
@@ -1,7 +1,31 @@
+/* istanbul ignore file */
 import {QueryCache, QueryClient} from '@tanstack/react-query'
+import {isAxiosError} from 'axios'
 
 export const queryCache = new QueryCache()
 
+const HTTP_STATUS_TO_NOT_RETRY = [401, 403, 404, 419, 429]
+
+const isRetryable = (error: unknown) => {
+    if (
+        isAxiosError(error) &&
+        !!error.response?.status &&
+        HTTP_STATUS_TO_NOT_RETRY.includes(error.response.status)
+    ) {
+        return false
+    }
+
+    return true
+}
+
 export const appQueryClient = new QueryClient({
     queryCache,
+    defaultOptions: {
+        queries: {
+            retry: (_, error) => isRetryable(error),
+        },
+        mutations: {
+            retry: (_, error) => isRetryable(error),
+        },
+    },
 })
