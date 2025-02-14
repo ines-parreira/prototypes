a87a87bb299f886797cf5f3a2f3644c807418902
Nicolas Toulemont <40027895+nicolastoulemont@users.noreply.github.com>
2025-02-14 16:45:09 +0100

    feat: add no retries on 500 + add the return delay (#6654)

--- src/api/queryClient.ts
+++ src/api/queryClient.ts
@@ -4,9 +4,9 @@ import {isAxiosError} from 'axios'
 
 export const queryCache = new QueryCache()
 
-const HTTP_STATUS_TO_NOT_RETRY = [401, 403, 404, 419, 429]
+const HTTP_STATUS_TO_NOT_RETRY = [401, 403, 404, 419, 429, 500]
 
-const isRetryable = (error: unknown) => {
+const isRetryable = (failureCount: number, error: unknown) => {
     if (
         isAxiosError(error) &&
         !!error.response?.status &&
@@ -15,17 +15,35 @@ const isRetryable = (error: unknown) => {
         return false
     }
 
-    return true
+    return failureCount <= 5
 }
 
 export const appQueryClient = new QueryClient({
     queryCache,
     defaultOptions: {
         queries: {
-            retry: (_, error) => isRetryable(error),
+            retry: (failureCount, error) => isRetryable(failureCount, error),
+            /**
+             * 1st failure: 2000ms (2 seconds)
+             * 2nd failure: 4000ms (4 seconds)
+             * 3rd failure: 8000ms (8 seconds)
+             * 4th failure: 16000ms (16 seconds)
+             * 5th failure: 32000ms (32 seconds)
+             */
+            retryDelay: (failureCount) =>
+                Math.min(1000 * 2 ** failureCount, 30000),
         },
         mutations: {
-            retry: (_, error) => isRetryable(error),
+            retry: (failureCount, error) => isRetryable(failureCount, error),
+            /**
+             * 1st failure: 2000ms (2 seconds)
+             * 2nd failure: 4000ms (4 seconds)
+             * 3rd failure: 8000ms (8 seconds)
+             * 4th failure: 16000ms (16 seconds)
+             * 5th failure: 32000ms (32 seconds)
+             */
+            retryDelay: (failureCount) =>
+                Math.min(1000 * 2 ** failureCount, 30000),
         },
     },
 })
