5389829e9caeae631a05181edf9c49142e449d97
Nicolas Toulemont <40027895+nicolastoulemont@users.noreply.github.com>
2025-02-21 13:11:21 +0100

    feat(react-query): remove mutation retries (#6730)

--- src/api/queryClient.ts
+++ src/api/queryClient.ts
@@ -4,7 +4,7 @@ import {isAxiosError} from 'axios'
 
 export const queryCache = new QueryCache()
 
-const HTTP_STATUS_TO_NOT_RETRY = [401, 403, 404, 419, 429, 500]
+const HTTP_STATUS_TO_NOT_RETRY = [400, 401, 403, 404, 419, 429]
 
 const isRetriable = (retryIndex: number, error: unknown) => {
     if (
@@ -22,17 +22,6 @@ export const appQueryClient = new QueryClient({
     queryCache,
     defaultOptions: {
         queries: {
-            retry: (retryIndex, error) => isRetriable(retryIndex, error),
-            /**
-             * 1st failure: 2000ms (2 seconds)
-             * 2nd failure: 4000ms (4 seconds)
-             * 3rd failure: 8000ms (8 seconds)
-             * 4th failure: 16000ms (16 seconds)
-             * 5th failure: 32000ms (32 seconds)
-             */
-            retryDelay: (retryIndex) => Math.min(1000 * 2 ** retryIndex, 30000),
-        },
-        mutations: {
             retry: (retryIndex, error) => isRetriable(retryIndex, error),
             /**
              * 1st failure: 2000ms (2 seconds)
