3b133abcca55b116f8f1c0f76bbb630f05da32c6
Andrei Don <redimp@gmail.com>
2025-02-25 17:22:17 +0200

    chore(formatting): Use prettier config from @gorgias/config (#6779)
    * Use prettier config from @gorgias/config
    * Bump to stable @gorgias/config

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,34 +1,3 @@
-import fs from 'node:fs'
-import path from 'node:path'
+import config from '@gorgias/config/prettier'
 
-const topLevelDir = import.meta.dirname ?? process.cwd()
-const srcDir = path.join(topLevelDir, 'src')
-
-const topLevelModules = fs
-    .readdirSync(srcDir, { withFileTypes: true })
-    .filter((entry) => entry.isDirectory() && !entry.name.includes('.'))
-    .map((entry) => entry.name)
-    .join('|')
-
-export default {
-    semi: false,
-    quoteProps: 'as-needed',
-    singleQuote: true,
-    arrowParens: 'always',
-    trailingComma: 'all',
-    bracketSpacing: true,
-    plugins: ['@trivago/prettier-plugin-sort-imports'],
-    importOrder: [
-        '^react$', // react
-        '^node:.+', // node stdlib
-        '<THIRD_PARTY_MODULES>', // external modules (npm packages)
-        '^@gorgias', // @gorgias modules
-        `^(${topLevelModules})`, // absolute modules
-        '^[./]((?!less).)*$', // relative imports
-        '.(css|less)$', // styles
-    ],
-    importOrderSeparation: true,
-    importOrderSideEffects: false,
-    importOrderSortSpecifiers: true,
-    importOrderCaseInsensitive: true,
-}
+export default config
