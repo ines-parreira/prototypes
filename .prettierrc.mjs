2bdd6805e92e228d6f1da62491de899cc55e63e2
Andrei Don <redimp@gmail.com>
2025-02-25 12:47:59 +0200

    hotfix(formatting): Fix prettier issue with vscode (#6770)

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,3 +1,34 @@
-import config from '@gorgias/config/prettier'
+import fs from 'node:fs'
+import path from 'node:path'
 
-export default config
+const topLevelDir = import.meta.dirname ?? process.cwd()
+const srcDir = path.join(topLevelDir, 'src')
+
+const topLevelModules = fs
+    .readdirSync(srcDir, { withFileTypes: true })
+    .filter((entry) => entry.isDirectory() && !entry.name.includes('.'))
+    .map((entry) => entry.name)
+    .join('|')
+
+export default {
+    semi: false,
+    quoteProps: 'as-needed',
+    singleQuote: true,
+    arrowParens: 'always',
+    trailingComma: 'all',
+    bracketSpacing: true,
+    plugins: ['@trivago/prettier-plugin-sort-imports'],
+    importOrder: [
+        '^react$', // react
+        '^node:.+', // node stdlib
+        '<THIRD_PARTY_MODULES>', // external modules (npm packages)
+        '^@gorgias', // @gorgias modules
+        `^(${topLevelModules})`, // absolute modules
+        '^[./]((?!less).)*$', // relative imports
+        '.(css|less)$', // styles
+    ],
+    importOrderSeparation: true,
+    importOrderSideEffects: false,
+    importOrderSortSpecifiers: true,
+    importOrderCaseInsensitive: true,
+}
