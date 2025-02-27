7bf1cf5d5f065560ef90ec24bc4926c5fdb671d4
Andrei Don <redimp@gmail.com>
2025-02-27 14:08:38 +0200

    fix(formatting): Bump @gorgias/config and update prettier config (#6821)
    * Bump @gorgias/config and update prettier config
    * Bump to stable version

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,3 +1,14 @@
-import config from '@gorgias/config/prettier'
+import path from 'node:path'
+import url from 'node:url'
+
+import { createConfig } from '@gorgias/config/prettier'
+
+// import.meta.dirname is not available in node < 20
+// replace with import.meta.dirname when node >= 20
+const srcDir = path.join(
+    path.dirname(url.fileURLToPath(import.meta.url)),
+    'src',
+)
+const config = createConfig({ srcDir })
 
 export default config
