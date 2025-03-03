7b7e657c1076f8661fc18a157e18aa45607e2bcd
Matteo Silvestro <matteosilvestro@live.it>
2025-03-03 11:27:42 +0100

    feat(i/phone): add new unanswered statuses metrics to CSV export (#6808)

--- src/services/reporting/tests/voiceOverviewReportingService.spec.ts
+++ src/services/reporting/tests/voiceOverviewReportingService.spec.ts
@@ -3,8 +3,10 @@ import { renderHook } from '@testing-library/react-hooks'
 import { agents } from 'fixtures/agents'
 import { tags } from 'fixtures/tag'
 import { getCsvFileNameWithDates } from 'hooks/reporting/support-performance/overview/useDownloadOverviewData'
+import { VoiceCallSegment } from 'models/reporting/cubes/VoiceCallCube'
 import { ReportingGranularity } from 'models/reporting/types'
-import { LegacyStatsFilters } from 'models/stat/types'
+import { LegacyStatsFilters, StatsFilters } from 'models/stat/types'
+import { LogicalOperatorEnum } from 'pages/stats/common/components/Filter/constants'
 import {
     VOICE_OVERVIEW_CALL_EXPERIENCE_REPORT_FILE_NAME,
     VOICE_OVERVIEW_CALL_VOLUME_REPORT_FILE_NAME,
@@ -13,8 +15,10 @@ import {
 import { useNewVoiceStatsFilters } from 'pages/stats/voice/hooks/useNewVoiceStatsFilters'
 import { useVoiceCallAverageTimeTrend } from 'pages/stats/voice/hooks/useVoiceCallAverageTimeTrend'
 import { useVoiceCallCountTrend } from 'pages/stats/voice/hooks/useVoiceCallCountTrend'
+import { VoiceCallAverageTimeMetric } from 'pages/stats/voice/models/types'
 import {
-    saveReport,
+    DEPRECATED_saveReport,
+    DEPRECATED_useVoiceOverviewReportData,
     useVoiceOverviewReportData,
 } from 'services/reporting/voiceOverviewReportingService'
 import { fromLegacyStatsFilters } from 'state/stats/utils'
@@ -30,7 +34,7 @@ const useVoiceCallAverageTimeTrendMock = assumeMock(
 jest.mock('pages/stats/voice/hooks/useNewVoiceStatsFilters')
 const useNewVoiceStatsFiltersMock = assumeMock(useNewVoiceStatsFilters)
 
-describe('voiceOverviewReportingService', () => {
+describe('DEPRECATED_useVoiceOverviewReportData', () => {
     const period = {
         start_datetime: '2023-12-11T00:00:00.000Z',
         end_datetime: '2023-12-11T23:59:59.999Z',
@@ -40,8 +44,8 @@ describe('voiceOverviewReportingService', () => {
         agents: [agents[0].id],
         tags: [tags[0].id],
     }
-    const dateSeries: Parameters<typeof saveReport>[1] = period
-    const data: Parameters<typeof saveReport>[0] = {
+    const dateSeries: Parameters<typeof DEPRECATED_saveReport>[1] = period
+    const data: Parameters<typeof DEPRECATED_saveReport>[0] = {
         averageWaitTimeTrend: {
             isFetching: false,
             isError: false,
@@ -92,18 +96,12 @@ describe('voiceOverviewReportingService', () => {
         },
     }
 
-    // const period = {
-    //     end_datetime: '2024-01-15',
-    //     start_datetime: '2024-01-08',
-    // }
-
     const fakeReport = 'someValue'
 
     it('should call saveReport with a report', () => {
         jest.spyOn(files, 'createCsv').mockReturnValue(fakeReport)
-        // const zipperMock = jest.spyOn(files, 'saveZippedFiles')
 
-        const result = saveReport(data, dateSeries)
+        const result = DEPRECATED_saveReport(data, dateSeries)
 
         expect(result).toEqual({
             files: {
@@ -148,7 +146,9 @@ describe('voiceOverviewReportingService', () => {
                 period,
                 VOICE_OVERVIEW_REPORT_FILE_NAME,
             )
-            const { result } = renderHook(() => useVoiceOverviewReportData())
+            const { result } = renderHook(() =>
+                DEPRECATED_useVoiceOverviewReportData(),
+            )
 
             expect(result.current).toEqual({
                 files: {
@@ -164,6 +164,180 @@ describe('voiceOverviewReportingService', () => {
                 fileName,
                 isLoading: false,
             })
+        })
+    })
+})
+
+describe('voiceOverviewReportingService', () => {
+    const period = {
+        start_datetime: '2023-12-11T00:00:00.000Z',
+        end_datetime: '2023-12-11T23:59:59.999Z',
+    }
+    const statsFilters: StatsFilters = {
+        period,
+        agents: {
+            values: [agents[0].id],
+            operator: LogicalOperatorEnum.ONE_OF,
+        },
+    }
+
+    const fakeReport = 'someValue'
+
+    describe('useVoiceOverviewReportData', () => {
+        it('should fetch and format data', () => {
+            useVoiceCallCountTrendMock.mockReturnValue({
+                data: { prevValue: 10, value: 15 },
+                isFetching: false,
+                isError: false,
+            })
+            useVoiceCallAverageTimeTrendMock.mockReturnValue({
+                data: { prevValue: 1, value: 2 },
+                isFetching: false,
+                isError: false,
+            })
+            useNewVoiceStatsFiltersMock.mockReturnValue({
+                cleanStatsFilters: statsFilters,
+                granularity: ReportingGranularity.Day,
+                userTimezone: 'UTC',
+                isAnalyticsNewFilters: true,
+            })
+
+            const createCsvMock = jest.spyOn(files, 'createCsv')
+            createCsvMock.mockReturnValue(fakeReport)
+
+            const fileName = getCsvFileNameWithDates(
+                period,
+                VOICE_OVERVIEW_REPORT_FILE_NAME,
+            )
+            const { result } = renderHook(() => useVoiceOverviewReportData())
+
+            expect(result.current).toEqual({
+                files: {
+                    [getCsvFileNameWithDates(
+                        period,
+                        VOICE_OVERVIEW_REPORT_FILE_NAME,
+                    )]: fakeReport,
+                },
+                fileName,
+                isLoading: false,
+            })
+            expect(createCsvMock).toHaveBeenCalledWith([
+                [' ', 'current period', 'previous period'],
+                ['Total calls', '15', '10'],
+                ['Outbound calls', '15', '10'],
+                ['Inbound calls', '15', '10'],
+                ['Unanswered calls', '15', '10'],
+                ['Missed calls', '15', '10'],
+                ['Cancelled calls', '15', '10'],
+                ['Abandoned calls', '15', '10'],
+                ['Average wait time', '2', '1'],
+                ['Average talk time', '2', '1'],
+            ])
+            expect(useVoiceCallCountTrendMock).toHaveBeenCalledTimes(7)
+            expect(useVoiceCallCountTrendMock).toHaveBeenNthCalledWith(
+                1,
+                statsFilters,
+                'UTC',
+            )
+            expect(useVoiceCallCountTrendMock).toHaveBeenNthCalledWith(
+                2,
+                statsFilters,
+                'UTC',
+                VoiceCallSegment.outboundCalls,
+            )
+            expect(useVoiceCallCountTrendMock).toHaveBeenNthCalledWith(
+                3,
+                statsFilters,
+                'UTC',
+                VoiceCallSegment.inboundCalls,
+            )
+            expect(useVoiceCallCountTrendMock).toHaveBeenNthCalledWith(
+                4,
+                statsFilters,
+                'UTC',
+                VoiceCallSegment.inboundUnansweredCalls,
+            )
+            expect(useVoiceCallCountTrendMock).toHaveBeenNthCalledWith(
+                5,
+                statsFilters,
+                'UTC',
+                VoiceCallSegment.inboundMissedCalls,
+            )
+            expect(useVoiceCallCountTrendMock).toHaveBeenNthCalledWith(
+                6,
+                statsFilters,
+                'UTC',
+                VoiceCallSegment.inboundCancelledCalls,
+            )
+            expect(useVoiceCallCountTrendMock).toHaveBeenNthCalledWith(
+                7,
+                statsFilters,
+                'UTC',
+                VoiceCallSegment.inboundAbandonedCalls,
+            )
+            expect(useVoiceCallAverageTimeTrendMock).toHaveBeenCalledTimes(2)
+            expect(useVoiceCallAverageTimeTrendMock).toHaveBeenNthCalledWith(
+                1,
+                VoiceCallAverageTimeMetric.WaitTime,
+                statsFilters,
+                'UTC',
+            )
+            expect(useVoiceCallAverageTimeTrendMock).toHaveBeenNthCalledWith(
+                2,
+                VoiceCallAverageTimeMetric.TalkTime,
+                statsFilters,
+                'UTC',
+            )
+        })
+
+        it('should fetch and format unavailable data', () => {
+            useVoiceCallCountTrendMock.mockReturnValue({
+                isFetching: false,
+                isError: false,
+            })
+            useVoiceCallAverageTimeTrendMock.mockReturnValue({
+                isFetching: false,
+                isError: false,
+            })
+            useNewVoiceStatsFiltersMock.mockReturnValue({
+                cleanStatsFilters: statsFilters,
+                granularity: ReportingGranularity.Day,
+                userTimezone: 'UTC',
+                isAnalyticsNewFilters: true,
+            })
+
+            const createCsvMock = jest.spyOn(files, 'createCsv')
+            createCsvMock.mockReturnValue(fakeReport)
+
+            const fileName = getCsvFileNameWithDates(
+                period,
+                VOICE_OVERVIEW_REPORT_FILE_NAME,
+            )
+            const { result } = renderHook(() => useVoiceOverviewReportData())
+
+            expect(result.current).toEqual({
+                files: {
+                    [getCsvFileNameWithDates(
+                        period,
+                        VOICE_OVERVIEW_REPORT_FILE_NAME,
+                    )]: fakeReport,
+                },
+                fileName,
+                isLoading: false,
+            })
+
+            expect(createCsvMock).toHaveBeenCalledWith([
+                [' ', 'current period', 'previous period'],
+                ['Total calls', 'N/A', 'N/A'],
+                ['Outbound calls', 'N/A', 'N/A'],
+                ['Inbound calls', 'N/A', 'N/A'],
+                ['Unanswered calls', 'N/A', 'N/A'],
+                ['Missed calls', 'N/A', 'N/A'],
+                ['Cancelled calls', 'N/A', 'N/A'],
+                ['Abandoned calls', 'N/A', 'N/A'],
+                ['Average wait time', 'N/A', 'N/A'],
+                ['Average talk time', 'N/A', 'N/A'],
+            ])
         })
     })
 })
--- src/services/reporting/voiceOverviewReportingService.ts
+++ src/services/reporting/voiceOverviewReportingService.ts
@@ -6,12 +6,16 @@ import { VoiceCallSegment } from 'models/reporting/cubes/VoiceCallCube'
 import { Period } from 'models/stat/types'
 import { formatMetricValue } from 'pages/stats/common/utils'
 import {
+    ABANDONED_CALLS_METRIC_TITLE,
     AVERAGE_TALK_TIME_METRIC_TITLE,
     AVERAGE_WAIT_TIME_METRIC_TITLE,
+    CANCELLED_CALLS_METRIC_TITLE,
     DEPRECATED_MISSED_CALLS_METRIC_TITLE,
     INBOUND_CALLS_METRIC_TITLE,
+    MISSED_CALLS_METRIC_TITLE,
     OUTBOUND_CALLS_METRIC_TITLE,
     TOTAL_CALLS_METRIC_TITLE,
+    UNANSWERED_CALLS_METRIC_TITLE,
     VOICE_OVERVIEW_CALL_EXPERIENCE_REPORT_FILE_NAME,
     VOICE_OVERVIEW_CALL_VOLUME_REPORT_FILE_NAME,
     VOICE_OVERVIEW_REPORT_FILE_NAME,
@@ -28,7 +32,7 @@ import {
 } from 'services/reporting/constants'
 import { createCsv } from 'utils/file'
 
-export interface VoiceReportData {
+interface DEPRECATED_VoiceReportData {
     averageWaitTimeTrend: MetricTrend
     averageTalkTimeTrend: MetricTrend
     totalCallsCountTrend: MetricTrend
@@ -37,7 +41,10 @@ export interface VoiceReportData {
     missedCallsCountTrend: MetricTrend
 }
 
-export const saveReport = (data: VoiceReportData, period: Period) => {
+export const DEPRECATED_saveReport = (
+    data: DEPRECATED_VoiceReportData,
+    period: Period,
+) => {
     const {
         averageWaitTimeTrend,
         averageTalkTimeTrend,
@@ -107,7 +114,7 @@ export const saveReport = (data: VoiceReportData, period: Period) => {
     }
 }
 
-export const useVoiceOverviewReportData = () => {
+export const DEPRECATED_useVoiceOverviewReportData = () => {
     const { cleanStatsFilters, userTimezone } = useNewVoiceStatsFilters()
 
     const averageWaitTimeTrend = useVoiceCallAverageTimeTrend(
@@ -156,6 +163,183 @@ export const useVoiceOverviewReportData = () => {
         outboundCallsCountTrend,
         inboundCallsCountTrend,
         missedCallsCountTrend,
+    ])
+
+    const isLoading = useMemo(() => {
+        return Object.values(exportableData).some((metric) => metric.isFetching)
+    }, [exportableData])
+
+    return {
+        ...DEPRECATED_saveReport(exportableData, cleanStatsFilters.period),
+        isLoading,
+    }
+}
+
+interface VoiceReportData {
+    totalCallsCountTrend: MetricTrend
+    outboundCallsCountTrend: MetricTrend
+    inboundCallsCountTrend: MetricTrend
+    unansweredCallsCountTrend: MetricTrend
+    missedCallsCountTrend: MetricTrend
+    cancelledCallsCountTrend: MetricTrend
+    abandonedCallsCountTrend: MetricTrend
+    averageWaitTimeTrend: MetricTrend
+    averageTalkTimeTrend: MetricTrend
+}
+
+const saveReport = (data: VoiceReportData, period: Period) => {
+    const {
+        totalCallsCountTrend,
+        outboundCallsCountTrend,
+        inboundCallsCountTrend,
+        unansweredCallsCountTrend,
+        missedCallsCountTrend,
+        cancelledCallsCountTrend,
+        abandonedCallsCountTrend,
+        averageWaitTimeTrend,
+        averageTalkTimeTrend,
+    } = data
+
+    const formatIntegerValue = (value?: number | null) =>
+        value !== undefined
+            ? formatMetricValue(value, 'integer')
+            : NOT_AVAILABLE_LABEL
+    const formatDecimalValue = (value?: number | null) =>
+        value ? formatMetricValue(value) : NOT_AVAILABLE_LABEL
+
+    const voiceOverviewData = [
+        [EMPTY_LABEL, CURRENT_PERIOD_LABEL, PREVIOUS_PERIOD_LABEL],
+        [
+            TOTAL_CALLS_METRIC_TITLE,
+            formatIntegerValue(totalCallsCountTrend.data?.value),
+            formatIntegerValue(totalCallsCountTrend.data?.prevValue),
+        ],
+        [
+            OUTBOUND_CALLS_METRIC_TITLE,
+            formatIntegerValue(outboundCallsCountTrend.data?.value),
+            formatIntegerValue(outboundCallsCountTrend.data?.prevValue),
+        ],
+        [
+            INBOUND_CALLS_METRIC_TITLE,
+            formatIntegerValue(inboundCallsCountTrend.data?.value),
+            formatIntegerValue(inboundCallsCountTrend.data?.prevValue),
+        ],
+        [
+            UNANSWERED_CALLS_METRIC_TITLE,
+            formatIntegerValue(unansweredCallsCountTrend.data?.value),
+            formatIntegerValue(unansweredCallsCountTrend.data?.prevValue),
+        ],
+        [
+            MISSED_CALLS_METRIC_TITLE,
+            formatIntegerValue(missedCallsCountTrend.data?.value),
+            formatIntegerValue(missedCallsCountTrend.data?.prevValue),
+        ],
+        [
+            CANCELLED_CALLS_METRIC_TITLE,
+            formatIntegerValue(cancelledCallsCountTrend.data?.value),
+            formatIntegerValue(cancelledCallsCountTrend.data?.prevValue),
+        ],
+        [
+            ABANDONED_CALLS_METRIC_TITLE,
+            formatIntegerValue(abandonedCallsCountTrend.data?.value),
+            formatIntegerValue(abandonedCallsCountTrend.data?.prevValue),
+        ],
+        [
+            AVERAGE_WAIT_TIME_METRIC_TITLE,
+            formatDecimalValue(averageWaitTimeTrend.data?.value),
+            formatDecimalValue(averageWaitTimeTrend.data?.prevValue),
+        ],
+        [
+            AVERAGE_TALK_TIME_METRIC_TITLE,
+            formatDecimalValue(averageTalkTimeTrend.data?.value),
+            formatDecimalValue(averageTalkTimeTrend.data?.prevValue),
+        ],
+    ]
+
+    const downloadFileName = getCsvFileNameWithDates(
+        period,
+        VOICE_OVERVIEW_REPORT_FILE_NAME,
+    )
+
+    return {
+        files: {
+            [getCsvFileNameWithDates(period, VOICE_OVERVIEW_REPORT_FILE_NAME)]:
+                createCsv(voiceOverviewData),
+        },
+        fileName: downloadFileName,
+    }
+}
+
+export const useVoiceOverviewReportData = () => {
+    const { cleanStatsFilters, userTimezone } = useNewVoiceStatsFilters()
+
+    const totalCallsCountTrend = useVoiceCallCountTrend(
+        cleanStatsFilters,
+        userTimezone,
+    )
+    const outboundCallsCountTrend = useVoiceCallCountTrend(
+        cleanStatsFilters,
+        userTimezone,
+        VoiceCallSegment.outboundCalls,
+    )
+    const inboundCallsCountTrend = useVoiceCallCountTrend(
+        cleanStatsFilters,
+        userTimezone,
+        VoiceCallSegment.inboundCalls,
+    )
+    const unansweredCallsCountTrend = useVoiceCallCountTrend(
+        cleanStatsFilters,
+        userTimezone,
+        VoiceCallSegment.inboundUnansweredCalls,
+    )
+    const missedCallsCountTrend = useVoiceCallCountTrend(
+        cleanStatsFilters,
+        userTimezone,
+        VoiceCallSegment.inboundMissedCalls,
+    )
+    const cancelledCallsCountTrend = useVoiceCallCountTrend(
+        cleanStatsFilters,
+        userTimezone,
+        VoiceCallSegment.inboundCancelledCalls,
+    )
+    const abandonedCallsCountTrend = useVoiceCallCountTrend(
+        cleanStatsFilters,
+        userTimezone,
+        VoiceCallSegment.inboundAbandonedCalls,
+    )
+    const averageWaitTimeTrend = useVoiceCallAverageTimeTrend(
+        VoiceCallAverageTimeMetric.WaitTime,
+        cleanStatsFilters,
+        userTimezone,
+    )
+    const averageTalkTimeTrend = useVoiceCallAverageTimeTrend(
+        VoiceCallAverageTimeMetric.TalkTime,
+        cleanStatsFilters,
+        userTimezone,
+    )
+
+    const exportableData = useMemo(() => {
+        return {
+            totalCallsCountTrend,
+            outboundCallsCountTrend,
+            inboundCallsCountTrend,
+            unansweredCallsCountTrend,
+            missedCallsCountTrend,
+            cancelledCallsCountTrend,
+            abandonedCallsCountTrend,
+            averageWaitTimeTrend,
+            averageTalkTimeTrend,
+        }
+    }, [
+        totalCallsCountTrend,
+        outboundCallsCountTrend,
+        inboundCallsCountTrend,
+        unansweredCallsCountTrend,
+        missedCallsCountTrend,
+        cancelledCallsCountTrend,
+        abandonedCallsCountTrend,
+        averageWaitTimeTrend,
+        averageTalkTimeTrend,
     ])
 
     const isLoading = useMemo(() => {
