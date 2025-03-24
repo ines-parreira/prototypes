be6f592dcc24bd9d5ad7949e823d5dc27e439c1a
Anissa BENZINA <anissa.benzina@gorgias.com>
2025-03-24 17:21:42 +0100

    feat(reporting/rbac) [CRMREP-3550] disabled RBAC for Dossier (#7265)

--- src/pages/stats/report-chart-restrictions/config.ts
+++ src/pages/stats/report-chart-restrictions/config.ts
@@ -1,9 +1,7 @@
 import { UserRole } from 'config/types/user'
 import { ReportsIDs } from 'pages/stats/dashboards/constants'
-import { BASE_STATS_PATH } from 'routes/constants'
 
 const LOOPY_CASES_ACCOUNT_ID = 141994
-const DOSSIER_CASES_ACCOUNT_ID = 12945
 
 export enum RestrictedComponentType {
     Module = 'module',
@@ -46,13 +44,6 @@ export const RBAC_RESTRICTIONS: RestrictionsPerCustomer = {
             ],
             type: RestrictedComponentType.Report,
             role: UserRole.Agent,
-        },
-    ],
-    [DOSSIER_CASES_ACCOUNT_ID]: [
-        {
-            ids: [BASE_STATS_PATH],
-            type: RestrictedComponentType.Module,
-            role: UserRole.Admin,
         },
     ],
 }
