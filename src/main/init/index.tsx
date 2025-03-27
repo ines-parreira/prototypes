49f34265813b872c2a9e0102f6624da3b623c685
nasef-ferjani <nasef.ferjani@gorgias.com>
2025-03-27 13:29:42 +0100

    feat:fe-883: add react-scan helpdesk-web-app (#7309)

--- src/main/init/index.tsx
+++ src/main/init/index.tsx
@@ -1,3 +1,6 @@
+// Import react-scan BEFORE React or ReactDOM
+import { scan } from 'react-scan'
+
 // in order to prevent issues with overriding third-party (bootstrap)
 // styles, it is important that this import remain here as the very
 // first thing that is imported. This ensures they are loaded first
@@ -8,6 +11,8 @@ import './initNotifications'
 import './initQueryClient'
 import './initSocketManager'
 
+import React from 'react'
+
 import { render } from 'react-dom'
 
 import 'init'
@@ -16,6 +21,20 @@ import { store } from 'common/store'
 import Root from 'pages/Root'
 // eslint-disable-next-line
 import mobileScrollManager from 'services/mobileScrollManager.js'
+
+// Only import and run scan in development
+if (process.env.NODE_ENV === 'development') {
+    const whyDidYouRender = require('@welldone-software/why-did-you-render')
+    whyDidYouRender(React, {
+        trackAllPureComponents: false,
+    })
+
+    scan({
+        enabled: true,
+        log: false,
+        trackUnnecessaryRenders: true,
+    })
+}
 
 const container = document.getElementById('App')
 
