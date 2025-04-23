1a9e4da41d93c9f0fd5307bc9f67ef09a2482420
Andrei Don <redimp@gmail.com>
2025-04-23 10:45:44 +0300

    Revert "chore(realtime): Wrap typing status and forward error status to sentry (#7664)" (#7672)

--- src/pages/tickets/detail/components/ReplyArea/withTypingActivity.tsx
+++ src/pages/tickets/detail/components/ReplyArea/withTypingActivity.tsx
@@ -1,24 +1,13 @@
 import React, { useCallback, useEffect, useState } from 'react'
 
-import { debounce, isObject, toPlainObject } from 'lodash'
+import { debounce } from 'lodash'
 
 import { useAgentActivity } from '@gorgias/realtime'
 
 import { TYPING_ACTIVITY_AGENT_TIMEOUT_MS } from 'state/newMessage/constants'
-import { reportError } from 'utils/errors'
 
 export type TypingActivityProps = {
     handleTypingActivity: () => void
-}
-
-const handlePubNubError = (error: unknown) => {
-    const status =
-        isObject(error) && 'status' in error
-            ? toPlainObject(error?.status)
-            : undefined
-    reportError(new Error('Realtime typing status error'), {
-        extra: { status },
-    })
 }
 
 export default function withTypingActivity<P>(
@@ -36,33 +25,17 @@ export default function withTypingActivity<P>(
             [],
         )
 
-        const handleStartTyping = useCallback(async () => {
-            if (isTyping) {
-                try {
-                    await startTyping()
-                } catch (error) {
-                    handlePubNubError(error)
-                }
-            }
-        }, [isTyping, startTyping])
-
-        const handleStopTyping = useCallback(async () => {
-            if (isTyping) {
-                try {
-                    await stopTyping()
-                } catch (error) {
-                    handlePubNubError(error)
-                }
-            }
-        }, [isTyping, stopTyping])
-
         useEffect(() => {
-            handleStartTyping()
+            if (isTyping) {
+                startTyping()
+            }
 
             return () => {
-                handleStopTyping()
+                if (isTyping) {
+                    stopTyping()
+                }
             }
-        }, [handleStartTyping, handleStopTyping])
+        }, [isTyping, startTyping, stopTyping])
 
         const handleTypingActivity = useCallback(() => {
             setIsTyping(true)
