2503e83e7ef92a3796e654e414f77bf318edc830
vladgardusgorgias <alecsandru-vlad.gardus@gorgias.com>
2025-06-26 11:23:44 +0300

    fix: increased cache and stale time; only call this query if the ticket creation date is after the first time we consumed events (#8549)

--- src/common/utils/useIsTicketAfterFeedbackCollectionPeriod.ts
+++ src/common/utils/useIsTicketAfterFeedbackCollectionPeriod.ts
@@ -7,6 +7,13 @@ import { useGetAiAgentFeedback } from 'models/aiAgentFeedback/queries'
 import { useGetEarliestExecution } from 'models/knowledgeService/queries'
 import { getTicketState } from 'state/ticket/selectors'
 
+// we want to show feedback only after a given date when we started consuming orchestration events
+// reason is that prior to that date we don't have the data we need to gather feedback for resources based on the new design
+// so we fallback to the old feedback collection mechanism
+export const FIRST_CONSUMED_ORCH_EVENT_DATETIME = new Date(
+    '2025-05-23T10:00:00Z',
+)
+
 export const NUMBER_OF_MONTHS_TO_SKIP_NEW_FEEDBACK_COLLECTION = 3
 const CACHE_TIME = 1000 * 60 * 60 * 1 // 1 hour
 const STALE_TIME = 1000 * 60 * 60 * 1 // 1 hour
@@ -25,7 +32,16 @@ export const useTicketIsAfterFeedbackCollectionPeriod = () => {
         enabled: isNewAgenticArchitectureEnabled,
     })
 
-    const { data: earliestExecution } = useGetEarliestExecution()
+    const { data: earliestExecution } = useGetEarliestExecution({
+        refetchOnWindowFocus: false,
+        cacheTime: CACHE_TIME,
+        staleTime: STALE_TIME,
+        enabled:
+            isNewAgenticArchitectureEnabled &&
+            createdDatetime &&
+            new Date(createdDatetime).getTime() >
+                FIRST_CONSUMED_ORCH_EVENT_DATETIME.getTime(),
+    })
 
     return useMemo(() => {
         if (!isNewAgenticArchitectureEnabled || !earliestExecution) return false
