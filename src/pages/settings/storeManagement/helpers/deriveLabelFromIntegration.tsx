51dc2188177a0de9db05e0437b65cce631006c70
Boris C. <7647317+bcupac@users.noreply.github.com>
2025-06-27 09:56:43 +0200

    feat(MultiStore) use name as label for help center and contact form (#8566)

--- src/pages/settings/storeManagement/helpers/deriveLabelFromIntegration.tsx
+++ src/pages/settings/storeManagement/helpers/deriveLabelFromIntegration.tsx
@@ -1,6 +1,12 @@
 import { Integration } from 'models/integration/types'
 
+import { isContactFormChannel, isHelpCenterChannel } from './isIntegration'
+
 export default function deriveLabelFromIntegration(integration: Integration) {
+    if (isHelpCenterChannel(integration) || isContactFormChannel(integration)) {
+        return integration.name
+    }
+
     return (
         'address' in integration.meta
             ? integration.meta.address
--- src/pages/settings/storeManagement/helpers/tests/deriveLabelFromIntegration.spec.ts
+++ src/pages/settings/storeManagement/helpers/tests/deriveLabelFromIntegration.spec.ts
@@ -29,4 +29,31 @@ describe('deriveLabelFromIntegration', () => {
         const result = deriveLabelFromIntegration(mockIntegration)
         expect(result).toBe('Test Facebook')
     })
+
+    it('should return integration name for help center channel', () => {
+        const mockIntegration = {
+            id: 1,
+            name: 'Help Center',
+            type: IntegrationType.App,
+            meta: {
+                address: 'help-center|1234',
+            },
+        } as Integration
+
+        const result = deriveLabelFromIntegration(mockIntegration)
+        expect(result).toBe('Help Center')
+    })
+    it('should return integration name for contact form channel', () => {
+        const mockIntegration = {
+            id: 1,
+            name: 'Contact Form',
+            type: IntegrationType.App,
+            meta: {
+                address: 'contact-form|1234',
+            },
+        } as Integration
+
+        const result = deriveLabelFromIntegration(mockIntegration)
+        expect(result).toBe('Contact Form')
+    })
 })
