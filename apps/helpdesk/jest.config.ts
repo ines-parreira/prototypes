3e7155dcb0530f15557cd998e34d2b11033f28c1
Boris C. <7647317+bcupac@users.noreply.github.com>
2025-08-04 11:36:51 +0200

    chore: fix webstorm ui test runner (#9088)

--- apps/helpdesk/jest.config.ts
+++ apps/helpdesk/jest.config.ts
@@ -10,6 +10,7 @@ const config: Config = {
         uuid: 'uuid',
         'react-markdown': '<rootDir>/tests/__mocks__/react-markdown.ts',
         'rehype-raw': '<rootDir>/tests/__mocks__/rehype-raw.ts',
+        tabbable: '<rootDir>/tests/__mocks__/tabbable.js',
     },
     setupFiles: ['jest-launchdarkly-mock', 'construct-style-sheets-polyfill'],
     // A list of paths to modules that run some code to configure or set up the testing framework before each test
