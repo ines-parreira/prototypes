d2ceef2488a8ed42b8fc0363ed484a5bf5b86a4f
Nicolas Toulemont <40027895+nicolastoulemont@users.noreply.github.com>
2025-08-20 11:58:08 +0200

    feat(platform): fix vitest based packages tests results upload (#9377)
    * feat: testing without vitest config changes
    
    * feat: fix test result upload
    
    * feat: remove the comments needed to trigger CICD steps
    
    * feat: remove undeed cicd change

--- config/src/vitest/config.ts
+++ config/src/vitest/config.ts
@@ -9,6 +9,13 @@ export function createConfig(overrides?: Config): Config {
                 globals: true,
                 setupFiles: ['@repo/config/vitest/setup.ts'],
                 environment: 'jsdom',
+
+                // Required for the Codecov tests results to be uploaded
+                // These are needed for the Codecov tests analytics like flakiness reports for example
+                reporters: process.env['CI']
+                    ? ['default', 'junit']
+                    : ['default'],
+                outputFile: process.env['CI'] ? './junit.xml' : undefined,
             },
         },
         overrides ?? {},
