abe1c2b7659982c115771ad08e39500dd64224d1
TimB-Gorgias <timothy.brookes@gorgias.com>
2025-08-29 13:54:11 +0100

    chore(billing): aligned SummaryTotal tests with cadence typing (#9515)

--- apps/helpdesk/src/pages/settings/new_billing/components/CancelProductModal/UI/tests/SummaryTotal.spec.tsx
+++ apps/helpdesk/src/pages/settings/new_billing/components/CancelProductModal/UI/tests/SummaryTotal.spec.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 import { render } from '@testing-library/react'
 
 import { Cadence } from 'models/billing/types'
@@ -7,12 +5,15 @@ import { Cadence } from 'models/billing/types'
 import SummaryTotal from '../SummaryTotal'
 
 describe('SummaryTotal', () => {
-    it('renders with total', () => {
-        const { container } = render(
-            <SummaryTotal total={100} cadence={Cadence.Month} />,
-        )
+    it.each(Object.values(Cadence))(
+        'renders with total: (cadence=%s)',
+        (cadence: Cadence) => {
+            const { container } = render(
+                <SummaryTotal total={100} cadence={cadence} />,
+            )
 
-        expect(container).toHaveTextContent('Total with plan updates')
-        expect(container).toHaveTextContent('$100/month')
-    })
+            expect(container).toHaveTextContent('Total with plan updates')
+            expect(container).toHaveTextContent(`$100/${cadence}`)
+        },
+    )
 })
