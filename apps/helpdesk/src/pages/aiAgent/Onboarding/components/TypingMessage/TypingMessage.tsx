0d7b801357acb3b7e9db429f85cee913f0fc03aa
Arthur Suh Balduini <34691066+arthurbalduini@users.noreply.github.com>
2025-09-08 11:34:13 +0200

    ref: improves TypingMessage API (#9691)

--- apps/helpdesk/src/pages/aiAgent/Onboarding/components/TypingMessage/TypingMessage.tsx
+++ apps/helpdesk/src/pages/aiAgent/Onboarding/components/TypingMessage/TypingMessage.tsx
@@ -2,9 +2,15 @@ import css from './TypingMessage.less'
 
 export type TypingMessageProps = {
     color: string
+    indicatorColor?: string
+    customStyle?: React.CSSProperties
 }
 
-const TypingMessage = ({ color }: TypingMessageProps) => {
+const TypingMessage = ({
+    color,
+    indicatorColor = '#fff',
+    customStyle,
+}: TypingMessageProps) => {
     return (
         <div className="d-flex flex-column">
             <div
@@ -12,12 +18,16 @@ const TypingMessage = ({ color }: TypingMessageProps) => {
                 data-testid="typing-message-bubble"
                 style={{
                     backgroundColor: color,
+                    ...customStyle,
                 }}
             >
                 <div className={css.typingIndicator}>
-                    <span></span>
-                    <span></span>
-                    <span></span>
+                    {Array.from({ length: 3 }).map((_, index) => (
+                        <span
+                            key={`indicator-${index}`}
+                            style={{ backgroundColor: indicatorColor }}
+                        ></span>
+                    ))}
                 </div>
             </div>
         </div>
