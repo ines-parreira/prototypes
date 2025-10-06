b51f5dc8c9612aa23567eb53636be5c5e586ec99
Boris C. <7647317+bcupac@users.noreply.github.com>
2025-10-06 12:50:14 +0200

    feat(metafields): explainer (#9688)
    * feat: metafields ingestion flag
    
    * feat: render based on flag
    
    * feat: routring
    
    * chore: tests
    
    * chore: lint
    
    * feat: add explainer
    
    * feat: new copy
    
    * feat: update with latest ux
    
    * chore: lint

--- apps/helpdesk/src/pages/settings/storeManagement/storeDetailsPage/ShopifyMetafields/ShopifyMetafields.tsx
+++ apps/helpdesk/src/pages/settings/storeManagement/storeDetailsPage/ShopifyMetafields/ShopifyMetafields.tsx
@@ -1,3 +1,15 @@
+import { Text } from '@gorgias/axiom'
+
+import styles from './ShopifyMetafields.less'
+
 export default function ShopifyMetafields() {
-    return <div>ShopifyMetafields</div>
+    return (
+        <div className={styles.explainerContainer}>
+            <Text variant="regular" as="p" size="md">
+                Manage <span className={styles.metafields}>metafields</span>{' '}
+                from your Shopify store to Gorgias and choose which ones to show
+                in each customer&apos;s profile.
+            </Text>
+        </div>
+    )
 }
