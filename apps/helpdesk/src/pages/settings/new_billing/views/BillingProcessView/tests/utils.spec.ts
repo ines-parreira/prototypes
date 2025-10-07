ca3cd317c40510657a58fbcb18e4267387db7108
TimB-Gorgias <timothy.brookes@gorgias.com>
2025-10-07 16:06:12 +0100

    feat(billing): `BillingProcessView` tests now dynamically handle cadences (#10000)

--- apps/helpdesk/src/pages/settings/new_billing/views/BillingProcessView/tests/utils.spec.ts
+++ apps/helpdesk/src/pages/settings/new_billing/views/BillingProcessView/tests/utils.spec.ts
@@ -1,8 +1,9 @@
 import {
+    automationProduct,
     basicMonthlyAutomationPlan,
     basicMonthlyHelpdeskPlan,
 } from 'fixtures/productPrices'
-import { Cadence } from 'models/billing/types'
+import { AutomatePlan, Cadence } from 'models/billing/types'
 
 import { setAutomationNotification, setHelpdeskNotification } from '../utils'
 
@@ -81,55 +82,72 @@ describe('setAutomationNotification', () => {
         expect(notification?.buttons?.[0].name).toBe('Set Up AI Agent')
     })
 
-    it('renders upgrade message when new product amount is greater', () => {
-        const oldPlan = basicMonthlyAutomationPlan // amount is 3000
-        const newPlan = {
-            ...basicMonthlyAutomationPlan,
-            amount: 20000,
-            num_quota_tickets: 350,
-            name: 'New AI Agent Plan',
-        }
-        const periodEnd = '2023-06-30'
-        const cadence = Cadence.Month
+    it.each(Object.values(Cadence))(
+        'renders upgrade message when new product amount is greater [cadence: %s]',
+        (cadence: Cadence) => {
+            const oldPlan = automationProduct.prices.find(
+                (plan: AutomatePlan) => plan.cadence === cadence,
+            )
+            expect(oldPlan).toBeDefined()
+            if (oldPlan === undefined) {
+                return
+            }
+            const newPlan = {
+                ...oldPlan,
+                amount: oldPlan.amount + 1,
+                num_quota_tickets: 350,
+                name: 'New AI Agent Plan',
+            }
+            const periodEnd = '2023-06-30'
 
-        const notification = setAutomationNotification({
-            oldPlan: oldPlan,
-            newPlan: newPlan,
-            cadence: cadence,
-            periodEnd,
-            onClick,
-            isFreeTrial: false,
-        })
+            const notification = setAutomationNotification({
+                oldPlan: oldPlan,
+                newPlan: newPlan,
+                cadence: cadence,
+                periodEnd,
+                onClick,
+                isFreeTrial: false,
+            })
 
-        expect(notification?.message).toContain(
-            `Success! You now have <strong>${newPlan.num_quota_tickets} automated interactions per month</strong>`,
-        )
-        expect(notification?.buttons?.[0].name).toBe('AI Agent Settings')
-    })
+            expect(notification?.message).toContain(
+                `Success! You now have <strong>${newPlan.num_quota_tickets} automated interactions per ${newPlan.cadence}</strong>`,
+            )
+            expect(notification?.buttons?.[0].name).toBe('AI Agent Settings')
+        },
+    )
 
-    it('renders downgrade message when new product amount is less', () => {
-        const oldPlan = basicMonthlyAutomationPlan // amount is 3000
-        const newPlan = {
-            ...basicMonthlyAutomationPlan,
-            amount: 1000,
-            num_quota_tickets: 350,
-            name: 'New AI Agent Plan',
-        }
-        const periodEnd = '2023-06-30'
-        const cadence = Cadence.Month
+    it.each(Object.values(Cadence))(
+        'renders downgrade message when new product amount is less [cadence: %s]',
+        (cadence: Cadence) => {
+            const oldPlan = automationProduct.prices.find(
+                (plan: AutomatePlan) => plan.cadence === cadence,
+            )
+            expect(oldPlan).toBeDefined()
+            if (oldPlan === undefined) {
+                return
+            }
 
-        const notification = setAutomationNotification({
-            oldPlan: oldPlan,
-            newPlan: newPlan,
-            periodEnd,
-            cadence: cadence,
-            onClick,
-            isFreeTrial: false,
-        })
+            const newPlan = {
+                ...oldPlan,
+                amount: oldPlan.amount - 1,
+                num_quota_tickets: 350,
+                name: 'New AI Agent Plan',
+            }
+            const periodEnd = '2023-06-30'
 
-        expect(notification?.message).toContain(
-            `Your AI Agent subscription will change to <strong>${newPlan.num_quota_tickets} automated interactions/month</strong> on <strong>${periodEnd}</strong>.`,
-        )
-        expect(notification?.buttons?.length).toBe(0)
-    })
+            const notification = setAutomationNotification({
+                oldPlan: oldPlan,
+                newPlan: newPlan,
+                periodEnd,
+                cadence: cadence,
+                onClick,
+                isFreeTrial: false,
+            })
+
+            expect(notification?.message).toContain(
+                `Your AI Agent subscription will change to <strong>${newPlan.num_quota_tickets} automated interactions/${newPlan.cadence}</strong> on <strong>${periodEnd}</strong>.`,
+            )
+            expect(notification?.buttons?.length).toBe(0)
+        },
+    )
 })
