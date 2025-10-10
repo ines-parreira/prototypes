449d90cbd7ea5c7cea273b16ddb456a44657f19c
Boris C. <7647317+bcupac@users.noreply.github.com>
2025-10-10 10:09:20 +0200

    feat(MetafieldIngestion): table - add empty state component (#9798)
    * feat: add empty state component
    
    * chore: spacings
    
    * chore: unuse
    
    * chore: tests

---
+++ apps/helpdesk/src/pages/settings/storeManagement/storeDetailsPage/ShopifyMetafields/EmptyMetafieldsState.tsx
@@ -0,0 +1,17 @@
+import { Button, Heading, Text } from '@gorgias/axiom'
+
+import styles from './EmptyMetafieldsState.less'
+
+export default function EmptyMetafieldsState() {
+    return (
+        <div className={styles.emptyMetafieldsState}>
+            <Heading>You haven’t added any metafields yet</Heading>
+            <Text as="p" align="center">
+                Once you import them, you can manage them here. Please add one
+                to use the data in customer profiles, macros, rules, flows,
+                views and advanced search.{' '}
+            </Text>
+            <Button leadingIcon="add">Import</Button>
+        </div>
+    )
+}
---
+++ apps/helpdesk/src/pages/settings/storeManagement/storeDetailsPage/ShopifyMetafields/tests/EmptyMetafieldsState.spec.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import { render, screen } from '@testing-library/react'
+
+import EmptyMetafieldsState from '../EmptyMetafieldsState'
+
+describe('EmptyMetafieldsState', () => {
+    it('should render the heading correctly', () => {
+        render(<EmptyMetafieldsState />)
+
+        expect(
+            screen.getByText(/Once you import them, you can manage them here/i),
+        ).toBeInTheDocument()
+
+        const heading = screen.getByRole('heading')
+        expect(heading).toBeInTheDocument()
+
+        const importButton = screen.getByRole('button', { name: /import/i })
+        expect(importButton).toBeInTheDocument()
+    })
+})
