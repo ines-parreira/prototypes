d30325da57511dfdd399c8eb7a9d08169e0b2876
Boris C. <7647317+bcupac@users.noreply.github.com>
2025-10-30 15:22:55 +0100

    feat: use the axiom modal (#10243)

--- apps/helpdesk/src/pages/settings/storeManagement/storeDetailsPage/ShopifyMetafields/RemoveMetafieldConfirmation/RemoveMetafieldConfirmation.tsx
+++ apps/helpdesk/src/pages/settings/storeManagement/storeDetailsPage/ShopifyMetafields/RemoveMetafieldConfirmation/RemoveMetafieldConfirmation.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-import { LegacyButton as Button } from '@gorgias/axiom'
-
-import Modal from 'pages/common/components/modal/Modal'
-import ModalActionsFooter from 'pages/common/components/modal/ModalActionsFooter'
-import ModalBody from 'pages/common/components/modal/ModalBody'
-import ModalHeader from 'pages/common/components/modal/ModalHeader'
-
-import styles from './RemoveMetafieldConfirmation.less'
+import {
+    Box,
+    Button,
+    Modal,
+    OverlayContent,
+    OverlayFooter,
+    OverlayHeader,
+} from '@gorgias/axiom'
 
 type Props = {
     isOpen: boolean
@@ -23,24 +23,27 @@ const RemoveMetafieldConfirmation = ({ isOpen, onClose, onConfirm }: Props) => {
     return (
         <Modal
             isOpen={isOpen}
-            onClose={onClose}
-            size="medium"
-            classNameContent={styles.modalContent}
+            onOpenChange={(open) => {
+                if (!open) {
+                    onClose()
+                }
+            }}
+            size="sm"
+            isDismissable
         >
-            <ModalHeader className={styles.header} title="Remove metafield?" />
+            <OverlayHeader title="Remove metafield?" />
 
-            <ModalBody>
-                Once removed, the metafield data won&apos;t be available to use
-                in Gorgias, or to view in the customer profile. You can add it
-                back at any time.
-            </ModalBody>
+            <OverlayContent>
+                <Box>
+                    Once removed, the metafield data won&apos;t be available to
+                    use in Gorgias, or to view in the customer profile. You can
+                    add it back at any time.
+                </Box>
+            </OverlayContent>
 
-            <ModalActionsFooter innerClassName={styles.actions}>
-                <Button intent="secondary" onClick={onClose}>
-                    Cancel
-                </Button>
+            <OverlayFooter>
                 <Button onClick={handleConfirm}>Remove</Button>
-            </ModalActionsFooter>
+            </OverlayFooter>
         </Modal>
     )
 }
