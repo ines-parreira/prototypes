78f5551f7cd98ce277975818a99ac4b3e2da1e5a
NikolaMijovic93 <56353185+NikolaMijovic93@users.noreply.github.com>
2025-12-11 15:14:34 +0100

    chore(realtime): enable aus-southeast1-fcb9 & us-east4-65cd cluster traffic (#10503)
    * chore(realtime): enable aus-southeast1-fcb9 &  europe-west3-86c1 traffic
    
    * refactor: use us-east4-65cd instead of  europe-west3-86c1

--- apps/helpdesk/src/providers/realtime-ably/utils/isRealtimeEnabledOnCluster.ts
+++ apps/helpdesk/src/providers/realtime-ably/utils/isRealtimeEnabledOnCluster.ts
@@ -1,9 +1,19 @@
 const GORGIAS_CLUSTER = window.GORGIAS_CLUSTER
+
 const US_EAST1_CLUSTER = 'us-east1-635c'
+const US_EAST4_CLUSTER = 'us-east4-65cd'
+const AUS_SOUTHEAST1_CLUSTER = 'aus-southeast1-fcb9'
 const STAGING_CLUSTER = 'us-east1-86cc'
 
+const SUPPORTED_CLUSTERS = [
+    US_EAST1_CLUSTER,
+    US_EAST4_CLUSTER,
+    AUS_SOUTHEAST1_CLUSTER,
+    STAGING_CLUSTER,
+]
+
 function checkIfRealtimeEnabledOnCluster() {
-    return [US_EAST1_CLUSTER, STAGING_CLUSTER].includes(GORGIAS_CLUSTER)
+    return SUPPORTED_CLUSTERS.includes(GORGIAS_CLUSTER)
 }
 
 export const isRealtimeEnabledOnCluster = checkIfRealtimeEnabledOnCluster()
--- apps/helpdesk/src/providers/realtime-ably/utils/tests/isRealtimeEnabledOnCluster.spec.ts
+++ apps/helpdesk/src/providers/realtime-ably/utils/tests/isRealtimeEnabledOnCluster.spec.ts
@@ -1,6 +1,10 @@
 describe('isRealtimeEnabledOnCluster', () => {
     const US_EAST1_CLUSTER = 'us-east1-635c'
+    const US_EAST4_CLUSTER = 'us-east4-65cd'
+    const AUS_SOUTHEAST1_CLUSTER = 'aus-southeast1-fcb9'
+
     const STAGING_CLUSTER = 'us-east1-86cc'
+
     const OTHER_CLUSTER = 'europe-west1-c511'
 
     beforeEach(() => {
@@ -12,6 +16,16 @@ describe('isRealtimeEnabledOnCluster', () => {
             cluster: US_EAST1_CLUSTER,
             expected: true,
             description: 'cluster is us-east1-635c ',
+        },
+        {
+            cluster: AUS_SOUTHEAST1_CLUSTER,
+            expected: true,
+            description: 'cluster is aus-southeast1-fcb9 ',
+        },
+        {
+            cluster: US_EAST4_CLUSTER,
+            expected: true,
+            description: 'cluster is us-east4-65cd ',
         },
         {
             cluster: STAGING_CLUSTER,
