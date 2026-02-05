2e3b055fac2b79f396f7b72ad77995d176dd3cae
sarahlancaster-gorgias <sarah.lancaster@gorgias.com>
2026-02-05 09:01:44 +0100

    fix(helpdesk): fix label in delete instgration popup (#11630)

--- apps/helpdesk/src/pages/integrations/integration/constants.ts
+++ apps/helpdesk/src/pages/integrations/integration/constants.ts
@@ -1,2 +1,2 @@
 export const INTEGRATION_REMOVAL_CONFIGURATION_TEXT =
-    'Are you sure you want to delete your email integration from Gorgias? This action cannot be undone. All views and rules associated with this store will be disabled. The store integration needs to be removed from Saved Filters manually.'
+    'Are you sure you want to delete your integration from Gorgias? This action cannot be undone. All views and rules associated with this store will be disabled. The store integration needs to be removed from Saved Filters manually.'
