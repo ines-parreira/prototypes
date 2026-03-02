7b445909309c9ecc5220bb7970101f78980a2c73
andrejtomic <andrej.tomic@gorgias.com>
2026-03-02 10:40:06 +0100

    fix(live-agents): truncate text to prevent status being cut off (#12089)

--- packages/agent-status/src/components/AgentAvailabilityStatusSelect/AgentAvailabilityStatusSelect.tsx
+++ packages/agent-status/src/components/AgentAvailabilityStatusSelect/AgentAvailabilityStatusSelect.tsx
@@ -11,6 +11,8 @@ import {
 import { useSelectableAgentAvailabilityStatuses } from '../../hooks'
 import type { AgentAvailabilityStatusSelectProps } from './types'
 
+import css from './AgentAvailabilityStatusSelect.less'
+
 export function AgentAvailabilityStatusSelect({
     activeAvailabilityStatus,
     onSelect,
@@ -25,37 +27,44 @@ export function AgentAvailabilityStatusSelect({
     )
 
     return (
-        <Select
-            items={allStatuses}
-            selectedItem={activeAvailabilityStatus}
-            onSelect={onSelect}
-            trigger={({ ref, selectedText, isPlaceholder }) => {
-                const text = isPlaceholder ? placeholder : selectedText
-                return (
-                    <SelectTrigger ref={ref}>
-                        <Badge type={isAvailable ? 'success' : 'warning'}>
-                            {text}
-                            {!isDisabled && <span>▾</span>}
-                        </Badge>
-                    </SelectTrigger>
-                )
-            }}
-            placeholder={placeholder}
-            isDisabled={isDisabled}
-            keyName="id"
-        >
-            {({ id, name }) => {
-                return (
-                    <ListItem
-                        id={id}
-                        label={name}
-                        textValue={name}
-                        leadingSlot={
-                            <Dot color={id === 'available' ? 'green' : 'red'} />
-                        }
-                    />
-                )
-            }}
-        </Select>
+        <div className={css.container}>
+            <Select
+                items={allStatuses}
+                selectedItem={activeAvailabilityStatus}
+                onSelect={onSelect}
+                trigger={({ ref, selectedText, isPlaceholder }) => {
+                    const text = isPlaceholder ? placeholder : selectedText
+                    return (
+                        <SelectTrigger ref={ref}>
+                            <Badge
+                                type={isAvailable ? 'success' : 'warning'}
+                                className={css.badge}
+                            >
+                                <span className={css.badgeText}>{text}</span>
+                                {!isDisabled && <span>▾</span>}
+                            </Badge>
+                        </SelectTrigger>
+                    )
+                }}
+                placeholder={placeholder}
+                isDisabled={isDisabled}
+                keyName="id"
+            >
+                {({ id, name }) => {
+                    return (
+                        <ListItem
+                            id={id}
+                            label={name}
+                            textValue={name}
+                            leadingSlot={
+                                <Dot
+                                    color={id === 'available' ? 'green' : 'red'}
+                                />
+                            }
+                        />
+                    )
+                }}
+            </Select>
+        </div>
     )
 }
