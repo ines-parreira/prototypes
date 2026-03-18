e0b0e2e0d0da0315c37f7efd603877f525062be4
Vesna Katanic <vesna.katanic@gorgias.com>
2026-03-18 10:10:42 +0100

    Revert "fix(COACH-2265): ensure chat bubble is clickable when side panel is o…" (#12447)
    This reverts commit 5dcf020399d5ca5857e58d1a77aa2fbfd0ef1a02.

--- apps/helpdesk/src/pages/aiAgent/components/KnowledgeEditor/KnowledgeEditor.tsx
+++ apps/helpdesk/src/pages/aiAgent/components/KnowledgeEditor/KnowledgeEditor.tsx
@@ -136,7 +136,6 @@ export const KnowledgeEditor = (props: Props) => {
                 }
             }}
             isDismissable
-            withoutOverlay
             withoutPadding
             width={width}
         >
