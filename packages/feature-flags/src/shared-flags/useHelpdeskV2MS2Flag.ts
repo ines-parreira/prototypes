f5c3b486aa8ca9184ee619ff19bde862aeaadf58
Boris C. <7647317+bcupac@users.noreply.github.com>
2026-05-08 16:02:21 +0200

    fix(Helpdesk 2): disable ms flags in mobile view (#13590)

--- packages/feature-flags/src/shared-flags/useHelpdeskV2MS2Flag.ts
+++ packages/feature-flags/src/shared-flags/useHelpdeskV2MS2Flag.ts
@@ -1,7 +1,11 @@
+import { useIsMobileResolution } from '@repo/hooks'
+
 import { useHelpdeskV2BaselineFlag } from './useHelpdeskV2BaselineFlag'
 
 export function useHelpdeskV2MS2Flag(): boolean {
+    const isMobileResolution = useIsMobileResolution()
+
     const { hasUIVisionBeta } = useHelpdeskV2BaselineFlag()
 
-    return hasUIVisionBeta
+    return hasUIVisionBeta && !isMobileResolution
 }
