fc735148f004d5d7b59eb7e86bb8aa51f896609b
Nicolas Toulemont <40027895+nicolastoulemont@users.noreply.github.com>
2026-06-05 12:28:02 +0200

    test: harden DiscountCodeResults flaky test (#14042)

--- apps/helpdesk/src/pages/common/components/DiscountCodeResults/tests/DiscountCodeResults.spec.tsx
+++ apps/helpdesk/src/pages/common/components/DiscountCodeResults/tests/DiscountCodeResults.spec.tsx
@@ -2,17 +2,16 @@ import React from 'react'
 
 import client from '@repo/api-resources'
 import { render } from '@repo/testing'
-import { screen, waitFor } from '@testing-library/react'
+import { act, screen } from '@testing-library/react'
 import MockAdapter from 'axios-mock-adapter'
 import { fromJS } from 'immutable'
-import { Provider } from 'react-redux'
-import configureMockStore from 'redux-mock-store'
-import thunk from 'redux-thunk'
 
 import { discountCodeResult } from 'fixtures/discountCodes'
 import { integrationsState } from 'fixtures/integrations'
 
 import DiscountCodeResults from '../DiscountCodeResults'
+
+const FETCH_RESULTS_DEBOUNCE_MS = 300
 
 const minProps = {
     integration: fromJS({
@@ -28,23 +27,32 @@ const minProps = {
     onResetStoreChoice: jest.fn(),
 }
 
-const middlewares = [thunk]
-const mockStore = configureMockStore(middlewares)
+const renderDiscountCodeResults = () => {
+    return render(<DiscountCodeResults {...minProps} />)
+}
+
+const flushDiscountCodeFetchDebounce = async () => {
+    await act(async () => {
+        await jest.advanceTimersByTimeAsync(FETCH_RESULTS_DEBOUNCE_MS)
+    })
+}
 
 describe('<DiscountCodeResults />', () => {
-    const store = mockStore({})
     let mockServer: MockAdapter
 
     beforeEach(() => {
+        jest.useFakeTimers()
         mockServer = new MockAdapter(client)
     })
 
+    afterEach(() => {
+        mockServer.restore()
+        jest.useRealTimers()
+    })
+
     it('should render the component', () => {
-        const { container } = render(
-            <Provider store={store}>
-                <DiscountCodeResults {...minProps} />
-            </Provider>,
-        )
+        const { container } = renderDiscountCodeResults()
+
         expect(container.firstChild).toMatchSnapshot()
     })
 
@@ -53,41 +61,33 @@ describe('<DiscountCodeResults />', () => {
             .onGet('/api/discount-codes/5/')
             .reply(200, discountCodeResult())
 
-        const { container, getByText } = render(
-            <Provider store={store}>
-                <DiscountCodeResults {...minProps} />
-            </Provider>,
-        )
+        const { container } = renderDiscountCodeResults()
 
-        await waitFor(() => {
-            expect(getByText(/CODE1/i)).toBeDefined()
-            expect(container).toMatchSnapshot()
-        })
+        await flushDiscountCodeFetchDebounce()
+
+        expect(mockServer.history.get).toHaveLength(1)
+        expect(await screen.findByText(/CODE1/i)).toBeInTheDocument()
+        expect(container).toMatchSnapshot()
     })
 
     it('should render no results', async () => {
         mockServer.onGet('/api/discount-codes/5/').reply(200, { data: [] })
 
-        const { container, getByText } = render(
-            <Provider store={store}>
-                <DiscountCodeResults {...minProps} />
-            </Provider>,
-        )
+        const { container } = renderDiscountCodeResults()
 
-        await waitFor(() => {
-            expect(getByText(/No results found/i)).toBeDefined()
-            expect(container).toMatchSnapshot()
-        })
+        await flushDiscountCodeFetchDebounce()
+
+        expect(mockServer.history.get).toHaveLength(1)
+        expect(await screen.findByText(/No results found/i)).toBeInTheDocument()
+        expect(container).toMatchSnapshot()
     })
 
     it('shows an error toast when fetching discount codes fails', async () => {
         mockServer.onGet('/api/discount-codes/5/').reply(500)
 
-        render(
-            <Provider store={store}>
-                <DiscountCodeResults {...minProps} />
-            </Provider>,
-        )
+        renderDiscountCodeResults()
+
+        await flushDiscountCodeFetchDebounce()
 
         const toastEl = await screen.findByRole('status', {
             name: "Couldn't fetch discount codes",
